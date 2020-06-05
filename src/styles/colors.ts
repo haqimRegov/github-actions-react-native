e8faa8dde9c55e453eb29b7f3b53909da526eab4
Aldrin <aldrin.castro@regovtech.com>
2020-06-05 16:38:20 +0800

    added common colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,23 @@
+export const colorBlack = {
+  _1: "#000000",
+  _2: "#0a1f44",
+};
+
+export const colorBlue = {
+  _1: "#0C66FF",
+  _2: "#0D55CF",
+  _3: "#007fff",
+};
+
+export const colorGray = {
+  _1: "#a6aebc",
+  _2: "#e1e4e8",
+};
+
+export const colorRed = {
+  _1: "#f03d3d",
+};
+
+export const colorWhite = {
+  _1: "#FFFFFF",
+};

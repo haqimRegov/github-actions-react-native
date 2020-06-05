f4513ef4be0d38b228f97693e45278bf8c85e7d5
Aldrin <aldrin.castro@regovtech.com>
2020-06-05 16:36:36 +0800

    added LinkText component

---
+++ src/components/Touchables/LinkText.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, TextStyle } from "react-native";
+
+import { fs13MedBlue2, fsAlignCenter, fsCapitalize } from "../../styles";
+
+interface LinkTextProps {
+  onPress?: () => void;
+  style?: TextStyle;
+  text: string;
+}
+
+export const LinkText = ({ onPress, style, text }: LinkTextProps) => {
+  const textLinkStyle: TextStyle = { ...fs13MedBlue2, ...fsAlignCenter, ...fsCapitalize, ...style };
+
+  return (
+    <Text onPress={onPress} style={textLinkStyle}>
+      {text}
+    </Text>
+  );
+};

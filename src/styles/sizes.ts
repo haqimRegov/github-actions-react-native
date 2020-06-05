c49e72cc2c027645f6c9b53544c7e8aab1560f60
Aldrin <aldrin.castro@regovtech.com>
2020-06-05 16:38:46 +0800

    added common sizes

---
+++ src/styles/sizes.ts
@@ -0,0 +1,24 @@
+import { scaleHeight, scaleWidth } from "./scaler";
+
+export const sh10 = scaleHeight(10);
+export const sh11 = scaleHeight(11);
+export const sh12 = scaleHeight(12);
+export const sh13 = scaleHeight(13);
+export const sh14 = scaleHeight(14);
+export const sh16 = scaleHeight(16);
+export const sh20 = scaleHeight(20);
+export const sh23 = scaleHeight(23);
+export const sh24 = scaleHeight(24);
+export const sh32 = scaleHeight(32);
+export const sh44 = scaleHeight(44);
+export const sh48 = scaleHeight(48);
+export const sh80 = scaleHeight(80);
+
+export const sw2 = scaleWidth(2);
+export const sw5 = scaleWidth(5);
+export const sw14 = scaleWidth(14);
+export const sw40 = scaleWidth(40);
+export const sw80 = scaleWidth(80);
+export const sw100 = scaleWidth(100);
+export const sw200 = scaleWidth(200);
+export const sw304 = scaleWidth(304);

b8a9d6df3ecd1a1045949a43b7a351d89226277e
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2020-06-12 15:53:02 +0800

    Modal on login(for now)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Alert, Image, ImageStyle, Text, View, ViewStyle } from "react-native";
+import React, { useState, Fragment } from "react";
+import { Alert, Image, ImageStyle, Text, View, ViewStyle, TextStyle } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
 import { LocalAssets } from "../assets/LocalAssets";
@@ -23,23 +23,73 @@ import {
   sh32,
   sh44,
   sh80,
+  fs14MedBlack,
   shadow,
   sw200,
   sw304,
   sw80,
+  sh28,
+  fs20MedBlack,
 } from "../styles";
+import { CustomModal } from "./CustomModal";
 
 const { INPUT_AGENT_CODE, INPUT_PASSWORD, BUTTON_LOGIN, FORGOT_PASSWORD, TITLE, PRIVACY_TERMS } = Language.PAGE.LOGIN;
-
+const {
+  CONFIRM_YOUR_DETAILS,
+  OUTPUT_FULL_NAME,
+  OUTPUT_FULL_NAME_PLACEHOLDER,
+  OUTPUT_NRIC_PASSPORT,
+  OUTPUT_NRIC_PASSPORT_PLACEHOLDER,
+  OUTPUT_GENDER,
+  OUTPUT_GENDER_PLACEHOLDER,
+  OUTPUT_DATE_OF_BIRTH,
+  OUTPUT_DATE_OF_BIRTH_PLACEHOLDER,
+  BUTTON_RE_UPLOAD,
+  BUTTON_CONFIRM,
+} = Language.PAGE.MODAL;
+const buttonLR: string[] = [BUTTON_RE_UPLOAD, BUTTON_CONFIRM];
+const ModalInfo = [
+  {
+    Data: {
+      Title: OUTPUT_FULL_NAME,
+      Output: OUTPUT_FULL_NAME_PLACEHOLDER,
+    },
+  },
+  {
+    Data: {
+      Title: OUTPUT_NRIC_PASSPORT,
+      Output: OUTPUT_NRIC_PASSPORT_PLACEHOLDER,
+    },
+  },
+  {
+    Data: {
+      Title: OUTPUT_GENDER,
+      Output: OUTPUT_GENDER_PLACEHOLDER,
+    },
+  },
+  {
+    Data: {
+      Title: OUTPUT_DATE_OF_BIRTH,
+      Output: OUTPUT_DATE_OF_BIRTH_PLACEHOLDER,
+    },
+  },
+];
 export const LoginPage = () => {
   const container: ViewStyle = { ...centerHV, ...flexChild, backgroundColor: colorWhite._1 };
   const formContainer: ViewStyle = { ...px(sw80), ...shadow, backgroundColor: colorWhite._1 };
   const inputStyle: ViewStyle = { minWidth: sw304 };
   const logoStyle: ImageStyle = { ...alignSelfCenter, height: sh80, resizeMode: "contain", width: sw200 };
+  const groupHeight: TextStyle = { marginBottom: sh28, ...fs20MedBlack };
+  const groupHeightSmall: TextStyle = { ...fs14MedBlack };
+  const [ModalVisible, setModalVisible] = useState<boolean>(false);
 
-  const handleLogin = () => {
-    Alert.alert("Login");
+  const handleModel = () => {
+    setModalVisible(!ModalVisible);
   };
+
+  // const handleLogin = () => {
+  //   Alert.alert("Login");
+  // };
 
   const handleForgotPassword = () => {
     Alert.alert("ForgotPassword");
@@ -47,6 +97,15 @@ export const LoginPage = () => {
 
   const handlePrivacyTerms = () => {
     Alert.alert("PrivacyTerms");
+  };
+
+  const ModalContent = () => {
+    return ModalInfo.map(({ Data }) => (
+      <Fragment>
+        <Text style={groupHeightSmall}>{Data.Title}</Text>
+        <Text style={groupHeight}>{Data.Output}</Text>
+      </Fragment>
+    ));
   };
 
   return (
@@ -67,11 +126,18 @@ export const LoginPage = () => {
           <View style={{ ...flexRow, ...centerVertical }}>
             <LinkText onPress={handleForgotPassword} text={FORGOT_PASSWORD} />
             <CustomFlexSpacer />
-            <RoundedButton onPress={handleLogin} text={BUTTON_LOGIN} />
+            <RoundedButton onPress={handleModel} text={BUTTON_LOGIN} />
           </View>
           <CustomSpacer space={sh80} />
         </View>
       </ScrollView>
+      <CustomModal
+        visible={ModalVisible}
+        handleClose={handleModel}
+        buttonLR={buttonLR}
+        content={ModalContent}
+        Title={CONFIRM_YOUR_DETAILS}
+      />
     </SafeAreaPage>
   );
 };

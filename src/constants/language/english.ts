3a575da91d76926499c324cc3ac6c9971340b8bf
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2020-06-12 15:52:09 +0800

    static data

--- src/constants/language/english.ts
+++ src/constants/language/english.ts
@@ -8,5 +8,18 @@ export const english = {
       INPUT_AGENT_CODE: "Enter Agent Code",
       INPUT_PASSWORD: "Enter your Password",
     },
+    MODAL: {
+      CONFIRM_YOUR_DETAILS: "Please confirm your details.",
+      OUTPUT_FULL_NAME: "Full Name",
+      OUTPUT_FULL_NAME_PLACEHOLDER: "Edgar Constantine",
+      OUTPUT_NRIC_PASSPORT: "NRIC/Passport",
+      OUTPUT_NRIC_PASSPORT_PLACEHOLDER: "123 4567 8901",
+      OUTPUT_GENDER: "Gender",
+      OUTPUT_GENDER_PLACEHOLDER: "Male",
+      OUTPUT_DATE_OF_BIRTH: "Date of Birth",
+      OUTPUT_DATE_OF_BIRTH_PLACEHOLDER: "14 August 1988",
+      BUTTON_RE_UPLOAD: "Re-Upload",
+      BUTTON_CONFIRM: "Confirm",
+    },
   },
 };

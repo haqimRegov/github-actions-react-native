451b212205a3aecd3a26c8f8fe144d409a91ec1d
Aldrin <aldrin.castro@regovtech.com>
2020-06-17 18:01:20 +0800

    added simple checkbox without functionality

---
+++ src/components/Touchables/CheckBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Text, TextStyle, View, ViewStyle } from "react-native";
+
+import { centerVertical, colorBlack, flexRow, fs12RegBlack2, fsAlignCenter, fsCapitalize, sh18, sw10, sw18, sw2 } from "../../styles";
+import { CustomSpacer } from "../Views";
+
+interface CheckBoxProps {
+  label: string;
+  onPress?: () => void;
+  spaceToLabel?: number;
+  style?: TextStyle;
+  toggle: boolean;
+}
+
+export const CheckBox = ({ onPress, spaceToLabel, style, label, toggle }: CheckBoxProps) => {
+  const labelStyle: TextStyle = { ...fs12RegBlack2, ...fsAlignCenter, ...fsCapitalize, ...style };
+  // TODO proper design for toggle
+  const toggleStyle: ViewStyle = toggle ? { ...flexRow, ...centerVertical } : { ...flexRow, ...centerVertical };
+  const defaultSpace = spaceToLabel !== undefined ? spaceToLabel : sw10;
+
+  return (
+    <View style={toggleStyle}>
+      <View style={{ borderWidth: sw2, borderColor: colorBlack._2, borderRadius: sw2, height: sh18, width: sw18 }} />
+      <CustomSpacer isHorizontal={true} space={defaultSpace} />
+      <Text onPress={onPress} style={labelStyle}>
+        {label}
+      </Text>
+    </View>
+  );
+};

6081e1f561570ec888f379aca9833f92c278f9e2
Aldrin <aldrin.castro@regovtech.com>
2020-06-18 11:57:49 +0800

    hide splash screen on render

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { KeyboardAvoidingView, Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import SplashScreen from "react-native-splash-screen";
 
 import { RootNavigator } from "./Navigator";
 import { flexChild } from "./styles";
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
       {Platform.select({

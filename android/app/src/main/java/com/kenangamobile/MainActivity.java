0e1df856bc94abea1800fd0102f1a4ff4a34f4f2
Aldrin <aldrin.castro@regovtech.com>
2020-06-18 12:38:02 +0800

    [android] updated onCreate

--- android/app/src/main/java/com/kenangamobile/MainActivity.java
+++ android/app/src/main/java/com/kenangamobile/MainActivity.java
@@ -1,9 +1,16 @@
 package com.kenangamobile;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
 
+public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+//    SplashScreen.show(this, true);  // commented by developer because of issue
+    super.onCreate(savedInstanceState);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.

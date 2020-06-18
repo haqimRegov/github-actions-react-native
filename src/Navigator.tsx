70d74c839f061462ac3522ec5fa806a731c0f27b
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2020-06-18 15:10:30 +0800

    navigator

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -4,16 +4,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 
-import { LoginPage, OnboardingPage } from "./pages";
+import { LoginPage, OnboardingPage, NetWorthAssessment } from "./pages";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export const RootNavigator = () => {
   return (
     <NavigationContainer>
-      <Navigator initialRouteName="Login" headerMode="none">
+      <Navigator initialRouteName="NetWorthAssessment" headerMode="none">
         <Screen name="Login" component={LoginPage} />
         <Screen name="Onboarding" component={OnboardingPage} />
+        <Screen name="NetWorthAssessment" component={NetWorthAssessment} />
       </Navigator>
     </NavigationContainer>
   );

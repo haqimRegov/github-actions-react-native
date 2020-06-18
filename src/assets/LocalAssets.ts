26643d5e656006d2ea60a0a00eabbcfefc970afa
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2020-06-18 15:15:00 +0800

    importing onboarding people pictures into local assests

--- src/assets/LocalAssets.ts
+++ src/assets/LocalAssets.ts
@@ -3,6 +3,7 @@ import logoKenanga from "./logo_kenanga/logo_kenanga.png";
 import kib from "./logo_kib/logo_kib.png";
 import riskAssessmentGraph1 from "./risk_assessment_graph_1/risk_assessment_graph_1.png";
 import riskAssessmentGraph2 from "./risk_assessment_graph_2/risk_assessment_graph_2.png";
+import OnboardingPeople from "./onboarding_people/onboarding_group_xhdpi.png";
 
 export const LocalAssets = {
   logo: {
@@ -15,5 +16,8 @@ export const LocalAssets = {
   graph: {
     risk_assessment_graph_1: riskAssessmentGraph1,
     risk_assessment_graph_2: riskAssessmentGraph2,
+  },
+  OnboardingPeople: {
+    OnboardingPeople: OnboardingPeople,
   },
 };

188ef6f002939863a6834a921443f4ae1501c321
Aldrin <aldrin.castro@regovtech.com>
2020-06-18 13:10:42 +0800

    added onboarding routes in constants

---
+++ src/constants/routes/onboarding.ts
@@ -0,0 +1,11 @@
+export const ONBOARDING_ROUTES = {
+  Address: "Address",
+  Confirmation: "Confirmation",
+  ContactDetails: "ContactDetails",
+  Declaration: "Declaration",
+  EmploymentDetails: "EmploymentDetails",
+  FundingOptions: "FundingOptions",
+  Payment: "Payment",
+  ProductRecommendation: "ProductRecommendation",
+  Questionnaire: "Questionnaire",
+};

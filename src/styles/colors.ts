d994b4f31a266d1460cc5f81dfae9e35895af52e
Nithin <johnnithin08@gmail.com>
2020-06-18 11:47:19 +0800

    colors added

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,17 +2,25 @@ export const colorBlack = {
   _1: "#000000",
   _2: "#4d4d4d",
   _3: "#515151",
+  _4: "#0a1f44",
+  _5: "#202020",
 };
 
 export const colorBlue = {
   _1: "#0091ff",
   _2: "#0D55CF",
   _3: "#007fff",
+  _4: "#0072ff",
+  _5: "#0091ff",
+  _6: "#4098f6",
 };
 
 export const colorGray = {
   _1: "#a6aebc",
   _2: "#e1e4e8",
+  _3: "#f2f4f6",
+  _4: "#8f9bb3",
+  _5: "#c8c9cc",
 };
 
 export const colorWhite = {

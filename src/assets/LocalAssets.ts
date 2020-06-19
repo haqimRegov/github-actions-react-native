c0d4f5a50244bddf95f0c943b52c753f01d1075e
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2020-06-19 16:12:12 +0800

    changing the naming in local assets

--- src/assets/LocalAssets.ts
+++ src/assets/LocalAssets.ts
@@ -3,7 +3,7 @@ import logoKenanga from "./logo_kenanga/logo_kenanga.png";
 import kib from "./logo_kib/logo_kib.png";
 import riskAssessmentGraph1 from "./risk_assessment_graph_1/risk_assessment_graph_1.png";
 import riskAssessmentGraph2 from "./risk_assessment_graph_2/risk_assessment_graph_2.png";
-import OnboardingPeople from "./onboarding_people/onboarding_group_xhdpi.png";
+import onboardingPeople from "./onboarding_people/onboarding_people.png";
 
 export const LocalAssets = {
   logo: {
@@ -17,7 +17,7 @@ export const LocalAssets = {
     risk_assessment_graph_1: riskAssessmentGraph1,
     risk_assessment_graph_2: riskAssessmentGraph2,
   },
-  OnboardingPeople: {
-    OnboardingPeople: OnboardingPeople,
+  onboarding: {
+    people: onboardingPeople,
   },
 };

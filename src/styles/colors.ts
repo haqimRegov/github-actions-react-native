0d98e15b3addea06e43689b9b6755da8f056d7ce
HamidMirghafoori <datasciencestep@gmail.com>
2020-06-19 12:34:25 +0800

    colors scaler and sized for newclient page added

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -10,7 +10,7 @@ export const colorBlue = {
   _1: "#0091ff",
   _2: "#0D55CF",
   _3: "#007fff",
-  _4: "#0072ff",
+  _4: "#377bff",
 };
 
 export const colorGray = {
@@ -19,6 +19,9 @@ export const colorGray = {
   _3: "#f2f4f6",
   _4: "#8f9bb3",
   _5: "#c8c9cc",
+  _4: "#dae1ed",
+  _5: "#fafcfd",
+  _6: "#9e9e9e",
 };
 
 export const colorWhite = {

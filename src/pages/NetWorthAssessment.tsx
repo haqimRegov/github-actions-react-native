1bbe9a07187cb49d3eaaa31f6ad3fa402e033bac
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2020-06-19 16:15:13 +0800

    net worth assessment page

--- src/pages/NetWorthAssessment.tsx
+++ src/pages/NetWorthAssessment.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { Text, View, ViewStyle, Image, Alert } from "react-native";
 
-// import { useSafeArea } from "react-native-safe-area-context";
 import { LocalAssets } from "../assets/LocalAssets";
 import {
   colorGray,
-  f32SemiBoldBlack4,
+  fs32SemiBoldBlack4,
   flexRow,
   fs12BoldBlack4,
   fs16RegBlack2,
@@ -27,79 +26,71 @@ import {
   sw36,
   sw400,
   sw96,
+  sw24,
+  colorWhite,
+  sw240,
 } from "../styles";
 import { CustomSpacer, CustomFlexSpacer, CheckBox, RoundedButton } from "../components";
+import { Language } from "../constants";
 
+const { HEADING } = Language.PAGE.ONBOARDING;
+const {
+  ANSWER_1,
+  ANSWER_2,
+  ANSWER_3,
+  BEFORE_WE_CONTINUE,
+  BUTTON_SKIP_RISK_ASSESSMENT,
+  BUTTON_TAKE_RISK_ASSESSMENT,
+  HELLO,
+  QUESTION,
+} = Language.PAGE.RISK_ASSESSMENT;
+const USER_NAME: string = "Edgar";
+const HELLO_USER: string = `${HELLO} ${USER_NAME}`;
 const STEP_INFO = ["Risk Assessment", "Product Recommendation", "Personal Information", "Conformation", "Payment"];
-const RISK_ASSESSMENT_INFO = [
-  "My gross annual income is more than RM 300,000/Year.",
-  "My annual household income* is more than RM 400,000/Year.",
-  "My current net asset** worth is  more than RM 3,000,000.",
-];
-const BUTTONS = ["Skip Risk Assessment", "Take Risk Assessment"];
+const RISK_ASSESSMENT_ANSWER = [ANSWER_1, ANSWER_2, ANSWER_3];
 
 export const NetWorthAssessment = () => {
-  // const { bottom, top } = useSafeArea();
   const handlePress = () => Alert.alert("alert");
-  const pageContainer: ViewStyle = { ...flexRow, ...fullHeight };
-  const stepsContainer: ViewStyle = { paddingLeft: sw36, width: sw400 };
-  const assessmentContainer: ViewStyle = { paddingLeft: sw96, backgroundColor: colorGray._3, ...fullWidth };
+  const pageContainer: ViewStyle = { ...flexRow, ...fullHeight, backgroundColor: colorGray._3 };
   return (
     <View style={pageContainer}>
-      <View>
-        <View style={stepsContainer}>
+      <View style={{ backgroundColor: colorWhite._2 }}>
+        <View style={{ paddingLeft: sw36, width: sw400 }}>
           <CustomSpacer space={sh72} />
-          <View>
-            <Image source={LocalAssets.logo.kenanga} />
-          </View>
+          <Image source={LocalAssets.logo.kenanga} />
           <CustomSpacer space={sh91} />
-          <View>
-            <Text style={f32SemiBoldBlack4}>Your financial</Text>
-            <Text style={f32SemiBoldBlack4}>freedom in just</Text>
-            <Text style={f32SemiBoldBlack4}>5 simple steps</Text>
-          </View>
+          <Text style={{ ...fs32SemiBoldBlack4, width: sw240 }}>{HEADING}</Text>
           <CustomSpacer space={sh24} />
-          <View>
-            {STEP_INFO.map((INFO: string, index: number) => (
-              <View style={flexRow} key={index}>
-                <Text style={fs12BoldBlack4}>
-                  STEP {index + 1}
-                  {"           "}
-                </Text>
-                <Text style={fs16RegBlack4}>{INFO}</Text>
-                <CustomSpacer space={sh40} />
-              </View>
-            ))}
-          </View>
-        </View>
-        <CustomFlexSpacer />
-        <Image source={LocalAssets.OnboardingPeople.OnboardingPeople} />
-      </View>
-      <View style={assessmentContainer}>
-        <CustomSpacer space={sh184} />
-        <View>
-          <Text style={fs36SemiBoldBlack2}>Hello Edgar.</Text>
-          <Text style={fs24RegBlack2}>Before we continue, let's find your risk appetite.</Text>
-          <CustomSpacer space={sh8} />
-          <Text style={fs16RegBlack2}>Do you fall under any of the following categories,</Text>
-        </View>
-        <CustomSpacer space={sh12} />
-        <View>
-          {RISK_ASSESSMENT_INFO.map((INFO: string, index: number) => (
-            <React.Fragment key={index}>
-              <CheckBox label={INFO} toggle={true} />
-              <CustomSpacer space={sh16} />
-            </React.Fragment>
+          {STEP_INFO.map((INFO: string, index: number) => (
+            <View style={flexRow} key={index}>
+              <Text style={fs12BoldBlack4}>STEP {index + 1}</Text>
+              <CustomSpacer space={sw24} isHorizontal={true} />
+              <Text style={fs16RegBlack4}>{INFO}</Text>
+              <CustomSpacer space={sh40} />
+            </View>
           ))}
         </View>
+        <CustomFlexSpacer />
+        <Image source={LocalAssets.onboarding.people} />
+      </View>
+      <View style={{ paddingLeft: sw96, ...fullWidth }}>
+        <CustomSpacer space={sh184} />
+        <Text style={fs36SemiBoldBlack2}>{HELLO_USER}</Text>
+        <Text style={fs24RegBlack2}>{BEFORE_WE_CONTINUE}</Text>
+        <CustomSpacer space={sh8} />
+        <Text style={fs16RegBlack2}>{QUESTION}</Text>
+        <CustomSpacer space={sh12} />
+        {RISK_ASSESSMENT_ANSWER.map((INFO: string, index: number) => (
+          <React.Fragment key={index}>
+            <CheckBox label={INFO} toggle={true} />
+            <CustomSpacer space={sh16} />
+          </React.Fragment>
+        ))}
         <CustomFlexSpacer />
         <View style={flexRow}>
-          {BUTTONS.map((INFO: string, index: number) => (
-            <React.Fragment key={index}>
-              <RoundedButton text={INFO} onPress={handlePress} secondary={INFO === "Skip Risk Assessment"} />
-              <CustomSpacer space={sw16} isHorizontal={true} />
-            </React.Fragment>
-          ))}
+          <RoundedButton text={BUTTON_SKIP_RISK_ASSESSMENT} onPress={handlePress} secondary={true} />
+          <CustomSpacer space={sw16} isHorizontal={true} />
+          <RoundedButton text={BUTTON_TAKE_RISK_ASSESSMENT} onPress={handlePress} />
         </View>
         <CustomSpacer space={sh48} />
       </View>

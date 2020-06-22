34fe1bd534ac673983e3d892e814de81390350d9
Aldrin <aldrin.castro@regovtech.com>
2020-06-22 12:24:53 +0800

    removed comments

--- src/styles/text.ts
+++ src/styles/text.ts
@@ -2,7 +2,7 @@ import { TextStyle } from "react-native";
 
 import { NunitoBold, NunitoRegular, NunitoSemiBold } from "../constants/fonts";
 import { colorBlack, colorBlue, colorGray, colorWhite } from "./colors";
-import { sh12, sh13, sh15, sh16, sh24, sh32, sh36 } from "./sizes";
+import { sh12, sh13, sh15, sh16, sh18, sh24, sh32, sh36, sh8 } from "./sizes";
 
 export const fsAlignCenter: TextStyle = { textAlign: "center" };
 export const fsUppercase: TextStyle = {
@@ -16,188 +16,161 @@ export const fsUnderline: TextStyle = {
   textDecorationLine: "underline",
 };
 
-// TODO change to proper lineHeight
 export const fs8SemiBoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoSemiBold,
   fontSize: sh8,
 };
 
-// TODO change to proper color
 export const fs12BoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoBold,
   fontSize: sh12,
 };
-// TODO change to proper lineHeight
 export const fs12BoldBlack4: TextStyle = {
   color: colorBlack._4,
   fontFamily: NunitoBold,
   fontSize: sh12,
 };
 
-// TODO change to proper font
 export const fs12BoldGray4: TextStyle = {
   color: colorGray._4,
   fontFamily: NunitoBold,
   fontSize: sh12,
 };
 
-// TODO change to proper font
 export const fs12RegBlack: TextStyle = {
   color: colorBlack._1,
   fontFamily: NunitoRegular,
   fontSize: sh12,
 };
 
-// TODO change to proper lineHeight
 export const fs12RegBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoRegular,
   fontSize: sh12,
 };
 
-// TODO change to proper color
 export const fs12RegBlue: TextStyle = {
   color: colorBlue._1,
   fontFamily: NunitoRegular,
   fontSize: sh12,
 };
 
-// TODO change to proper color
 export const fs12SemiBoldBlack: TextStyle = {
   color: colorBlack._1,
   fontFamily: NunitoSemiBold,
   fontSize: sh12,
 };
 
-// TODO change to proper font
 export const fs12SemiBoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoSemiBold,
   fontSize: sh12,
 };
 
-// TODO change to proper font
 export const fs13RegBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoRegular,
   fontSize: sh13,
 };
 
-// TODO change to proper font
 export const fs13SemiBoldBlue2: TextStyle = {
   color: colorBlue._2,
   fontFamily: NunitoSemiBold,
   fontSize: sh13,
 };
 
-// TODO change to proper color
 export const fs15BoldBlack4: TextStyle = {
   color: colorBlack._4,
   fontFamily: NunitoBold,
   fontSize: sh15,
 };
 
-// TODO change to proper font
 export const fs15BoldWhite: TextStyle = {
   color: colorWhite._1,
   fontFamily: NunitoBold,
   fontSize: sh15,
 };
 
-// TODO change to proper font
 export const fs15RegWhite: TextStyle = {
   color: colorWhite._1,
   fontFamily: NunitoRegular,
   fontSize: sh15,
 };
 
-// TODO change to proper font
 export const fs16BoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoBold,
   fontSize: sh16,
 };
 
-// TODO change to proper lineHeight
 export const fs16RegBlack4: TextStyle = {
   color: colorBlack._4,
   fontFamily: NunitoRegular,
   fontSize: sh16,
 };
 
-// TODO change to proper lineHeight
 export const fs16RegBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoRegular,
   fontSize: sh16,
 };
 
-// TODO change to proper font
 export const fs16SemiBoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoSemiBold,
   fontSize: sh16,
 };
 
-// TODO change to proper lineHeight
-export const fs16SemiBoldGrey6: TextStyle = {
-  color: colorGray._6,
+export const fs16SemiBoldGray8: TextStyle = {
+  color: colorGray._8,
   fontFamily: NunitoSemiBold,
   fontSize: sh16,
 };
 
-// TODO change to proper lineHeight
 export const fs16SemiBoldWhite: TextStyle = {
   color: colorWhite._1,
   fontFamily: NunitoSemiBold,
   fontSize: sh16,
 };
 
-// TODO change to proper lineHeight
 export const fs18SemiBoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoSemiBold,
   fontSize: sh18,
 };
 
-// TODO change to proper font
 export const fs16SemiBoldBlack4: TextStyle = {
   color: colorBlack._4,
   fontFamily: NunitoSemiBold,
   fontSize: sh16,
 };
 
-// TODO change to proper color
 export const fs24RegBlack: TextStyle = {
   color: colorBlack._3,
   fontFamily: NunitoRegular,
   fontSize: sh24,
 };
 
-// TODO change to proper font
 export const fs24RegBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoRegular,
   fontSize: sh24,
 };
 
-// TODO change to proper font
 export const fs24SemiBoldBlack: TextStyle = {
   color: colorBlack._1,
   fontFamily: NunitoSemiBold,
   fontSize: sh24,
 };
 
-// TODO change to proper lineHeight
 export const fs32SemiBoldBlack4: TextStyle = {
   color: colorBlack._4,
   fontFamily: NunitoSemiBold,
   fontSize: sh32,
 };
 
-// TODO change to proper font
 export const fs36SemiBoldBlack2: TextStyle = {
   color: colorBlack._2,
   fontFamily: NunitoSemiBold,

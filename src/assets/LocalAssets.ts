20cad4fef85b446169b457744797be1cf4cb71fe
Aldrin <aldrin.castro@regovtech.com>
2020-06-22 12:24:37 +0800

    updated LocalAssets

--- src/assets/LocalAssets.ts
+++ src/assets/LocalAssets.ts
@@ -2,27 +2,26 @@ import uploadICSmall from "./getting_started/document.png";
 import uploadICBig from "./getting_started/papers.png";
 import klView from "./kl_view/kl_view.png";
 import logoKenanga from "./logo_kenanga/logo_kenanga.png";
-import kib from "./logo_kib/logo_kib.png";
+import onboardingPeople from "./onboarding_people/onboarding_people.png";
 import riskAssessmentGraph1 from "./risk_assessment_graph_1/risk_assessment_graph_1.png";
 import riskAssessmentGraph2 from "./risk_assessment_graph_2/risk_assessment_graph_2.png";
-import onboardingPeople from "./onboarding_people/onboarding_people.png";
 
 export const LocalAssets = {
-  logo: {
-    kenanga: logoKenanga,
-  },
   login: {
     background: klView,
+  },
+  logo: {
+    kenanga: logoKenanga,
   },
   graph: {
     risk_assessment_graph_1: riskAssessmentGraph1,
     risk_assessment_graph_2: riskAssessmentGraph2,
   },
-  onboarding: {
-    people: onboardingPeople,
-  },
   newClient: {
     uploadICSmall: uploadICSmall,
     uploadICBig: uploadICBig,
+  },
+  onboarding: {
+    people: onboardingPeople,
   },
 };

6e8898d4d5a3adc243b5d7a58d7799c5292360d7
Aldrin <aldrin.castro@regovtech.com>
2020-06-23 14:43:01 +0800

    updated Slider component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,39 +1,36 @@
-import React, { Fragment, useState } from "react";
-import { View, ViewStyle, Text } from "react-native";
 import { Slider } from "@miblanchard/react-native-slider";
+import React, { Fragment, useState } from "react";
+import { Text, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
-import { CustomFlexSpacer } from "../../components";
-import { colorBlue, colorGray, flexRow, fs12BoldGray4, colorWhite, sh6, sh16, sh18, sw18 } from "../../styles";
+import { colorBlue, colorGray, colorWhite, flexRowSbSb, fs12BoldGray4, sh16, sh6, sw1, sw18 } from "../../styles";
 
 export interface SliderProps {
   disabled?: boolean;
   options: string[];
   setSelected: (result: string) => void;
-  // setValue: (value: number) => void;
-  // value: number;
 }
 
-export const SliderComponent = ({ disabled, options, setSelected }) => {
+export const CustomSlider = ({ disabled, options, setSelected }) => {
   const [value, setValue] = useState<number>(0);
-  const handleCalculation = (currentValue: number) => {
-    const index: number = Math.round(currentValue);
-    return options[index];
-  };
+
   const handleChange = (update: number[]) => {
     const current: number = update[0];
-    const result = handleCalculation(current);
-    setSelected(result);
+    const index = Math.round(current);
+    setValue(index);
+    setSelected(options[index]);
   };
+
   const handleSeek = (index: number) => {
     setValue(index);
     setSelected(options[index]);
   };
-  const thumbStyle: ViewStyle = { borderColor: colorWhite._1, borderWidth: 1, height: sh18, width: sw18 };
-  const labelStyle: ViewStyle = { ...flexRow };
+
+  const thumbStyle: ViewStyle = { borderColor: colorWhite._1, borderWidth: sw1, height: sw18, width: sw18 };
+
   return (
     <Fragment>
       <Slider
+        animateTransitions={true}
         containerStyle={{ height: sh16 }}
         disabled={disabled}
         maximumTrackTintColor={colorGray._9}
@@ -46,15 +43,12 @@ export const SliderComponent = ({ disabled, options, setSelected }) => {
         trackStyle={{ height: sh6 }}
         value={value}
       />
-      <View style={labelStyle}>
+      <View style={flexRowSbSb}>
         {options.map((option: string, index: number) => {
           return (
-            <Fragment key={index}>
-              <TouchableWithoutFeedback onPress={() => handleSeek(index)}>
-                <Text style={fs12BoldGray4}>{option}</Text>
-              </TouchableWithoutFeedback>
-              {index < options.length - 1 ? <CustomFlexSpacer /> : null}
-            </Fragment>
+            <TouchableWithoutFeedback key={index} onPress={() => handleSeek(index)}>
+              <Text style={fs12BoldGray4}>{option}</Text>
+            </TouchableWithoutFeedback>
           );
         })}
       </View>

8b46e254ee2b491235117ab5410bdf8df601df79
Aldrin <aldrin.castro@regovtech.com>
2020-06-23 14:43:22 +0800

    disabled yellowbox warning (should be removed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
-import { KeyboardAvoidingView, Platform } from "react-native";
+import { KeyboardAvoidingView, Platform, YellowBox } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SplashScreen from "react-native-splash-screen";
 
 import { RootNavigator } from "./Navigator";
 import { flexChild } from "./styles";
+
+// TODO to be removed
+// eslint-disable-next-line no-console
+console.disableYellowBox = true;
+YellowBox.ignoreWarnings(["Warning:"]);
 
 export const App = () => {
   useEffect(() => {

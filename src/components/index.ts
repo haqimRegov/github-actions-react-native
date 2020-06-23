ba36af2de5cd14b16065cc45290b0e12ea7302e5
Aldrin <aldrin.castro@regovtech.com>
2020-06-23 17:05:11 +0800

    added SideMenu component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./CommonPages";
 export * from "./Input";
 export * from "./Modals";
+export * from "./Nav";
 export * from "./RadioButton";
 export * from "./Steps";
 export * from "./Touchables";

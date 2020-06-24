9cf6c53d738423b78d758e76d715f06a2af387ec
Aldrin Castro <aldrin.castro@regovtech.com>
2020-06-24 16:19:20 +0800

    enhanced LabeledTitle

--- src/components/Views/LabeledTitle.tsx
+++ src/components/Views/LabeledTitle.tsx
@@ -2,19 +2,24 @@ import React, { Fragment } from "react";
 import { Text, TextStyle } from "react-native";
 
 import { fs12BoldBlack2, fs16RegBlack2 } from "../../styles";
+import { CustomSpacer } from "./Spacer";
 
 interface LabeledTitleProps {
   label: string;
   labelStyle?: TextStyle;
+  spaceToBottom?: number;
+  spaceToLabel?: number;
   title: string;
   titleStyle?: TextStyle;
 }
 
-export const LabeledTitle = ({ label, labelStyle, title, titleStyle }: LabeledTitleProps) => {
+export const LabeledTitle = ({ label, labelStyle, spaceToBottom, spaceToLabel, title, titleStyle }: LabeledTitleProps) => {
   return (
     <Fragment>
       <Text style={{ ...fs12BoldBlack2, ...labelStyle }}>{label}</Text>
+      {spaceToLabel === undefined ? null : <CustomSpacer space={spaceToLabel} />}
       <Text style={{ ...fs16RegBlack2, ...titleStyle }}>{title}</Text>
+      {spaceToBottom === undefined ? null : <CustomSpacer space={spaceToBottom} />}
     </Fragment>
   );
 };

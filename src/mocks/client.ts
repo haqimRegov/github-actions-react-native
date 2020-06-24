82c75441c222f9a443b121371a719e81244125a3
Aldrin Castro <aldrin.castro@regovtech.com>
2020-06-24 16:18:28 +0800

    added mock data folder

---
+++ src/mocks/client.ts
@@ -0,0 +1,7 @@
+export const SAMPLE_CLIENT = {
+  address: "F-7-15, Block B, Lucky Gardens, Jalan Telawi, Bangsar, Kuala Lumpur, 46001",
+  dateOfBirth: "14 August 1988",
+  gender: "Male",
+  id: "123 4567 8901",
+  name: "Edgar Constantine",
+};

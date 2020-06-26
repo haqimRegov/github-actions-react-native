8fba4fa42820aec291f92b9e577c463a54b0579f
Nithin <johnnithin08@gmail.com>
2020-06-26 15:00:46 +0800

    confirmation page update

--- src/pages/Onboarding/Confirmation.tsx
+++ src/pages/Onboarding/Confirmation.tsx
@@ -18,41 +18,30 @@ import {
   sh43,
   sh56,
   sh8,
+  sh5,
+  sh05,
+  sh20,
+  sw05,
   sw24,
   sw96,
 } from "../../styles";
 
-const {
-  BUTTON_AGREE,
-  BUTTON_CANCEL,
-  HEADING,
-  SIGNATURE_AGENT,
-  SIGNATURE_CLIENT,
-  SUB_HEADING,
-  TERMS_CONTENT_1,
-  TERMS_CONTENT_2,
-  TERMS_CONTENT_3,
-  TERMS_CONTENT_4,
-  TERMS_CONTENT_5,
-  TERMS_CONTENT_6,
-  TERMS_CONTENT_7,
-  TERMS_HEADING_1,
-  TERMS_HEADING_2,
-  TERMS_HEADING_3,
-  TERMS_HEADING_4,
-  TERMS_HEADING_5,
-  TERMS_HEADING_6,
-  TERMS_HEADING_7,
-} = Language.PAGE.CONFIRMATION;
+const { CONFIRMATION } = Language.PAGE;
 
+interface AccordionContentProps {
+  text: string;
+}
+const AccordionContent = ({ text }: AccordionContentProps) => {
+  return <Text>{text}</Text>;
+};
 const SECTIONS = [
-  { title: TERMS_HEADING_1, content: TERMS_CONTENT_1 },
-  { title: TERMS_HEADING_2, content: TERMS_CONTENT_2 },
-  { title: TERMS_HEADING_3, content: TERMS_CONTENT_3 },
-  { title: TERMS_HEADING_4, content: TERMS_CONTENT_4 },
-  { title: TERMS_HEADING_5, content: TERMS_CONTENT_5 },
-  { title: TERMS_HEADING_6, content: TERMS_CONTENT_6 },
-  { title: TERMS_HEADING_7, content: TERMS_CONTENT_7 },
+  { title: CONFIRMATION.TERMS_HEADING_1, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_1} /> },
+  { title: CONFIRMATION.TERMS_HEADING_2, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_2} /> },
+  { title: CONFIRMATION.TERMS_HEADING_3, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_3} /> },
+  { title: CONFIRMATION.TERMS_HEADING_4, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_4} /> },
+  { title: CONFIRMATION.TERMS_HEADING_5, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_5} /> },
+  { title: CONFIRMATION.TERMS_HEADING_6, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_6} /> },
+  { title: CONFIRMATION.TERMS_HEADING_7, content: <AccordionContent text={CONFIRMATION.TERMS_CONTENT_7} /> },
 ];
 
 interface ConfirmationProps {
@@ -67,28 +56,25 @@ export const Confirmation = ({ handleNextStep }: ConfirmationProps) => {
     Alert.alert("Cancel");
   };
   const handleAgree = () => {
-    Alert.alert("Agree");
-  };
-  const handleSubmit = () => {
     handleNextStep(ONBOARDING_ROUTES.Payment);
   };
-  const divider: ViewStyle = { borderTopColor: colorBlack._6, borderTopWidth: 0.5, ...flexChild, opacity: 0.1 };
+  const divider: ViewStyle = { borderTopColor: colorBlack._6_3, borderTopWidth: sw05, ...flexChild };
   const sectionContainer: ViewStyle = {
     backgroundColor: colorWhite._1,
     borderRadius: 10,
-    ...customShadow(colorGray._4, 5, 0, 0.5, 20),
+    ...customShadow(colorGray._4, sh5, 0, sh05, sh20),
     ...flexChild,
     ...px(sw24),
   };
   const touchableContainer: ViewStyle = { ...centerHorizontal, height: sh56 };
   return (
     <ContentPage
-      heading={HEADING}
+      heading={CONFIRMATION.HEADING}
       handleLeftButton={handleCancel}
       handleRightButton={handleAgree}
-      leftButtonText={BUTTON_CANCEL}
-      rightButtonText={BUTTON_AGREE}
-      subHeading={SUB_HEADING}>
+      leftButtonText={CONFIRMATION.BUTTON_CANCEL}
+      rightButtonText={CONFIRMATION.BUTTON_AGREE}
+      subHeading={CONFIRMATION.SUB_HEADING}>
       <View style={{ ...px(sw96) }}>
         <BasicAccordion sections={SECTIONS} />
       </View>
@@ -97,13 +83,13 @@ export const Confirmation = ({ handleNextStep }: ConfirmationProps) => {
       <View style={{ ...px(sw96) }}>
         <View style={sectionContainer}>
           <TouchableWithoutFeedback onPress={handleSignature} style={touchableContainer}>
-            <Text style={fs16RegBlue1}>{SIGNATURE_CLIENT}</Text>
+            <Text style={fs16RegBlue1}>{CONFIRMATION.SIGNATURE_CLIENT}</Text>
           </TouchableWithoutFeedback>
         </View>
         <CustomSpacer space={sh8} />
         <View style={sectionContainer}>
           <TouchableWithoutFeedback onPress={handleSignature} style={touchableContainer}>
-            <Text style={fs16RegBlue1}>{SIGNATURE_AGENT}</Text>
+            <Text style={fs16RegBlue1}>{CONFIRMATION.SIGNATURE_AGENT}</Text>
           </TouchableWithoutFeedback>
         </View>
       </View>

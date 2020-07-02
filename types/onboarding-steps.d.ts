94ebe40d3dc8b93a2fd19885ccf54b038819dab7
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-02 15:12:56 +0800

    updated onboarding step types

--- types/onboarding-steps.d.ts
+++ types/onboarding-steps.d.ts
@@ -1,12 +1,27 @@
-declare interface RenderContentProps {
+declare interface IContentItem {
+  title: string;
+  route: string;
+}
+
+declare interface IOnboarding {
+  content?: IContentItem[];
+  label: string;
+  route?: string;
+}
+
+declare interface OnboardingContentProps {
   handleNextStep: (nextRoute: string) => void;
 }
 
 declare interface OnboardingStepsProps {
   activeContent?: IContentItem | IOnboarding;
   activeSection: number;
+  collapse?: boolean;
   handleContentChange: (item: IContentItem | IOnboarding) => void;
-  RenderContent: (props: RenderContentProps) => JSX.Element;
+  onPressBackdrop?: () => void;
+  onPressExpand?: () => void;
+  overlay?: boolean;
+  RenderContent: (props: OnboardingContentProps) => JSX.Element;
   setActiveContent: (content: IContentItem | IOnboarding) => void;
   setActiveSection: (section: any) => void;
   setFinishedStep: (step: number[]) => void;

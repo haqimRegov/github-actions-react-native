8a8564bf4d919e7ef8eeb7ddfde5d35926acaa35
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-03 14:59:37 +0800

    sort navigator

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -12,9 +12,9 @@ export const RootNavigator = () => {
   return (
     <NavigationContainer>
       <Navigator initialRouteName="Login" headerMode="none">
+        <Screen name="AddClient" component={AddClientPage} />
         <Screen name="Login" component={LoginPage} />
         <Screen name="Onboarding" component={OnboardingPage} />
-        <Screen name="AddClient" component={AddClientPage} />
         <Screen name="RiskAppetite" component={RiskAppetitePage} />
       </Navigator>
     </NavigationContainer>

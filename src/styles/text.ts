efcae24876f46d272b7f5a8dfe69ada3cda80d73
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-03 16:07:45 +0800

    updated texts

--- src/styles/text.ts
+++ src/styles/text.ts
@@ -2,7 +2,7 @@ import { TextStyle } from "react-native";
 
 import { NunitoBold, NunitoRegular, NunitoSemiBold } from "../constants/fonts";
 import { colorBlack, colorBlue, colorGray, colorWhite } from "./colors";
-import { sh12, sh13, sh15, sh16, sh18, sh24, sh32, sh36 } from "./sizes";
+import { sh12, sh13, sh14, sh15, sh16, sh18, sh24, sh32, sh36 } from "./sizes";
 
 export const fsAlignCenter: TextStyle = { textAlign: "center" };
 export const fsUppercase: TextStyle = {
@@ -69,9 +69,9 @@ export const fs13RegBlack2: TextStyle = {
   fontSize: sh13,
 };
 
-export const fs13SemiBoldBlue2: TextStyle = {
-  color: colorBlue._2,
-  fontFamily: NunitoSemiBold,
+export const fs13RegBlue: TextStyle = {
+  color: colorBlue._1,
+  fontFamily: NunitoRegular,
   fontSize: sh13,
 };
 
@@ -87,51 +87,14 @@ export const fs14RegularBlack6: TextStyle = {
   fontSize: sh14,
 };
 
-export const fs15BoldBlack4: TextStyle = {
-  color: colorBlack._4,
-  fontFamily: NunitoBold,
-  fontSize: sh15,
-};
-
 export const fs15BoldWhite: TextStyle = {
   color: colorWhite._1,
   fontFamily: NunitoBold,
   fontSize: sh15,
 };
 
-
-export const fs15RegWhite: TextStyle = {
-  color: colorWhite._1,
-  fontFamily: NunitoRegular,
-  fontSize: sh15,
-};
-
-export const fs15SemiBoldBlack2: TextStyle = {
-  color: colorBlack._2,
-  fontFamily: NunitoSemiBold,
-  fontSize: sh15,
-};
-
-export const fs15SemiBoldWhite: TextStyle = {
-  color: colorWhite._1,
-  fontFamily: NunitoSemiBold,
-  fontSize: sh15,
-};
-
 export const fs16BoldBlack2: TextStyle = {
   color: colorBlack._2,
-  fontFamily: NunitoBold,
-  fontSize: sh16,
-};
-
-export const fs16BoldBlack2: TextStyle = {
-  color: colorBlack._2,
-  fontFamily: NunitoBold,
-  fontSize: sh16,
-};
-
-export const fs16BoldBlack4: TextStyle = {
-  color: colorBlack._4,
   fontFamily: NunitoBold,
   fontSize: sh16,
 };
@@ -141,7 +104,6 @@ export const fs16BoldWhite: TextStyle = {
   fontFamily: NunitoBold,
   fontSize: sh16,
 };
-
 
 export const fs16RegBlack2: TextStyle = {
   color: colorBlack._2,
@@ -169,6 +131,12 @@ export const fs16SemiBoldBlack2: TextStyle = {
 
 export const fs16SemiBoldBlack4: TextStyle = {
   color: colorBlack._4,
+  fontFamily: NunitoSemiBold,
+  fontSize: sh16,
+};
+
+export const fs16SemiBoldBlack6: TextStyle = {
+  color: colorBlack._6,
   fontFamily: NunitoSemiBold,
   fontSize: sh16,
 };

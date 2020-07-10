0e648e0db98419e96da8012238626e872c78165d
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-10 13:15:23 +0800

    updated LabeledTitle component

--- src/components/Views/LabeledTitle.tsx
+++ src/components/Views/LabeledTitle.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TextStyle, View } from "react-native";
+import { Text, TextStyle, View, ViewStyle } from "react-native";
 
 import { fs12BoldBlack2, fs16RegBlack2 } from "../../styles";
 import { CustomSpacer } from "./Spacer";
@@ -9,13 +9,14 @@ interface LabeledTitleProps {
   labelStyle?: TextStyle;
   spaceToBottom?: number;
   spaceToLabel?: number;
+  style?: ViewStyle;
   title: string;
   titleStyle?: TextStyle;
 }
 
-export const LabeledTitle = ({ label, labelStyle, spaceToBottom, spaceToLabel, title, titleStyle }: LabeledTitleProps) => {
+export const LabeledTitle = ({ label, labelStyle, spaceToBottom, spaceToLabel, style, title, titleStyle }: LabeledTitleProps) => {
   return (
-    <View>
+    <View style={style}>
       <Text style={{ ...fs12BoldBlack2, ...labelStyle }}>{label}</Text>
       {spaceToLabel === undefined ? null : <CustomSpacer space={spaceToLabel} />}
       <Text style={{ ...fs16RegBlack2, ...titleStyle }}>{title}</Text>

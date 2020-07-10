f71c3d2d3bd230e2754e6356289fcea72a647982
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-10 13:17:36 +0800

    implemented ActionButtons in ConfirmationModal

--- src/components/Modals/Confirmation.tsx
+++ src/components/Modals/Confirmation.tsx
@@ -2,34 +2,39 @@ import React, { Fragment } from "react";
 import { Text, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 
 import { centerHV, colorGray, colorWhite, flexRow, fs16SemiBoldBlack4, px, py, sh24, sw24, sw5 } from "../../styles";
-import { CustomButtonProps, RoundedButton } from "../Touchables";
-import { CustomFlexSpacer, CustomSpacer } from "../Views";
+import { ActionButtons, ActionButtonsProps, CustomFlexSpacer, CustomSpacer } from "../Views";
 import { BasicModal } from "./Basic";
 
-interface ModalProps {
-  buttons: CustomButtonProps[];
+interface ModalProps extends ActionButtonsProps {
   children: JSX.Element;
   handleClose: () => void;
   title: string;
   visible: boolean;
 }
 
-export const ConfirmationModal = ({ buttons, children, title, visible, handleClose }: ModalProps) => {
+export const ConfirmationModal = ({ children, title, visible, handleClose, ...rest }: ModalProps) => {
   const modelContainer: ViewStyle = {
     backgroundColor: colorGray._2,
     borderRadius: sw5,
   };
+
   const padding: ViewStyle = {
     ...py(sh24),
     ...px(sw24),
   };
+
   const buttonContainer: ViewStyle = {
-    ...flexRow,
     ...padding,
     backgroundColor: colorWhite._1,
     borderBottomLeftRadius: sw5,
     borderBottomRightRadius: sw5,
   };
+
+  const actionButtonProps: ActionButtonsProps = {
+    ...rest,
+    buttonContainerStyle: buttonContainer,
+  };
+
   return (
     <BasicModal visible={visible}>
       <Fragment>
@@ -46,17 +51,7 @@ export const ConfirmationModal = ({ buttons, children, title, visible, handleClo
               <CustomSpacer space={sh24} />
               {children}
             </View>
-            <View style={buttonContainer}>
-              {buttons.map((button: CustomButtonProps, index: number) => {
-                const space = index === 0 ? 0 : sw24;
-                return (
-                  <Fragment key={index}>
-                    <CustomSpacer isHorizontal={true} space={space} />
-                    <RoundedButton {...button} />
-                  </Fragment>
-                );
-              })}
-            </View>
+            <ActionButtons {...actionButtonProps} />
           </View>
         </View>
       </Fragment>

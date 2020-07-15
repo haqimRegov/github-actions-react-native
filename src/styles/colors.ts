ad15c951808deb80e3bacaec9f2ca7b89e0afdcb
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-15 18:58:20 +0800

    updated to final colors (old colors are commented for reference)

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,42 +2,58 @@ export const colorTransparent = "rgba(0,0,0,0)";
 
 export const colorBlack = {
   _1: "#000000",
-  _2: "#4d4d4d",
-  _3: "#515151",
-  _4: "#202020",
-  _5: "#0a1f44",
-  _6: "#333333",
-  _6_1: "rgba(51,51,51,0.1)",
-  _6_3: "rgba(51,51,51,0.3)",
-  _7: "#3c3c3e",
+  _2: "#333333",
+  _2_1: "rgba(51,51,51,0.1)",
+  // old color for reference
+  // _1: "#000000",
+  // _2: "#4d4d4d",
+  // _3: "#515151",
+  // _4: "#202020",
+  // _5: "#0a1f44",
+  // _6: "#333333",
+  // _6_1: "rgba(51,51,51,0.1)",
+  // _6_3: "rgba(51,51,51,0.3)",
+  // _7: "#3c3c3e",
 };
 
 export const colorBlue = {
   _1: "#0091ff",
-  _2: "#0D55CF",
-  _3: "#007fff",
-  _4: "#377bff",
-  _5: "#0072ff",
-  _6: "#0676ed",
-  _7: "#4b74ff",
+  _2: "#002043",
+  _2_5: "rgba(0, 32, 67, 0.5)",
+  _3: "rgba(42, 54, 90, 0.8)",
+  // old colors for reference
+  // _1: "#0091ff",
+  // _2: "#0D55CF",
+  // _3: "#007fff",
+  // _4: "#377bff",
+  // _5: "#0072ff",
+  // _6: "#0676ed",
+  // _7: "#4b74ff",
 };
 
 export const colorGray = {
-  _1: "#a6aebc",
-  _2: "#e1e4e8",
-  _3: "#f2f4f6",
-  _4: "#8f9bb3",
-  _5: "#c8c9cc",
-  _6: "#dae1ed",
-  _7: "#fafcfd",
-  _8: "#9e9e9e",
-  _9: "#e9edf4",
-  _10: "#c6cbd4",
-  _11: "#6d7278",
+  _1: "#E1E4E8",
+  _2: "#EDF0F4",
+  _3: "#C6CBD4",
+  _4: "#EAEBEE",
+  _5: "#F4F4F4",
+  _6: "#A3A3A3",
+  _7: "#C9C9CD",
+  // old colors for reference
+  // _1: "#a6aebc",
+  // _2: "#e1e4e8",
+  // _4: "#8f9bb3",
+  // _5: "#c8c9cc",
+  // _6: "#dae1ed",
+  // _7: "#fafcfd",
+  // _8: "#9e9e9e",
+  // _9: "#e9edf4",
+  // _10: "#c6cbd4",
+  // _11: "#6d7278",
 };
 
-export const colorGreen = {
-  _1: "#6aa130",
+export const colorMint = {
+  _1: "#2CD9C5",
 };
 
 export const colorRed = {
@@ -46,5 +62,10 @@ export const colorRed = {
 
 export const colorWhite = {
   _1: "#FFFFFF",
-  _2: "#fdfefe",
+  _2: "#FDFEFE",
+  _3: "#FBFBFB",
+};
+
+export const colorYellow = {
+  _1: "#F6EDD9",
 };

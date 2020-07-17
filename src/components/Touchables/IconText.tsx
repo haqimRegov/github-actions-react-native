959709f940970caeb87bab49d0b407e751d981c0
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-17 23:47:16 +0800

    updated IconText to be just IconText

--- src/components/Touchables/IconText.tsx
+++ src/components/Touchables/IconText.tsx
@@ -2,30 +2,28 @@ import React from "react";
 import { Text, TextStyle, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 
 import { IcoMoon } from "../../icons";
-import { border, centerVertical, colorBlack, flexRow, fs12SemiBoldBlack2, px, sh16, sh32, sw1, sw10, sw156, sw20, sw5 } from "../../styles";
+import { centerVertical, colorBlack, flexRow, fs12SemiBoldBlack2, sh16, sw8 } from "../../styles";
 import { CustomSpacer } from "../Views";
-import { OutlineButtonProps } from "./OutlineButton";
 
-interface IconTextProps extends OutlineButtonProps {
+interface IconTextProps {
   color?: string;
+  iconPosition?: "left" | "right";
+  iconSize?: number;
   name: string;
-  position?: "left" | "right";
+  onPress?: () => void;
   spaceBetween?: number;
-  size?: number;
+  style?: ViewStyle;
+  text: string;
+  textStyle?: TextStyle;
 }
 
-export const IconText = ({ buttonStyle, color, disabled, name, onPress, position, size, spaceBetween, text, textStyle }: IconTextProps) => {
-  const flexDirection: ViewStyle = position === "right" ? { flexDirection: "row-reverse" } : flexRow;
+export const IconText = ({ color, iconPosition, iconSize, name, onPress, spaceBetween, style, text, textStyle }: IconTextProps) => {
+  const flexDirection: ViewStyle = iconPosition === "right" ? { flexDirection: "row-reverse" } : flexRow;
 
   const defaultButtonStyle: ViewStyle = {
-    ...border(colorBlack._2, sw1, sw20),
     ...centerVertical,
     ...flexDirection,
-    ...px(sw10),
-    height: sh32,
-    opacity: disabled === true ? 0.5 : 1,
-    width: sw156,
-    ...buttonStyle,
+    ...style,
   };
   const textColor = color ? { color: color } : {};
   const defaultTextStyle: TextStyle = {
@@ -34,11 +32,12 @@ export const IconText = ({ buttonStyle, color, disabled, name, onPress, position
     ...textStyle,
   };
 
-  const defaultSpaceBetween = spaceBetween !== undefined ? spaceBetween : sw5;
-  const defaultSize = size !== undefined ? size : sh16;
+  const defaultSpaceBetween = spaceBetween !== undefined ? spaceBetween : sw8;
+  const defaultSize = iconSize !== undefined ? iconSize : sh16;
   const defaultColor = color !== undefined ? color : colorBlack._2;
+
   return (
-    <TouchableWithoutFeedback onPress={disabled === true ? undefined : onPress}>
+    <TouchableWithoutFeedback onPress={onPress}>
       <View style={defaultButtonStyle}>
         <IcoMoon color={defaultColor} name={name} size={defaultSize} />
         <CustomSpacer isHorizontal={true} space={defaultSpaceBetween} />

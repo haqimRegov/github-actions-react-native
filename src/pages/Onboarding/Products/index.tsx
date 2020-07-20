3cdaade343ed24c2a907324f85e36a45901bfbe7
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-20 17:22:25 +0800

    implemented new product list and confirmation, added BottomFixedDetails

--- src/pages/Onboarding/Products/index.tsx
+++ src/pages/Onboarding/Products/index.tsx
@@ -1,150 +1,39 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
-import { Alert, ScrollView, View, ViewStyle } from "react-native";
+import { View } from "react-native";
 
-import { CustomFlexSpacer, CustomSpacer, CustomTableV2, Pagination, Tab } from "../../../components";
-import { Language } from "../../../constants";
-import { SAMPLE_PRODUCTS } from "../../../mocks/products";
-import {
-  colorWhite,
-  flexChild,
-  flexGrow,
-  flexRow,
-  sh15,
-  sh153,
-  sh16,
-  shadow5,
-  sw102,
-  sw109,
-  sw234,
-  sw24,
-  sw72,
-  sw83,
-  sw90,
-} from "../../../styles";
-import { ProductGraph } from "./Graph";
-import { ProductHeader } from "./Header";
-import { ProductOptions } from "./Options";
+import { BottomFixedDetails, SafeAreaPage } from "../../../components";
+import { Language, ONBOARDING_ROUTES } from "../../../constants";
+import { SAMPLE_PRODUCTS } from "../../../mocks";
+import { flexChild, flexCol } from "../../../styles";
+import { ProductConfirmation } from "./Confirmation";
+import { ProductList } from "./ProductList";
 
-const { PRODUCT_LIST } = Language.PAGE;
+const { PRODUCT_CONFIRMATION } = Language.PAGE;
 
-export const Products: FunctionComponent = () => {
-  const [activeAccordion, setActiveAccordion] = useState<number[]>([]);
-  const [allFunds, setAllFunds] = useState<boolean>(false);
-  const [filter, setFilter] = useState<boolean>(true);
-  const [inputSearch, setInputSearch] = useState<string>("");
-  const [productList, setProductList] = useState<ITableData[]>([]);
-  const [selectedProduct, setSelectedProduct] = useState<ITableData[]>([]);
+interface ProductsProps {
+  handleNextStep: (route: string) => void;
+}
 
-  const handleFilter = () => {
-    setFilter(!filter);
+export const Products: FunctionComponent<ProductsProps> = ({ handleNextStep }: ProductsProps) => {
+  const [productConfirmShow, setProductConfirmShow] = useState<boolean>(false);
+  const [fixedBottomShow, setFixedBottomShow] = useState<boolean>(true);
+
+  const [productList, setProductList] = useState<IProduct[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<IProduct[]>([]);
+
+  const handleCancel = () => {
+    return productConfirmShow ? setProductConfirmShow(false) : setSelectedProduct([]);
   };
 
-  const handleNext = () => {
-    Alert.alert("handlePress");
-  };
-
-  const handlePrev = () => {
-    Alert.alert("handlePress");
-  };
-
-  const handleRecommendedFunds = () => {
-    setAllFunds(false);
-  };
-
-  const handleAllFunds = () => {
-    setAllFunds(true);
-  };
-
-  const handleSelectProduct = (product: ITableData[]) => {
-    let newSelected = product === productList ? product : [...selectedProduct];
-    if (product.length === 0) {
-      newSelected = [];
+  const handleSubmit = () => {
+    if (productConfirmShow === true) {
+      handleNextStep(ONBOARDING_ROUTES.PersonalDetails);
     }
-    if (product.length === 1) {
-      const sectionIndex = newSelected.indexOf(product[0]);
-      if (sectionIndex > -1) {
-        newSelected.splice(sectionIndex, 1);
-      } else {
-        newSelected.push(product[0]);
-      }
-    }
-    setSelectedProduct(newSelected);
+    setProductConfirmShow(true);
   };
 
-  const handleShowPerformance = (item: IColumnItemAccordion) => {
-    const newSections: number[] = [...activeAccordion];
-    const sectionIndex = newSections.indexOf(item.index);
-    if (sectionIndex > -1) {
-      newSections.splice(sectionIndex, 1);
-    } else {
-      newSections.push(item.index);
-    }
-    setActiveAccordion(newSections);
-  };
-
-  const columns: ITableColumn[] = [
-    {
-      key: "name",
-      viewStyle: {
-        width: sw234,
-      },
-      title: PRODUCT_LIST.LABEL_COLUMN_NAME,
-    },
-    {
-      icon: {
-        name: "caret-down",
-      },
-      key: "type",
-      viewStyle: {
-        width: sw83,
-      },
-      title: PRODUCT_LIST.LABEL_COLUMN_PRODUCT,
-    },
-    {
-      icon: {
-        name: "caret-down",
-      },
-      key: "risk",
-      viewStyle: {
-        width: sw102,
-      },
-      title: PRODUCT_LIST.LABEL_COLUMN_RISK,
-    },
-    {
-      icon: {
-        name: "caret-down",
-      },
-      key: "epf",
-      viewStyle: {
-        width: sw72,
-      },
-      title: PRODUCT_LIST.LABEL_COLUMN_EPF,
-    },
-    {
-      icon: {
-        name: "caret-down",
-      },
-      key: "shariah",
-      viewStyle: {
-        width: sw90,
-      },
-      title: PRODUCT_LIST.LABEL_COLUMN_SHARIAH,
-    },
-    {
-      icon: {
-        name: "caret-down",
-      },
-      key: "performance",
-      viewStyle: {
-        width: sw109,
-      },
-      title: PRODUCT_LIST.LABEL_COLUMN_PERFORMANCE,
-      onPressItem: handleShowPerformance,
-      withAccordion: true,
-    },
-  ];
-
-  const tableContainer: ViewStyle = { backgroundColor: colorWhite._2, borderBottomRightRadius: sw24, borderBottomLeftRadius: sw24 };
+  const amount = selectedProduct.length * 1000;
+  const LABEL_FUND = selectedProduct.length === 1 ? PRODUCT_CONFIRMATION.LABEL_FUND_SELECTION : PRODUCT_CONFIRMATION.LABEL_FUNDS_SELECTION;
 
   useEffect(() => {
     // TODO integration
@@ -152,42 +41,33 @@ export const Products: FunctionComponent = () => {
   }, []);
 
   return (
-    <ScrollView contentContainerStyle={flexGrow} keyboardShouldPersistTaps="handled" scrollEnabled={filter}>
-      <View style={{ ...flexChild }}>
-        <ProductHeader
-          filter={filter}
-          handleFilter={handleFilter}
-          handleNext={handleNext}
-          handlePrev={handlePrev}
-          inputSearch={inputSearch}
-          setInputSearch={setInputSearch}
-        />
-        <View style={{ ...shadow5, margin: sw24, backgroundColor: colorWhite._1, borderRadius: sw24 }}>
-          <CustomSpacer space={sh153} />
-          <CustomSpacer space={sh16} />
-          <View style={{ ...flexRow }}>
-            <Tab onPress={handleRecommendedFunds} selected={!allFunds} text={PRODUCT_LIST.LABEL_RECOMMENDED} />
-            <Tab onPress={handleAllFunds} selected={allFunds} text={PRODUCT_LIST.LABEL_ALL_FUNDS} />
-            <CustomFlexSpacer />
-            <Pagination onPressNext={handleNext} onPressPrev={handlePrev} page={1} totalItems={36} itemsPerPage={20} />
-            <CustomSpacer isHorizontal={true} space={sw24} />
-          </View>
-          <CustomSpacer space={sh15} />
-          <View style={tableContainer}>
-            <CustomTableV2
-              activeAccordion={activeAccordion}
-              columns={columns}
-              data={productList}
-              rowSelection={selectedProduct}
-              onRowSelect={handleSelectProduct}
-              RenderAccordion={(item: ITableData) => <ProductGraph item={item} />}
-              RenderOptions={(props: ITableOptions) => <ProductOptions {...props} handleShowPerformance={handleShowPerformance} />}
-              rowSelectionLabel={PRODUCT_LIST.LABEL_COLUMN_BUY}
+    <SafeAreaPage>
+      <View style={flexChild}>
+        {productConfirmShow === true && selectedProduct.length !== 0 ? (
+          <ProductConfirmation
+            selectedProduct={selectedProduct}
+            setFixedBottomShow={setFixedBottomShow}
+            setProductConfirmShow={setProductConfirmShow}
+            setSelectedProduct={setSelectedProduct}
+          />
+        ) : (
+          <ProductList productList={productList} selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct} />
+        )}
+        {fixedBottomShow === true && selectedProduct.length !== 0 ? (
+          <View style={flexCol}>
+            <BottomFixedDetails
+              amount={amount.toString()}
+              cancelOnPress={handleCancel}
+              fundAmountText={PRODUCT_CONFIRMATION.LABEL_FUND_SELECTION_AMOUNT}
+              fundSelectionText={LABEL_FUND}
+              labelCancel={PRODUCT_CONFIRMATION.BUTTON_CANCEL}
+              labelSubmit={PRODUCT_CONFIRMATION.BUTTON_START_INVESTING}
+              numberOfFunds={selectedProduct.length}
+              submitOnPress={handleSubmit}
             />
-            <CustomSpacer space={sh15} />
           </View>
-        </View>
+        ) : null}
       </View>
-    </ScrollView>
+    </SafeAreaPage>
   );
 };

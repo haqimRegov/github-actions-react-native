11dd743aaf0ef5de393522f1355c98d37e61e770
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-21 13:06:45 +0800

    updated question style

--- src/components/Views/Question.tsx
+++ src/components/Views/Question.tsx
@@ -1,45 +1,31 @@
 import React, { Fragment, ReactNode } from "react";
-import { TextStyle, View } from "react-native";
+import { View } from "react-native";
 
 import { LabeledTitle } from "../../components/Views/LabeledTitle";
-import { flexRow, sh16 } from "../../styles";
+import { flexRow, fs10BoldBlack2, fs16SemiBoldBlack2, sh16, sw608 } from "../../styles";
 import { RadioButtonGroup } from "../RadioButton/RadioButtonGroup";
 import { CustomFlexSpacer, CustomSpacer } from "./Spacer";
 
 export interface QuestionContentProps {
-  contentOptions?: string[];
-  selected: string;
-  setSelectedOption: (name: string) => void;
-}
-export interface QuestionProps {
-  label: string;
   options?: string[];
-  RenderContent?: (props: QuestionContentProps) => JSX.Element;
-  right?: ReactNode;
   selected: string;
   setSelected: (name: string) => void;
+}
+export interface QuestionProps extends QuestionContentProps {
+  label: string;
+  RenderContent?: (props: QuestionContentProps) => JSX.Element;
+  right?: ReactNode;
   spaceToContent?: number;
   title: string;
-  titleStyle?: TextStyle;
 }
 
-export const Question = ({
-  label,
-  options,
-  RenderContent,
-  right,
-  selected,
-  setSelected,
-  spaceToContent,
-  title,
-  titleStyle,
-}: QuestionProps) => {
+export const Question = ({ label, options, RenderContent, right, selected, setSelected, spaceToContent, title }: QuestionProps) => {
   return (
     <Fragment>
-      <LabeledTitle label={label} title={title} titleStyle={titleStyle} />
+      <LabeledTitle label={label} labelStyle={fs10BoldBlack2} title={title} titleStyle={{ ...fs16SemiBoldBlack2, maxWidth: sw608 }} />
       <CustomSpacer space={spaceToContent === undefined ? sh16 : spaceToContent} />
       {RenderContent !== undefined ? (
-        <RenderContent contentOptions={options} selected={selected} setSelectedOption={setSelected} />
+        <RenderContent options={options} selected={selected} setSelected={setSelected} />
       ) : (
         <View style={flexRow}>
           {options !== undefined ? (

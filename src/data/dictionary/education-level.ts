00fc2af6e0b85141f926670296435325d6911f69
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-23 13:56:16 +0800

    added data dictionary for education and income

---
+++ src/data/dictionary/education-level.ts
@@ -0,0 +1,14 @@
+export const DICTIONARY_EDUCATION = [
+  {
+    value: "Bachelors",
+  },
+  {
+    value: "Higher Secondary",
+  },
+  {
+    value: "Masters",
+  },
+  {
+    value: "PhD",
+  },
+];

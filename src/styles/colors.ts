ec2033c3a204bac2a0431f007baffbafa54e368e
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-23 14:00:14 +0800

    updated colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,6 +4,7 @@ export const colorBlack = {
   _1: "#000000",
   _2: "#333333",
   _2_1: "rgba(51,51,51,0.1)",
+  _2_5: "rgba(51,51,51,0.5)",
   // old color for reference
   // _1: "#000000",
   // _2: "#4d4d4d",

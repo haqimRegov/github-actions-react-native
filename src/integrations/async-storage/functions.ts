4f2a5d3e63b4d3a734984ab093694b3736755714
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-27 17:27:37 +0800

    added async storage functions

---
+++ src/integrations/async-storage/functions.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-community/async-storage";
+import { Alert } from "react-native";
+
+export const updateStorageData = async (key: string, value: string | number | boolean | object) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    Alert.alert("Error", error);
+  }
+};
+
+export const getStorageData = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      return JSON.parse(value);
+    }
+  } catch (error) {
+    Alert.alert("Error", error);
+  }
+
+  return false;
+};
+
+export const clearStorage = async () => {
+  try {
+    await AsyncStorage.clear();
+  } catch (error) {
+    Alert.alert("Error", error);
+  }
+};

15d8bcfbc03c8c61394b947c9e916c5761c62a0a
Aldrin Castro <aldrin.castro@regovtech.com>
2020-07-30 16:18:18 +0800

    added integrations for react-native-document-picker

---
+++ src/integrations/react-native-document-picker/functions.ts
@@ -0,0 +1,36 @@
+import DocumentPicker, { DocumentPickerResponse } from "react-native-document-picker";
+
+type SuccessCallback = (result: DocumentPickerResponse) => void;
+
+export const documentPicker = async (handleSuccess: SuccessCallback) => {
+  try {
+    const res = await DocumentPicker.pick({
+      type: [DocumentPicker.types.pdf, DocumentPicker.types.images],
+    });
+
+    return handleSuccess(res);
+  } catch (err) {
+    if (DocumentPicker.isCancel(err)) {
+      // User cancelled the picker, exit any dialogs or menus and move on
+    } else {
+      throw err;
+    }
+  }
+  return false;
+};
+export const multipleDocumentPicker = async () => {
+  // Pick multiple files
+  try {
+    const results = await DocumentPicker.pickMultiple({
+      type: [DocumentPicker.types.images],
+    });
+    return results;
+  } catch (err) {
+    if (DocumentPicker.isCancel(err)) {
+      // User cancelled the picker, exit any dialogs or menus and move on
+    } else {
+      throw err;
+    }
+  }
+  return false;
+};

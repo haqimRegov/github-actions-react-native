96ee0f994f33e3b43b4c12589e4e9ff67692678a
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-03 15:24:08 +0800

    added id type data dictionary

---
+++ src/data/dictionary/id-type.ts
@@ -0,0 +1,2 @@
+export const DICTIONARY_ID_TYPE: TypeIDChoices[] = ["NRIC", "Passport", "Other"];
+export const DICTIONARY_ID_OTHER_TYPE = [{ value: "Army" }, { value: "Police" }];
--- src/data/dictionary/index.ts
+++ src/data/dictionary/index.ts
@@ -1,4 +1,5 @@
 export * from "./business-nature";
 export * from "./education-level";
+export * from "./id-type";
 export * from "./monthly-income";
 export * from "./occupation";

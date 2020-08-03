3c5f60daa7576e8e2f5ac83eb7f406ca31be0ca3
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-03 15:22:14 +0800

    updated mock data

--- src/mocks/client.ts
+++ src/mocks/client.ts
@@ -1,8 +1,8 @@
-export const SAMPLE_CLIENT = {
+export const SAMPLE_CLIENT: IClientIDDetails = {
   address: "F-7-15, Block B, Lucky Gardens, Jalan Telawi, Bangsar, Kuala Lumpur, 46001",
   dateOfBirth: "14 August 1988",
   gender: "Male",
   id: "123 4567 8901",
   name: "Edgar Constantine",
-  pinCode: "46001",
+  postCode: "46001",
 };

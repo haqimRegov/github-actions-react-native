5e51372846cc86869f6bede75d6e262ed902ff1b
HamidMirghafoori <datasciencestep@gmail.com>
2020-08-10 15:57:21 +0800

    type updated

--- types/client-id.d.ts
+++ types/client-id.d.ts
@@ -1,10 +1,15 @@
 declare interface IClientIDDetails {
+  isPassport: boolean;
   address: string;
+  city: string;
+  country: string;
   dateOfBirth: string;
   gender: string;
   id: string;
   name: string;
   postCode: string;
+  salutation: string;
+  state: string;
 }
 
 declare type TypeID = "NRIC" | "Passport" | "Army" | "Police";

69d7ee97571eaf733946264162a38af3af44f234
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-12 16:22:55 +0800

    wrapped app using redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React, { useEffect } from "react";
 import { KeyboardAvoidingView, Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SplashScreen from "react-native-splash-screen";
+import { Provider } from "react-redux";
 
 import { RootNavigator } from "./Navigator";
+import { store } from "./store";
 import { flexChild } from "./styles";
 
 export const App = () => {
@@ -13,14 +15,16 @@ export const App = () => {
 
   return (
     <SafeAreaProvider>
-      {Platform.select({
-        android: <RootNavigator />,
-        ios: (
-          <KeyboardAvoidingView behavior="padding" style={flexChild}>
-            <RootNavigator />
-          </KeyboardAvoidingView>
-        ),
-      })}
+      <Provider store={store}>
+        {Platform.select({
+          android: <RootNavigator />,
+          ios: (
+            <KeyboardAvoidingView behavior="padding" style={flexChild}>
+              <RootNavigator />
+            </KeyboardAvoidingView>
+          ),
+        })}
+      </Provider>
     </SafeAreaProvider>
   );
 };

c110795162421634fccf3b4942db517d09d34443
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-12 16:18:13 +0800

    updated mocks

--- src/mocks/client.ts
+++ src/mocks/client.ts
@@ -1,26 +1,35 @@
 export const SAMPLE_CLIENT: IClientIDDetails = {
-  isPassport: true,
   address: "",
   city: "",
   country: "",
   dateOfBirth: "14 August 1988",
   gender: "",
   id: "888 88 888",
+  isPassport: true,
   name: "",
   postCode: "",
   salutation: "",
   state: "",
 };
-// export const SAMPLE_CLIENT: IClientIDDetails = {
-// isPassport: true,
-//   address: "F-7-15, Block B, Lucky Gardens, Jalan Telawi, Bangsar, Kuala Lumpur, 46001",
-//   city: "Petaling Jaya",
-// country: "Malaysia",
-//   dateOfBirth: "14 August 1988",
-//   gender: "Male",
-//   id: "123 4567 8901",
-//   name: "Edgar Constantine",
-//   postCode: "46001",
-//   salutation: "Mr.",
-//   state: "Selangor",
-// };
+
+export const SAMPLE_CLIENT_2: IClientDetails = {
+  address: "F-7-15, Block B, Lucky Gardens, Jalan Telawi, Bangsar, Kuala Lumpur, 46001",
+  dateOfBirth: "14 August 1988",
+  gender: "Male",
+  id: "123456789012",
+  idType: "NRIC",
+  name: "Edgar Constantine",
+  postCode: "46001",
+};
+
+export const SAMPLE_CLIENT_3: IClientDetails = {
+  address: "B-7-3A, Block B West, Menara Pj8, 46050 Petaling Jaya, Selangor",
+  dateOfBirth: "04 January 1998",
+  gender: "Male",
+  id: "010419981234",
+  idType: "NRIC",
+  name: "Aldrin Castro",
+  postCode: "46050",
+};
+
+export const SAMPLE_CLIENTS = [SAMPLE_CLIENT_2, SAMPLE_CLIENT_3];
---
+++ src/mocks/risk-assessment.ts
@@ -0,0 +1,8 @@
+export const SAMPLE_RISK_ASSESSMENT: IRiskScore = {
+  appetite: "Medium",
+  profile:
+    "Your risk profile indicates that you can only tolerate minimal downside risks and potential capital loss. Your investment objective is skewed towards towards generating income as well some capital growth.",
+  return: "6% - 8% per annum",
+  type: "Income / Growth",
+  suggestedFunds: ["Local Fund", "International Fund"],
+};

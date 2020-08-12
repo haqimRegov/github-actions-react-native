6376bcb795007a770d902c6f8f59a57c23c937be
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-12 16:22:28 +0800

    added rootReducer and store

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+export * from "./Client";
+export * from "./Onboarding";
+export * from "./OnboardingSteps";
+
+export * from "./actionCreator";
+export * from "./rootReducer";
+export * from "./setup";

21d845bd8a2e66228e622e47aef1c84b20881bf8
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-13 15:45:04 +0800

    implemented QuickActions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,9 +2,17 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import React, { Fragment, useState } from "react";
 import { View } from "react-native";
 
-import { CustomSpacer, RoundedButton } from "../../components";
-import { SideMenuV2 } from "../../components/Nav/SideMenuV2";
-import { alignSelfCenter, centerHV, flexCol, flexRow, fullHW, sw20, sw256 } from "../../styles";
+import {
+  CustomDatePicker,
+  CustomFlexSpacer,
+  CustomSpacer,
+  IQuickAction,
+  QuickActions,
+  RoundedButton,
+  SafeAreaPage,
+  SideMenuV2,
+} from "../../components";
+import { alignSelfCenter, borderTopGray2, flexChild, flexRow, flexRowCC, fullHW, sh24, sw200, sw24 } from "../../styles";
 import { AddClient } from "./AddClient";
 
 interface DashboardPageProps {
@@ -13,10 +21,42 @@ interface DashboardPageProps {
 
 export const DashboardPage = ({ navigation }: DashboardPageProps) => {
   const [addClient, setAddClient] = useState<boolean>(false);
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [time, setTime] = useState<Date | undefined>(undefined);
+
+  const handleTopUp = () => {};
 
   const handleAddClient = () => {
     setAddClient(true);
   };
+
+  const QUICK_ACTIONS: IQuickAction[] = [
+    {
+      label: "Top Up",
+      onPress: handleTopUp,
+    },
+    {
+      label: "Switch",
+      onPress: handleTopUp,
+    },
+    {
+      label: "Redeem",
+      onPress: handleTopUp,
+    },
+    {
+      label: "Transfer",
+      onPress: handleTopUp,
+    },
+    {
+      label: "Buy Fund",
+      onPress: handleTopUp,
+    },
+    {
+      label: "Add New Client",
+      onPress: handleAddClient,
+      style: borderTopGray2,
+    },
+  ];
 
   const handleOnboarding = () => {
     navigation.navigate("Onboarding");
@@ -26,12 +66,25 @@ export const DashboardPage = ({ navigation }: DashboardPageProps) => {
     <Fragment>
       <View style={{ ...flexRow, ...fullHW }}>
         <SideMenuV2 />
-        <CustomSpacer isHorizontal={true} space={sw256} />
-        <View style={{ ...flexCol, ...centerHV }}>
-          <RoundedButton onPress={handleAddClient} text="Add Client" buttonStyle={alignSelfCenter} />
-          <CustomSpacer space={sw20} />
-          <RoundedButton onPress={handleOnboarding} text="Products" buttonStyle={alignSelfCenter} />
-        </View>
+        <CustomSpacer isHorizontal={true} space={sw200} />
+        <SafeAreaPage>
+          <View style={flexChild}>
+            <CustomSpacer space={sh24} />
+            <View style={flexRow}>
+              <CustomFlexSpacer />
+              <QuickActions actions={QUICK_ACTIONS} />
+              <CustomSpacer isHorizontal={true} space={sw24} />
+            </View>
+            <CustomSpacer space={sh24} />
+            <View style={flexRowCC}>
+              <CustomDatePicker value={date} setValue={setDate} mode="date" />
+              <CustomSpacer isHorizontal={true} space={sw24} />
+              <CustomDatePicker value={time} setValue={setTime} mode="time" />
+            </View>
+            <CustomSpacer space={sh24} />
+            <RoundedButton onPress={handleOnboarding} text="Jump to Onboarding" buttonStyle={alignSelfCenter} />
+          </View>
+        </SafeAreaPage>
       </View>
       <AddClient navigation={navigation} setVisible={setAddClient} visible={addClient} />
     </Fragment>

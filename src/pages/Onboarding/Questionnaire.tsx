8c87d77f93498226a741504e50af533e069a1850
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-13 13:01:00 +0800

    updated redux implementation

--- src/pages/Onboarding/Questionnaire.tsx
+++ src/pages/Onboarding/Questionnaire.tsx
@@ -1,13 +1,13 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Image, Text, TextStyle, View } from "react-native";
 import { connect } from "react-redux";
-import { bindActionCreators, Dispatch } from "redux";
 
 import { LocalAssets } from "../../assets/LocalAssets";
 import { ConfirmationModal, ContentPage, CustomSlider, CustomSpacer, LabeledTitle, Question, TextSpaceArea } from "../../components";
 import { Language, ONBOARDING_ROUTES } from "../../constants";
 import { Q1_OPTIONS, Q2_OPTIONS, Q3_OPTIONS, Q4_OPTIONS, Q5_OPTIONS, Q6_OPTIONS } from "../../data/dictionary/risk-assessment-questions";
-import { addAssessmentQuestions, getRiskAssessment, resetRiskAssessment, RootState } from "../../store";
+import { getRiskScore } from "../../network-actions";
+import { RiskMapDispatchToProps, RiskMapStateToProps, RiskStoreProps } from "../../store";
 import {
   flexRow,
   fs12SemiBoldBlack2,
@@ -26,10 +26,11 @@ import {
   sw282,
   sw432,
 } from "../../styles";
+import { isObjectEqual } from "../../utils";
 
 const { RISK_ASSESSMENT } = Language.PAGE;
 
-interface QuestionnaireContentProps extends OnboardingContentProps, ReduxStoreProps {
+interface QuestionnaireContentProps extends OnboardingContentProps, RiskStoreProps {
   navigation: IStackNavigationProp;
 }
 
@@ -39,12 +40,12 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
   const [confirmModal, setConfirmModal] = useState<TypeRiskAssessmentModal>(undefined);
   const [prevQuestionnaire, setPrevQuestionnaire] = useState<IRiskAssessmentQuestions | undefined>(undefined);
 
-  const q1 = questionnaire!.questionOne;
-  const q2 = questionnaire!.questionTwo;
-  const q3 = questionnaire!.questionThree;
-  const q4 = questionnaire!.questionFour;
-  const q5 = questionnaire!.questionFive;
-  const q6 = questionnaire!.questionSix;
+  const q1 = questionnaire.questionOne;
+  const q2 = questionnaire.questionTwo;
+  const q3 = questionnaire.questionThree;
+  const q4 = questionnaire.questionFour;
+  const q5 = questionnaire.questionFive;
+  const q6 = questionnaire.questionSix;
 
   const setQ1 = (index: number) => props.addAssessmentQuestions({ questionOne: index });
   const setQ2 = (index: number) => props.addAssessmentQuestions({ questionTwo: index });
@@ -72,7 +73,7 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
 
   const handlePageContinue = () => {
     setConfirmModal("assessment");
-    props.getRiskAssessment(questionnaire!);
+    getRiskScore(props, questionnaire);
   };
 
   const handleEdit = () => {
@@ -88,18 +89,6 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
     }
   };
 
-  useEffect(() => {
-    if (prevQuestionnaire === undefined && finishedSteps.includes(ONBOARDING_ROUTES.Questionnaire)) {
-      setPrevQuestionnaire(questionnaire);
-    }
-  }, [finishedSteps, prevQuestionnaire, questionnaire]);
-
-  useEffect(() => {
-    if (prevQuestionnaire !== undefined && JSON.stringify(questionnaire) !== JSON.stringify(prevQuestionnaire)) {
-      setConfirmModal("prompt");
-    }
-  }, [prevQuestionnaire, questionnaire]);
-
   const isAssessment = confirmModal === "assessment";
 
   const labelCancel = isAssessment ? RISK_ASSESSMENT.BUTTON_RETAKE : RISK_ASSESSMENT.BUTTON_NO;
@@ -109,15 +98,22 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
   const handleContinue = isAssessment ? handleConfirmAssessment : handleEdit;
   const modalTitleStyle: TextStyle = isAssessment ? {} : { width: sw432 };
 
-  const riskProfile =
-    riskScore !== undefined
-      ? [
-          { label: RISK_ASSESSMENT.PROFILE_APPETITE, title: riskScore.appetite },
-          { label: RISK_ASSESSMENT.PROFILE_LABEL_PROFILE, title: riskScore.profile },
-          { label: RISK_ASSESSMENT.PROFILE_LABEL_RETURN, title: riskScore.return },
-          { label: RISK_ASSESSMENT.PROFILE_LABEL_TYPE, title: riskScore.type },
-        ]
-      : [];
+  const riskProfile = [
+    { label: RISK_ASSESSMENT.PROFILE_APPETITE, title: riskScore.appetite },
+    { label: RISK_ASSESSMENT.PROFILE_LABEL_PROFILE, title: riskScore.profile },
+    { label: RISK_ASSESSMENT.PROFILE_LABEL_RETURN, title: riskScore.return },
+    { label: RISK_ASSESSMENT.PROFILE_LABEL_TYPE, title: riskScore.type },
+  ];
+
+  useEffect(() => {
+    if (prevQuestionnaire === undefined && finishedSteps.includes(ONBOARDING_ROUTES.Questionnaire)) {
+      setPrevQuestionnaire(questionnaire);
+    }
+
+    if (prevQuestionnaire !== undefined && !isObjectEqual(questionnaire, prevQuestionnaire)) {
+      setConfirmModal("prompt");
+    }
+  }, [finishedSteps, prevQuestionnaire, questionnaire]);
 
   return (
     <Fragment>
@@ -129,7 +125,7 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
             <Question
               label={RISK_ASSESSMENT.LABEL_QUESTION_1}
               options={Q1_OPTIONS}
-              selected={q1!}
+              selected={q1}
               setSelected={setQ1}
               title={RISK_ASSESSMENT.QUESTION_1}
             />
@@ -137,24 +133,24 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
             <Question
               label={RISK_ASSESSMENT.LABEL_QUESTION_2}
               options={Q2_OPTIONS}
-              selected={q2!}
+              selected={q2}
               setSelected={setQ2}
               title={RISK_ASSESSMENT.QUESTION_2}
             />
             <CustomSpacer space={sh32} />
             <LabeledTitle label={RISK_ASSESSMENT.LABEL_QUESTION_3} title={RISK_ASSESSMENT.QUESTION_3} titleStyle={fs16SemiBoldBlack2} />
             <CustomSpacer space={sh16} />
-            <CustomSlider disabled={true} options={Q3_OPTIONS} selected={q3!} setSelected={setQ3} />
+            <CustomSlider disabled={true} options={Q3_OPTIONS} selected={q3} setSelected={setQ3} />
             <CustomSpacer space={sh32} />
             <LabeledTitle label={RISK_ASSESSMENT.LABEL_QUESTION_4} title={RISK_ASSESSMENT.QUESTION_4} titleStyle={fs16SemiBoldBlack2} />
             <CustomSpacer space={sh16} />
-            <CustomSlider disabled={true} options={Q4_OPTIONS} selected={q4!} setSelected={setQ4} />
+            <CustomSlider disabled={true} options={Q4_OPTIONS} selected={q4} setSelected={setQ4} />
             <CustomSpacer space={sh32} />
             <Question
               label={RISK_ASSESSMENT.LABEL_QUESTION_5}
               options={Q5_OPTIONS}
               right={<Image style={{ height: sh143, width: sw140 }} source={LocalAssets.graph.risk_assessment_graph_1} />}
-              selected={q5!}
+              selected={q5}
               setSelected={setQ5}
               title={RISK_ASSESSMENT.QUESTION_5}
             />
@@ -163,7 +159,7 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
               label={RISK_ASSESSMENT.LABEL_QUESTION_6}
               options={Q6_OPTIONS}
               right={<Image style={{ height: sh189, width: sw282 }} source={LocalAssets.graph.risk_assessment_graph_2} />}
-              selected={q6!}
+              selected={q6}
               setSelected={setQ6}
               title={RISK_ASSESSMENT.QUESTION_6}
             />
@@ -206,22 +202,4 @@ const QuestionnaireContentComponent = (props: QuestionnaireContentProps) => {
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  questionnaire: state.riskAssessment.questionnaire,
-  riskScore: state.riskAssessment.riskScore,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators(
-    {
-      addAssessmentQuestions,
-      getRiskAssessment,
-      resetRiskAssessment,
-    },
-    dispatch,
-  );
-};
-
-type ReduxStoreProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
-
-export const QuestionnaireContent = connect(mapStateToProps, mapDispatchToProps)(QuestionnaireContentComponent);
+export const QuestionnaireContent = connect(RiskMapStateToProps, RiskMapDispatchToProps)(QuestionnaireContentComponent);

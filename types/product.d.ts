c2928ced9786d6c9870ead54cfe361d5afb400e5
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-21 19:02:43 +0800

    added new Fund type

--- types/product.d.ts
+++ types/product.d.ts
@@ -9,6 +9,71 @@ declare interface IProduct {
   type: string;
 }
 
+declare interface IFund {
+  annualManagementFee?: string;
+  fundCategory: string;
+  fundCurrency: string;
+  fundObjective?: string;
+  fundType: string;
+  isEpf: boolean;
+  isScheduled: boolean;
+  isShariah: boolean;
+  issuer: string;
+  latestFundSize?: number;
+  name: string;
+  newSalesAmount: {
+    cash?: {
+      maximum?: number;
+      minimum?: number;
+    };
+    epf?: {
+      maximum?: number;
+      minimum?: number;
+    };
+  };
+  performanceOverview: string;
+  performance?: {
+    benchmark?: {
+      oneMonth?: number[];
+      sixMonths?: number[];
+      oneYear?: number[];
+      threeYears?: number[];
+      fiveYears?: number[];
+      tenYears?: number[];
+    };
+    details?: string;
+    nav?: {
+      oneMonth?: number[];
+      sixMonths?: number[];
+      oneYear?: number[];
+      threeYears?: number[];
+      fiveYears?: number[];
+      tenYears?: number[];
+    };
+  };
+  riskCategory: string;
+  salesCharge: {
+    cash?: {
+      maximum?: string;
+      minimum?: string;
+    };
+    epf?: {
+      maximum?: string;
+      minimum?: string;
+    };
+  };
+  topUpAmount?: {
+    cash?: {
+      minimum?: number;
+      maximum?: number;
+    };
+    epf?: {
+      maximum?: number;
+      minimum?: number;
+    };
+  };
+}
+
 declare interface IProductConfirmation extends IProduct {
   accountType: string;
   channel: string;

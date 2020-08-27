795f55f3ee25dcb86bfd805345986f2fa731d5a0
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-27 18:49:53 +0800

    added new data dictionary

---
+++ src/data/dictionary/epf-type.ts
@@ -0,0 +1,4 @@
+export const DICTIONARY_EPF_TYPE: TypeEpfLabelValue[] = [
+  { label: "Conventional", value: "Conventional" },
+  { label: "Shariah", value: "Shariah" },
+];
---
+++ src/data/dictionary/races.ts
@@ -0,0 +1,6 @@
+export const DICTIONARY_MALAYSIA_RACE: TypeMalaysiaRaceLabelValue[] = [
+  { label: "Malay", value: "Malay" },
+  { label: "Chinese", value: "Chinese" },
+  { label: "Indian", value: "Indian" },
+  { label: "Others", value: "Others" },
+];

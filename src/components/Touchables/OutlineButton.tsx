d04fa2565cd09951c6c3ad1ba53f74fcc7a455e2
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-27 18:56:08 +0800

    updated OutlineButton

--- src/components/Touchables/OutlineButton.tsx
+++ src/components/Touchables/OutlineButton.tsx
@@ -1,24 +1,69 @@
-import React, { FunctionComponent } from "react";
-import { ViewStyle } from "react-native";
+import React, { Fragment, FunctionComponent } from "react";
+import { Text, TextStyle, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 
-import { colorBlack, flexChild, fs12RegBlack2, sh32, sw100 } from "../../styles";
-import { RoundedButton, RoundedButtonProps } from "./RoundedButton";
+import { IcoMoon } from "../../icons";
+import {
+  border,
+  centerVertical,
+  colorBlack,
+  colorRed,
+  colorTransparent,
+  flexRow,
+  fs16SemiBoldWhite1,
+  fsCapitalize,
+  px,
+  sh16,
+  sh32,
+  sw1,
+  sw100,
+  sw16,
+  sw8,
+} from "../../styles";
+import { CustomSpacer } from "../Views";
 
-export interface OutlineButtonProps extends RoundedButtonProps {
+export interface OutlineButtonProps {
+  buttonStyle?: ViewStyle;
   color?: string;
+  icon?: string;
+  onPress: () => void;
+  text: string;
+  textStyle?: TextStyle;
 }
 
-export const OutlineButton: FunctionComponent<OutlineButtonProps> = ({ color, buttonStyle, textStyle, ...rest }: OutlineButtonProps) => {
+export const OutlineButton: FunctionComponent<OutlineButtonProps> = ({
+  buttonStyle,
+  color,
+  icon,
+  onPress,
+  text,
+  textStyle,
+}: OutlineButtonProps) => {
   const defaultBorderColor: ViewStyle = color !== undefined ? { borderColor: color } : { borderColor: colorBlack._2 };
 
   const roundedButtonStyle: ViewStyle = {
-    backgroundColor: "transparent",
+    ...border(colorRed._1, sw1),
+    ...centerVertical,
+    ...flexRow,
+    ...px(sw16),
+    backgroundColor: colorTransparent,
     borderRadius: sw100,
     height: sh32,
+    marginRight: "auto", // TODO find better solution
     ...defaultBorderColor,
-    ...flexChild,
     ...buttonStyle,
   };
 
-  return <RoundedButton buttonStyle={roundedButtonStyle} iconColor={color} textStyle={{ ...fs12RegBlack2, ...textStyle }} {...rest} />;
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={roundedButtonStyle}>
+        {icon === undefined ? null : (
+          <Fragment>
+            <IcoMoon color={color || colorBlack._1} name={icon} size={sh16} />
+            <CustomSpacer isHorizontal={true} space={sw8} />
+          </Fragment>
+        )}
+        <Text style={{ ...fs16SemiBoldWhite1, ...fsCapitalize, color: color, ...textStyle }}>{text}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };

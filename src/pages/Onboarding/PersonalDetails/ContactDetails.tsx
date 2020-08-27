d99b0b9915fabbd2e9286f24f63915cc1465a4da
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-27 19:06:05 +0800

    updated ContactDetails component

--- src/pages/Onboarding/PersonalDetails/ContactDetails.tsx
+++ src/pages/Onboarding/PersonalDetails/ContactDetails.tsx
@@ -1,46 +1,31 @@
 import React, { Fragment, FunctionComponent } from "react";
 import { View } from "react-native";
 
-import {
-  CustomFlexSpacer,
-  CustomSpacer,
-  CustomTextInput,
-  IconButton,
-  OutlineButton,
-  RadioButtonGroup,
-  TextSpaceArea,
-} from "../../../components";
+import { CustomFlexSpacer, CustomSpacer, CustomTextInput, IconButton, OutlineButton } from "../../../components";
 import { Language } from "../../../constants";
+import { DICTIONARY_MOBILE_CODE } from "../../../data/dictionary";
 import {
   centerVertical,
-  circleBorder,
+  circle,
   colorBlack,
-  colorBlue,
+  colorGray,
+  colorTransparent,
   flexRow,
+  fs12RegBlack2,
   px,
+  sh16,
   sh24,
-  sh4,
-  sh40,
-  sh8,
-  sw05,
-  sw12,
-  sw14,
+  sh32,
   sw16,
   sw24,
-  sw32,
+  sw40,
 } from "../../../styles";
 
-const { CONTACT_DETAILS } = Language.PAGE;
+const { PERSONAL_DETAILS } = Language.PAGE;
 
-interface IContactDetailsProps {
+interface ContactDetailsProps {
   contactNumber: IContactNumber[];
-  inputEmailAddress: string;
-  inputRace: string;
-  inputBumiputera: string;
   setContactNumber: (input: IContactNumber[]) => void;
-  setInputEmailAddress: (input: string) => void;
-  setInputRace: (input: string) => void;
-  setInputBumiputera: (input: string) => void;
 }
 
 interface IContactNumberLabel {
@@ -50,113 +35,85 @@ interface IContactNumberLabel {
 
 const allLabels: IContactNumberLabel[] = [
   {
-    label: CONTACT_DETAILS.LABEL_MOBILE_NUMBER,
+    label: PERSONAL_DETAILS.LABEL_MOBILE_NUMBER,
     type: "mobile",
   },
   {
-    label: CONTACT_DETAILS.LABEL_HOME_NUMBER,
+    label: PERSONAL_DETAILS.LABEL_HOME_NUMBER,
     type: "home",
   },
   {
-    label: CONTACT_DETAILS.LABEL_OFFICE_NUMBER,
+    label: PERSONAL_DETAILS.LABEL_OFFICE_NUMBER,
     type: "office",
   },
   {
-    label: CONTACT_DETAILS.LABEL_FAX_NUMBER,
+    label: PERSONAL_DETAILS.LABEL_FAX_NUMBER,
     type: "fax",
   },
 ];
 
-export const ContactDetails: FunctionComponent<IContactDetailsProps> = ({
-  contactNumber,
-  inputEmailAddress,
-  inputRace,
-  inputBumiputera,
-  setContactNumber,
-  setInputEmailAddress,
-  setInputRace,
-  setInputBumiputera,
-}: IContactDetailsProps) => {
+export const ContactDetails: FunctionComponent<ContactDetailsProps> = ({ contactNumber, setContactNumber }: ContactDetailsProps) => {
   return (
-    <View style={px(sw24)}>
-      {contactNumber.map((item: IContactNumber, index: number) => {
-        const handleRemoveNumber = () => {
-          const updatedNumber = [...contactNumber];
-          updatedNumber.splice(updatedNumber.indexOf(item), 1);
-          setContactNumber(updatedNumber);
-        };
-
-        const handleChangeText = (input: string) => {
-          const updatedNumber = [...contactNumber];
-          updatedNumber[index].value = input;
-          setContactNumber(updatedNumber);
-        };
-
-        return (
-          <View key={index} style={{ ...centerVertical, ...flexRow }}>
-            <View>
-              <CustomTextInput
-                keyboardType="phone-pad"
-                label={item.label}
-                onChangeText={handleChangeText}
-                spaceToTop={sh24}
-                value={item.value}
-              />
-            </View>
-            <CustomSpacer isHorizontal={true} space={sw16} />
-            {index === 0 ? null : (
-              <View>
-                <CustomFlexSpacer />
-                <IconButton
-                  color={colorBlue._1}
-                  name="close"
-                  onPress={handleRemoveNumber}
-                  size={sw12}
-                  style={circleBorder(sw32, sw05, colorBlue._1)}
-                />
-                <CustomSpacer space={sh4} />
-              </View>
-            )}
-          </View>
-        );
-      })}
-      <View style={px(sw14)}>
-        {allLabels.map((item: IContactNumberLabel, index: number) => {
-          const buttonText = `${CONTACT_DETAILS.BUTTON_ADD} ${item.label}`;
-          const checkName = (element: IContactNumber) => element.label === item.label;
-
-          const handleAddNumber = () => {
-            const contactNumberClone = [...contactNumber, { ...item, value: "" }];
-            setContactNumber(contactNumberClone);
+    <View>
+      <View style={px(sw24)}>
+        {contactNumber.map((item: IContactNumber, index: number) => {
+          const handleRemoveNumber = () => {
+            const updatedNumber = [...contactNumber];
+            updatedNumber.splice(updatedNumber.indexOf(item), 1);
+            setContactNumber(updatedNumber);
           };
 
-          return contactNumber.some(checkName) ? null : (
-            <Fragment key={index}>
-              <CustomSpacer space={sh8} />
-              <OutlineButton iconColor={colorBlack._1} icon="plus" onPress={handleAddNumber} text={buttonText} />
-            </Fragment>
+          const handleChangeNumber = (input: string) => {
+            const updatedNumber = [...contactNumber];
+            updatedNumber[index].value = input;
+            setContactNumber(updatedNumber);
+          };
+
+          return (
+            <View key={index} style={{ ...centerVertical, ...flexRow }}>
+              <CustomTextInput
+                inputPrefix={item.code}
+                keyboardType="numeric"
+                label={item.label}
+                onChangeText={handleChangeNumber}
+                placeholder="12 3456 7890"
+                placeholderTextColor={colorGray._7}
+                spaceToTop={sh32}
+                value={item.value}
+              />
+              <CustomSpacer isHorizontal={true} space={sw16} />
+              {index === 0 ? null : (
+                <View>
+                  <CustomFlexSpacer />
+                  <IconButton
+                    name="trash"
+                    color={colorBlack._1}
+                    onPress={handleRemoveNumber}
+                    size={sh24}
+                    style={circle(sw40, colorTransparent)}
+                  />
+                </View>
+              )}
+            </View>
           );
         })}
-      </View>
-      <CustomTextInput
-        keyboardType="email-address"
-        label={CONTACT_DETAILS.LABEL_EMAIL}
-        onChangeText={setInputEmailAddress}
-        spaceToTop={sh24}
-        value={inputEmailAddress}
-      />
-      <CustomTextInput label={CONTACT_DETAILS.LABEL_RACE} onChangeText={setInputRace} spaceToTop={sh24} value={inputRace} />
-      <View style={flexRow}>
-        <CustomSpacer isHorizontal={true} space={sw12} />
         <View>
-          <TextSpaceArea spaceToBottom={sh8} spaceToTop={sh24} text={CONTACT_DETAILS.LABEL_BUMIPUTERA} />
-          <RadioButtonGroup
-            direction="row"
-            labels={[CONTACT_DETAILS.OPTION_BUMIPUTERA_YES, CONTACT_DETAILS.OPTION_BUMIPUTERA_NO]}
-            selected={inputBumiputera}
-            setSelected={setInputBumiputera}
-            space={sh40}
-          />
+          {allLabels.map((item: IContactNumberLabel, index: number) => {
+            const buttonText = `${PERSONAL_DETAILS.BUTTON_ADD} ${item.label}`;
+            const checkName = (element: IContactNumber) => element.label === item.label;
+
+            const handleAddNumber = () => {
+              const contactNumberClone = [...contactNumber, { ...item, value: "", code: DICTIONARY_MOBILE_CODE[0].value }];
+              setContactNumber(contactNumberClone);
+            };
+
+            return contactNumber.some(checkName) ? null : (
+              <Fragment key={index}>
+                <CustomSpacer space={sh16} />
+                <OutlineButton icon="plus" onPress={handleAddNumber} text={buttonText} textStyle={fs12RegBlack2} />
+              </Fragment>
+            );
+          })}
         </View>
       </View>
     </View>

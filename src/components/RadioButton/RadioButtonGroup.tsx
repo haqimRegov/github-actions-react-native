64ec40c0d1fa2dd410add66bdb5a82a71fb75fd0
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-30 23:22:34 +0800

    updated component

--- src/components/RadioButton/RadioButtonGroup.tsx
+++ src/components/RadioButton/RadioButtonGroup.tsx
@@ -1,36 +1,58 @@
 import React, { Fragment } from "react";
 import { TextStyle, View } from "react-native";
 
-import { RadioButton } from "../../components/RadioButton/RadioButton";
-import { flexCol, flexRow, sh16, sw16 } from "../../styles";
+import { flexCol, flexRow, fs16RegBlack2, sh16, sw16 } from "../../styles";
 import { CustomSpacer } from "../Views/Spacer";
+import { TextSpaceArea } from "../Views/TextSpaceArea";
+import { RadioButton } from "./RadioButton";
 
 export interface RadioButtonGroupProps {
   direction?: "row" | "column";
-  labels: string[];
+  label?: string;
   labelStyle?: TextStyle;
+  options: string[];
+  optionStyle?: TextStyle;
   selected: string;
   setSelected: (selected: string) => void;
   space?: number;
+  spaceToLabel?: number;
+  spaceToTop?: number;
 }
 
-export const RadioButtonGroup = ({ direction, labels, labelStyle, selected, setSelected, space }: RadioButtonGroupProps) => {
+export const RadioButtonGroup = ({
+  direction,
+  label,
+  labelStyle,
+  options,
+  optionStyle,
+  selected,
+  setSelected,
+  space,
+  spaceToLabel,
+  spaceToTop,
+}: RadioButtonGroupProps) => {
   const defaultSpace = direction === "row" ? sw16 : sh16;
   const radioSpace = space !== undefined ? space : defaultSpace;
 
   return (
-    <View style={direction === "row" ? flexRow : flexCol}>
-      {labels.map((label: string, index: number) => {
-        const handleSelect = () => {
-          setSelected(label);
-        };
-        return (
-          <Fragment key={index}>
-            {index === 0 ? null : <CustomSpacer isHorizontal={direction === "row"} space={radioSpace} />}
-            <RadioButton label={label} labelStyle={labelStyle} selected={label === selected} setSelected={handleSelect} />
-          </Fragment>
-        );
-      })}
+    <View>
+      {spaceToTop !== undefined ? <CustomSpacer space={spaceToTop} /> : null}
+      {label !== undefined ? (
+        <TextSpaceArea spaceToBottom={spaceToLabel || sh16} style={{ ...fs16RegBlack2, ...labelStyle }} text={label} />
+      ) : null}
+      <View style={direction === "row" ? flexRow : flexCol}>
+        {options.map((option: string, index: number) => {
+          const handleSelect = () => {
+            setSelected(option);
+          };
+          return (
+            <Fragment key={index}>
+              {index === 0 ? null : <CustomSpacer isHorizontal={direction === "row"} space={radioSpace} />}
+              <RadioButton label={option} labelStyle={optionStyle} selected={option === selected} setSelected={handleSelect} />
+            </Fragment>
+          );
+        })}
+      </View>
     </View>
   );
 };

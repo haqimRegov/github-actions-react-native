4fbaff15104f5f00b88e98d5644230ffac455bb7
Aldrin Castro <aldrin.castro@regovtech.com>
2020-08-31 16:45:49 +0800

    updated data dictionary

---
+++ src/data/dictionary/otp.ts
@@ -0,0 +1,4 @@
+// TODO FRS is set to 120 seconds, 5 seconds is for development purposes
+export const DICTIONARY_OTP_EXPIRY = 5;
+export const DICTIONARY_OTP_MAX_RETRY = 3;
+export const DICTIONARY_OTP_COOL_OFF = 300;

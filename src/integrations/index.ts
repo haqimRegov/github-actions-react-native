d0b866a0de1a3ed96c2ff8f777bbc446deacf0d7
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-01 22:08:24 +0800

    added react-native-share functions

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -2,3 +2,4 @@ export * from "./async-storage";
 export * from "./file-system";
 export * from "./react-native-document-picker";
 export * from "./react-native-image-crop-picker";
+export * from "./react-native-share";
---
+++ src/integrations/react-native-share/functions.ts
@@ -0,0 +1,15 @@
+import Share from "react-native-share";
+
+const filesBase64 = async (base64List: string[]) => {
+  const share = await Share.open({ urls: base64List })
+    .then((res) => res)
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    });
+  return share;
+};
+
+export const RNShareApi = {
+  filesBase64,
+};

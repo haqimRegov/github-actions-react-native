0d2bc211aa46f660f2dbfe6356d307af08943750
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-01 22:00:14 +0800

    added new props

--- src/components/Views/LabeledTitle.tsx
+++ src/components/Views/LabeledTitle.tsx
@@ -1,26 +1,50 @@
-import React from "react";
-import { Text, TextStyle, View, ViewStyle } from "react-native";
+import React, { Fragment } from "react";
+import { Text, TextStyle, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 
-import { fs12BoldBlack2, fs16RegBlack2 } from "../../styles";
+import { flexRow, fs12BoldBlack2, fs16RegBlack2, sw4 } from "../../styles";
 import { CustomSpacer } from "./Spacer";
 
 export interface LabeledTitleProps {
   label: string;
   labelStyle?: TextStyle;
+  onPress?: () => void;
   spaceToBottom?: number;
   spaceToLabel?: number;
   style?: ViewStyle;
   title: string;
+  titlePrefix?: string;
+  titlePrefixStyle?: TextStyle;
   titleStyle?: TextStyle;
 }
 
-export const LabeledTitle = ({ label, labelStyle, spaceToBottom, spaceToLabel, style, title, titleStyle }: LabeledTitleProps) => {
+export const LabeledTitle = ({
+  label,
+  labelStyle,
+  onPress,
+  spaceToBottom,
+  spaceToLabel,
+  style,
+  title,
+  titlePrefix,
+  titlePrefixStyle,
+  titleStyle,
+}: LabeledTitleProps) => {
   return (
-    <View style={style}>
-      <Text style={{ ...fs12BoldBlack2, ...labelStyle }}>{label}</Text>
-      {spaceToLabel === undefined ? null : <CustomSpacer space={spaceToLabel} />}
-      <Text style={{ ...fs16RegBlack2, ...titleStyle }}>{title}</Text>
-      {spaceToBottom === undefined ? null : <CustomSpacer space={spaceToBottom} />}
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={style}>
+        <Text style={{ ...fs12BoldBlack2, ...labelStyle }}>{label}</Text>
+        {spaceToLabel === undefined ? null : <CustomSpacer space={spaceToLabel} />}
+        <View style={flexRow}>
+          {titlePrefix !== undefined ? (
+            <Fragment>
+              <Text style={{ ...fs16RegBlack2, ...titlePrefixStyle }}>{titlePrefix}</Text>
+              <CustomSpacer isHorizontal={true} space={sw4} />
+            </Fragment>
+          ) : null}
+          <Text style={{ ...fs16RegBlack2, ...titleStyle }}>{title}</Text>
+        </View>
+        {spaceToBottom === undefined ? null : <CustomSpacer space={spaceToBottom} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };

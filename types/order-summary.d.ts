9469f75a77ff16ebe962c16f80cfcf7f8ac8aa29
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-09 11:21:49 +0800

    updated types

--- types/order-summary.d.ts
+++ types/order-summary.d.ts
@@ -1,8 +1,28 @@
-import { IFundSummary } from "./fund-summary";
+declare interface IFundOrderSummary {
+  fundingOption: string;
+  accountType: string;
+  distributionChannel: string;
+  fundCurrency: string;
+  fundIssuer: string;
+  fundName: string;
+  fundType: string;
+  investmentAmount: string;
+  isEpf: boolean;
+  isShariah: boolean;
+  salesCharge: number;
 
-export interface IOrderSummary {
-  date: string;
-  funds: IFundSummary[];
-  orderNo: string;
-  totalAmount: number;
+  fundCode?: string;
+  scheduledPayment?: boolean;
+}
+
+declare interface IOrderSummary {
+  date: Date;
+  funds: IFundOrderSummary[];
+  orderNumber: string;
+  totalAmount: IOrderTotalAmount[];
+}
+
+declare interface IOrderTotalAmount {
+  amount: string;
+  currency: TypeCurrency;
 }

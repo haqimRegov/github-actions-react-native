9f2a8dbbcd41e4081a02d00262e72dfa0ac2779b
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-10 18:09:23 +0800

    added new props

--- src/components/List/ButtonSelectionList.tsx
+++ src/components/List/ButtonSelectionList.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, FunctionComponent } from "react";
 import { TextStyle, View, ViewStyle } from "react-native";
 
-import { colorRed, flexRow, sw4 } from "../../styles";
+import { colorRed, flexRow, flexWrap, sw4 } from "../../styles";
 import { OutlineButton } from "../Touchables";
 import { CustomSpacer } from "../Views";
 
@@ -12,6 +12,7 @@ interface ButtonSelectionListProps {
   icon?: string;
   onPress: (item: number) => void;
   spaceBetween?: number;
+  style?: ViewStyle;
   textStyle?: TextStyle;
 }
 
@@ -22,10 +23,11 @@ export const ButtonSelectionList: FunctionComponent<ButtonSelectionListProps> = 
   icon,
   onPress,
   spaceBetween,
+  style,
   textStyle,
 }: ButtonSelectionListProps) => {
   return (
-    <View style={flexRow}>
+    <View style={{ ...flexRow, ...flexWrap, ...style }}>
       {data.map((item: string, index: number) => {
         const handlePress = () => {
           onPress(index);

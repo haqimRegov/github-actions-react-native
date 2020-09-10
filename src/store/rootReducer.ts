260ccae4625e7a173536013bea43923986d21282
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-10 18:06:15 +0800

    updated redux setup

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,12 +3,14 @@ import { combineReducers } from "redux";
 import { orderSummaryReducer } from "./Acknowledgement";
 import { clientReducer } from "./Client";
 import { onboardingStepsReducer } from "./OnboardingSteps";
+import { productsReducer } from "./Products";
 import { riskAssessmentReducer } from "./RiskAssessment";
 import { selectedFundReducer } from "./SelectedFund";
 
 export const rootReducer = combineReducers({
   client: clientReducer,
   onboardingSteps: onboardingStepsReducer,
+  products: productsReducer,
   riskAssessment: riskAssessmentReducer,
   selectedFund: selectedFundReducer,
   orderSummary: orderSummaryReducer,

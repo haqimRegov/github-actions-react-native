bb99a777c99f7f6323a60c563119e9899f82f158
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-11 00:10:14 +0800

    added bankLocation

--- src/pages/Onboarding/PersonalDetails/BankDetails/index.tsx
+++ src/pages/Onboarding/PersonalDetails/BankDetails/index.tsx
@@ -33,6 +33,7 @@ export const BankDetails: FunctionComponent<IBankDetailsProps> = ({
 
   const initialForeignBankState: IBankingDetails = {
     ...initialLocalBankState,
+    bankLocation: "",
     currency: [""],
   };
 

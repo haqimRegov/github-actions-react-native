af38ce8f5b8a53def0ce4fd9b54a5a5e68ed003c
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-14 16:47:47 +0800

    updated network actions (temporary, setting the state should be outside)

---
+++ src/network-actions/onboarding/UploadClientID.ts
@@ -0,0 +1,9 @@
+import { SAMPLE_CLIENT_4 } from "../../mocks";
+
+export const uploadClientId = (token: string, params: FileBase64) => {
+  // TODO integration
+  // eslint-disable-next-line no-console
+  console.log(token, params);
+  const results = SAMPLE_CLIENT_4;
+  return results;
+};
--- src/network-actions/onboarding/index.ts
+++ src/network-actions/onboarding/index.ts
@@ -1,1 +1,2 @@
 export * from "./GetRiskScore";
+export * from "./UploadClientID";

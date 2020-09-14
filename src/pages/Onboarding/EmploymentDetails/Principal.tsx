f01e7ce22a31ea4a700ecff807c256c56705dcac
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-14 17:01:47 +0800

    Principal Employment Details

---
+++ src/pages/Onboarding/EmploymentDetails/Principal.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from "react";
+import { View } from "react-native";
+
+import { LabeledTitle, TextSpaceArea } from "../../../components";
+import { Language } from "../../../constants";
+import { fs10BoldBlack2, fs16RegBlack2, fs24BoldBlack2, px, sh8, sw24 } from "../../../styles";
+import { EmploymentInfo } from "./Details";
+
+const { EMPLOYMENT_DETAILS } = Language.PAGE;
+
+interface PrincipalEmploymentDetails {
+  accountType: TypeAccountChoices;
+  employmentDetails: IEmploymentDetailsState;
+  personalDetails: IPersonalDetailsState;
+  setEmploymentDetails: (value: IEmploymentDetailsState) => void;
+}
+
+export const PrincipalEmploymentDetails: FunctionComponent<PrincipalEmploymentDetails> = ({
+  accountType,
+  employmentDetails,
+  personalDetails,
+  setEmploymentDetails,
+}: PrincipalEmploymentDetails) => {
+  return (
+    <View>
+      <View style={px(sw24)}>
+        {accountType === "Individual" ? null : <TextSpaceArea spaceToBottom={sh8} style={fs10BoldBlack2} text={personalDetails.name!} />}
+        <LabeledTitle
+          label={EMPLOYMENT_DETAILS.HEADING}
+          labelStyle={fs24BoldBlack2}
+          spaceToLabel={sh8}
+          title={EMPLOYMENT_DETAILS.SUBHEADING}
+          titleStyle={fs16RegBlack2}
+        />
+      </View>
+      <EmploymentInfo accountType={accountType} employmentDetails={employmentDetails} setEmploymentDetails={setEmploymentDetails} />
+    </View>
+  );
+};

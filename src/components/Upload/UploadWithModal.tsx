fa7122e17052dd79e9c889b059478a0bcfbbb768
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-15 23:03:06 +0800

    enhance viewing file

--- src/components/Upload/UploadWithModal.tsx
+++ src/components/Upload/UploadWithModal.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, FunctionComponent, useState } from "react";
-import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native";
+import { Image, ImageSourcePropType, ImageStyle, TextStyle, View, ViewStyle } from "react-native";
 import PDFView from "react-native-view-pdf";
 
 import { IcoMoon } from "../../icons";
@@ -26,11 +26,18 @@ import {
 } from "../../styles";
 import { BasicModal } from "../Modals";
 import { CustomFlexSpacer, CustomSpacer, LabeledTitle } from "../Views";
-import { BYTE_TO_MEGABYTE, UploadDocument } from "./Upload";
+import { BYTE_TO_KILOBYTE, BYTE_TO_MEGABYTE, UploadDocument } from "./Upload";
 
-interface UploadWithModalProps extends UploadProps {}
+interface UploadWithModalProps extends UploadProps {
+  resourceType?: "url" | "file" | "base64";
+}
 
-export const UploadWithModal: FunctionComponent<UploadWithModalProps> = ({ onPress, value, ...uploadProps }: UploadWithModalProps) => {
+export const UploadWithModal: FunctionComponent<UploadWithModalProps> = ({
+  onPress,
+  resourceType,
+  value,
+  ...uploadProps
+}: UploadWithModalProps) => {
   const [viewFile, setViewFile] = useState<boolean>(false);
 
   const handleViewFile = () => {
@@ -46,8 +53,10 @@ export const UploadWithModal: FunctionComponent<UploadWithModalProps> = ({ onPre
     setViewFile(false);
   };
 
-  const fileSize = value !== undefined && value.size !== undefined ? `${(value.size / BYTE_TO_MEGABYTE).toFixed(2).toString()}MB` : "";
-
+  const valueSize = value !== undefined && value.size !== undefined ? value.size : -1;
+  const fileSizeUnit = valueSize >= 100000 ? "MB" : "KB";
+  const fileSize = valueSize >= 100000 ? valueSize / BYTE_TO_MEGABYTE : valueSize / BYTE_TO_KILOBYTE;
+  const fileSizeLabel = valueSize !== -1 ? `${fileSize.toFixed(2)}${fileSizeUnit}` : "";
   const viewImageHeader: ViewStyle = {
     ...flexRow,
     ...centerVertical,
@@ -55,7 +64,6 @@ export const UploadWithModal: FunctionComponent<UploadWithModalProps> = ({ onPre
     ...px(sw40),
     height: sh32,
   };
-
   const headerBGColor = value !== undefined && value.type === "application/pdf" ? { backgroundColor: colorWhite._1 } : {};
   const headerTextColor = value !== undefined && value.type === "application/pdf" ? colorBlack._2 : colorWhite._1;
   const headerTextStyle: TextStyle = { ...fs12BoldWhite1, color: headerTextColor };
@@ -63,6 +71,35 @@ export const UploadWithModal: FunctionComponent<UploadWithModalProps> = ({ onPre
   const imageStyle: ImageStyle = { height: sh500, resizeMode: "contain", width: sw750 };
 
   const modalAnimationInTiming = value !== undefined && value.type === "application/pdf" ? 0 : 450;
+
+  let pdfValue = "";
+  let isFileValid = false;
+  let imageValue: ImageSourcePropType = [];
+
+  if (resourceType === "file" && value !== undefined && value.path !== undefined) {
+    pdfValue = value.path;
+    isFileValid = true;
+    imageValue = { uri: value.path };
+  }
+
+  if (resourceType === "base64" && value !== undefined && value.base64 !== undefined) {
+    pdfValue = value.base64;
+    isFileValid = true;
+    const base64String = `data:${value.type};base64,${value.base64}`;
+    imageValue = { uri: base64String };
+  }
+
+  if (resourceType === undefined && value !== undefined && value.path !== undefined) {
+    pdfValue = value.base64 !== undefined ? value.base64 : "";
+    isFileValid = true;
+    imageValue = { uri: value.path };
+  }
+
+  if (resourceType === undefined && value !== undefined && value.url !== undefined) {
+    pdfValue = value.url;
+    isFileValid = true;
+    imageValue = { uri: value.url };
+  }
 
   return (
     <Fragment>
@@ -74,21 +111,30 @@ export const UploadWithModal: FunctionComponent<UploadWithModalProps> = ({ onPre
               <View style={headerStyle}>
                 <CustomSpacer space={sh40} />
                 <View style={viewImageHeader}>
-                  <LabeledTitle label={value.name} labelStyle={headerTextStyle} title={fileSize} titleStyle={headerTextStyle} />
+                  <LabeledTitle label={value.name} labelStyle={headerTextStyle} title={fileSizeLabel} titleStyle={headerTextStyle} />
                   <CustomFlexSpacer />
                   <IcoMoon color={headerTextColor} name="close" onPress={handleCloseFile} size={sh24} />
                 </View>
                 <CustomSpacer space={sh24} />
               </View>
-              {value.type === "application/pdf" && value.base64 !== undefined ? (
-                <View style={flexChild}>
-                  <CustomSpacer space={sh96} />
-                  <PDFView fadeInDuration={350} style={{ ...fullHW, ...flexChild }} resource={value.base64} resourceType="base64" />
-                </View>
-              ) : (
-                <View style={{ ...centerHV, ...fullHW, backgroundColor: colorBlack._1_85 }}>
-                  <Image source={{ uri: value.path }} style={imageStyle} />
-                </View>
+              {isFileValid === false ? null : (
+                <Fragment>
+                  {value.type === "application/pdf" ? (
+                    <View style={flexChild}>
+                      <CustomSpacer space={sh96} />
+                      <PDFView
+                        fadeInDuration={350}
+                        style={{ ...fullHW, ...flexChild }}
+                        resource={pdfValue}
+                        resourceType={resourceType || "base64"}
+                      />
+                    </View>
+                  ) : (
+                    <View style={{ ...centerHV, ...fullHW, backgroundColor: colorBlack._1_85 }}>
+                      <Image source={imageValue} style={imageStyle} />
+                    </View>
+                  )}
+                </Fragment>
               )}
             </View>
           ) : (

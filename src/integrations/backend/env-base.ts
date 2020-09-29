0eddd353979a91d9622301422ccd14f7bcbf095b
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-29 13:59:07 +0800

    added aws cognito config., x-api-key. graphql url. IAM url

---
+++ src/integrations/backend/env-base.ts
@@ -0,0 +1,11 @@
+export const baseURL = "https://ukrr3zhuxvf27mfbafp3qing6q.appsync-api.ap-southeast-1.amazonaws.com/";
+export const configBaseURL = "https://4w895f9gak.execute-api.ap-southeast-1.amazonaws.com/";
+
+export const X_API_KEY = "nvtORmuuhZ35UzQNhsqs41OfDS7D2UXx22VqlQNK";
+
+export const COGNITO_CONFIG = {
+  aws_cognito_region: "ap-southeast-1",
+  aws_user_pools_id: "ap-southeast-1_AWYEhim4C",
+  aws_user_pools_web_client_id: "2gemjbqdmba9av5i7n8nkdnam9",
+  aws_cognito_identity_pool_id: "ap-southeast-1:15dbb055-d600-4daa-92b0-d8cce48142fc",
+};

7c04f544502265bc1eb99121fdca5ab95b377b16
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-29 14:00:23 +0800

    updated integration index

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -1,5 +1,7 @@
 export * from "./async-storage";
+export * from "./backend";
 export * from "./file-system";
+export * from "./graphql";
 export * from "./react-native-document-picker";
 export * from "./react-native-image-crop-picker";
 export * from "./react-native-share";

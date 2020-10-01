ca0cad7b9f8ed53319a2266ccf3c8b215b658745
Aldrin Castro <aldrin.castro@regovtech.com>
2020-10-01 18:30:14 +0800

    updated RadioButon style

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -3,6 +3,7 @@ import { Text, TextStyle, TouchableWithoutFeedback, View } from "react-native";
 
 import {
   centerVertical,
+  circle,
   circleBorder,
   colorBlue,
   colorRed,
@@ -10,9 +11,11 @@ import {
   flexRow,
   fs12BoldBlack2,
   sw1,
-  sw24,
+  sw12,
+  sw14,
+  sw16,
+  sw4,
   sw48,
-  sw6,
   sw8,
 } from "../../styles";
 import { CustomSpacer } from "../Views/Spacer";
@@ -27,13 +30,16 @@ interface RadioButtonProps {
 
 export const RadioButton = ({ label, labelStyle, right, selected, setSelected }: RadioButtonProps) => {
   const borderColor = selected ? colorRed._1 : colorBlue._3_8;
-  const borderWidth = selected ? sw6 : sw1;
+  const borderWidth = selected ? sw4 : sw1;
+  const circleSize = selected ? sw8 : sw14;
 
   return (
     <TouchableWithoutFeedback onPress={setSelected}>
       <View style={{ ...centerVertical, ...flexRow }}>
-        <View style={circleBorder(sw24, borderWidth, borderColor, colorWhite._1)} />
-        <CustomSpacer space={sw8} isHorizontal={true} />
+        <View style={circleBorder(sw16, borderWidth, borderColor, colorWhite._1)}>
+          <View style={circle(circleSize)} />
+        </View>
+        <CustomSpacer space={sw12} isHorizontal={true} />
         <Text style={{ ...fs12BoldBlack2, minWidth: sw48, ...labelStyle }}>{label}</Text>
         {right}
       </View>

83dac3bd7852c1deb883db0bf74e94bdd89af4db
Aldrin Castro <aldrin.castro@regovtech.com>
2020-10-02 15:39:28 +0800

    added more date formats

--- src/constants/formats/date.ts
+++ src/constants/formats/date.ts
@@ -2,4 +2,6 @@ export const DEFAULT_DATE_FORMAT = "MM/DD/YY";
 export const DEFAULT_TIME_FORMAT = "h:mm A";
 
 export const DATE_OF_BIRTH_FORMAT = "MM/DD/YYYY";
+export const DAY_FORMAT = "dddd";
+export const FULL_DATE_FORMAT = "DD MMMM YYYY";
 export const PAYMENT_DATE_FORMAT = "DD/MM/YYYY";

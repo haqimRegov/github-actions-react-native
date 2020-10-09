8183e9368a4627463030e840239e980e4dc730cb
Aldrin Castro <aldrin.castro@regovtech.com>
2020-10-09 18:09:33 +0800

    added new style prop

--- src/components/Badge/Count.tsx
+++ src/components/Badge/Count.tsx
@@ -5,16 +5,17 @@ import { centerHV, colorRed, fs10BoldWhite1, sh16, sw24, sw4 } from "../../style
 
 export interface BadgeCountProps {
   count: number;
+  countStyle?: ViewStyle;
   style?: ViewStyle;
 }
 
-export const BadgeCount: FunctionComponent<BadgeCountProps> = ({ count, style }: BadgeCountProps) => {
+export const BadgeCount: FunctionComponent<BadgeCountProps> = ({ count, countStyle, style }: BadgeCountProps) => {
   const badgeStyle: ViewStyle = { ...centerHV, backgroundColor: colorRed._1, borderRadius: sw4, height: sh16, width: sw24, ...style };
   return count === 0 ? (
     <View />
   ) : (
     <View style={badgeStyle}>
-      <Text style={fs10BoldWhite1}>{count}</Text>
+      <Text style={{ ...fs10BoldWhite1, ...countStyle }}>{count}</Text>
     </View>
   );
 };

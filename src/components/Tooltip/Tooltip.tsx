240022fe74f493afcdd6eead84eb4ebc52154c09
Aldrin Castro <aldrin.castro@regovtech.com>
2020-10-12 13:20:32 +0800

    updated tool tip component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,40 +1,44 @@
-import React, { ReactElement, ReactNode, useState } from "react";
+import React, { FunctionComponent, ReactElement, ReactNode, useState } from "react";
 import { TouchableWithoutFeedback, ViewStyle } from "react-native";
 import Tooltip from "react-native-walkthrough-tooltip";
 
-import { colorBlack, colorTransparent, px, py, sh16, sw16, sw208, sw8 } from "../../styles";
+import { IcoMoon } from "../../icons";
+import { colorBlack, colorTransparent, px, py, sh16, sh24, sw16, sw208, sw8 } from "../../styles";
 
-export interface TooltipProps {
+export interface CustomTooltipProps {
   arrowStyle?: ViewStyle;
-  backgroundColor?: string;
-  children: ReactNode;
+  children?: ReactNode;
   content: ReactElement;
   contentStyle?: ViewStyle;
   insets?: Object;
   isVisible?: boolean;
   onClose?: () => void;
   onPress?: () => void;
+  overlayColor?: string;
   placement?: "top" | "bottom" | "left" | "right";
-  shadow?: boolean;
   showChild?: boolean;
   tooltipStyle?: ViewStyle;
+  withShadow?: boolean;
 }
 
-export const TooltipComponent = ({
+export const CustomTooltip: FunctionComponent<CustomTooltipProps> = ({
   arrowStyle,
-  backgroundColor,
   children,
   content,
   contentStyle,
   insets,
   onClose,
   onPress,
+  overlayColor,
   placement,
   isVisible,
-  shadow,
   showChild,
   tooltipStyle,
-}: TooltipProps) => {
+  withShadow,
+}: CustomTooltipProps) => {
+  // TODO known library issues:
+  // TODO tooltip flickers when opening inside a scroll view. It gets the last position before scrolling.
+
   const [popup, setPopup] = useState<boolean>(false);
 
   const handleShow = () => {
@@ -55,31 +59,36 @@ export const TooltipComponent = ({
 
   const visible = isVisible !== undefined ? isVisible : popup;
   const defaultContentStyle: ViewStyle = {
-    backgroundColor: colorBlack._2,
-    borderRadius: sw8,
     ...px(sw16),
     ...py(sh16),
+    backgroundColor: colorBlack._2,
+    borderRadius: sw8,
     width: sw208,
     ...contentStyle,
   };
   const defaultArrowStyle: ViewStyle = { borderTopColor: colorBlack._2, ...arrowStyle };
   const defaultInsets = { left: 0, right: 0, top: 0, bottom: 0, ...insets };
-  const defaultBackgroundColor = backgroundColor !== undefined ? backgroundColor : colorTransparent;
+  const defaultOverlayColor = overlayColor !== undefined ? overlayColor : colorTransparent;
   const defaultPlacement = placement !== undefined ? placement : "right";
+  const defaultShadow = withShadow !== undefined ? withShadow : true;
+  const defaultShowChild = showChild !== undefined ? showChild : false;
+
   return (
     <Tooltip
-      isVisible={visible}
-      contentStyle={defaultContentStyle}
       arrowStyle={defaultArrowStyle}
+      backgroundColor={defaultOverlayColor}
       content={content}
-      placement={defaultPlacement}
+      contentStyle={defaultContentStyle}
+      disableShadow={defaultShadow}
+      displayInsets={defaultInsets}
+      isVisible={visible}
       onClose={handleHide}
-      tooltipStyle={tooltipStyle}
-      backgroundColor={defaultBackgroundColor}
-      disableShadow={shadow}
-      showChildInTooltip={showChild}
-      displayInsets={defaultInsets}>
-      <TouchableWithoutFeedback onPress={handleShow}>{children}</TouchableWithoutFeedback>
+      placement={defaultPlacement}
+      showChildInTooltip={defaultShowChild}
+      tooltipStyle={tooltipStyle}>
+      <TouchableWithoutFeedback onPress={handleShow}>
+        {children !== undefined ? children : <IcoMoon name="info" size={sh24} />}
+      </TouchableWithoutFeedback>
     </Tooltip>
   );
 };

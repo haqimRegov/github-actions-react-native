670a55fcbbddf3fcbb6d29741e2a15cf316df4a3
Aldrin Castro <aldrin.castro@regovtech.com>
2020-10-15 14:39:36 +0800

    added decrypt function

--- src/utils/Encryption.ts
+++ src/utils/Encryption.ts
@@ -1,6 +1,12 @@
 import CryptoJS from "react-native-crypto-js";
 
-export const Encrypt = async (text: string, token: string) => {
-  const encryptedPassword = CryptoJS.AES.encrypt(text, token).toString();
-  return encryptedPassword;
+export const Encrypt = (text: string, token: string) => {
+  const encryptedText = CryptoJS.AES.encrypt(text, token).toString();
+  return encryptedText;
+};
+
+export const Decrypt = (text: string, token: string) => {
+  const bytes = CryptoJS.AES.decrypt(text, token);
+  const decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+  return decryptedText;
 };

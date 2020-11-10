82ed6ea3901c67ca2c3b8c2517aaed342a67c887
Nithin <johnnithin08@gmail.com>
2020-11-10 14:34:32 +0800

    update: terms and conditions page

--- src/pages/Onboarding/TermsAndConditions/index.tsx
+++ src/pages/Onboarding/TermsAndConditions/index.tsx
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import { OrderSummaryMapDispatchToProps, OrderSummaryMapStateToProps, OrderSummaryStoreProps } from "../../../store";
 import { flexChild } from "../../../styles";
 import { EditPdf } from "./EditPDF";
-import { Signature } from "./Signature";
+import { PDFList } from "./PDFList";
 import { TermsAndConditions } from "./TermsAndConditions";
 
 interface TermsAndConditionsPageProps extends OrderSummaryStoreProps {
@@ -20,7 +20,7 @@ const initialData = {
   index: 0,
 };
 
-export const TermsAndConditionPagesComponent = ({ handleNextStep, orders }: TermsAndConditionsPageProps) => {
+export const TermsAndConditionPagesComponent = ({ addOrders, handleNextStep, orders }: TermsAndConditionsPageProps) => {
   const [page, setPage] = useState<number>(0);
   const [initialPdfArray, setInitialPdfArray] = useState<FileBase64[]>([]);
   const [pdfList, setPdfList] = useState<PdfWithSignature[]>([]);
@@ -38,13 +38,13 @@ export const TermsAndConditionPagesComponent = ({ handleNextStep, orders }: Term
   let content: JSX.Element = <View />;
   switch (page) {
     case 1:
-      content = <Signature {...props} />;
+      content = <PDFList {...props} />;
       break;
     case 2:
       content = <EditPdf {...props} />;
       break;
     default:
-      content = <TermsAndConditions orders={orders} setPage={setPage} />;
+      content = <TermsAndConditions addOrders={addOrders} orders={orders} setPage={setPage} />;
   }
   return <View style={flexChild}>{content}</View>;
 };

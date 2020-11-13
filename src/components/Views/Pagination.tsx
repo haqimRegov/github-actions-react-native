b2932173970e6c82626c2a34ddad8c1c992f6e7f
Aldrin Castro <aldrin.castro@regovtech.com>
2020-11-13 20:51:49 +0800

    updated Pagination

--- src/components/Views/Pagination.tsx
+++ src/components/Views/Pagination.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Fragment, FunctionComponent } from "react";
 import { Text, View, ViewStyle } from "react-native";
 
 import { Language } from "../../constants";
-import { centerVertical, circleBorder, colorBlack, colorGray, flexRow, fs10RegBlue25, sh24, sw1, sw16, sw40, sw8 } from "../../styles";
+import {
+  alignSelfCenter,
+  centerVertical,
+  circleBorder,
+  colorBlack,
+  colorGray,
+  flexRow,
+  fs10RegBlue25,
+  fs12SemiBoldGray8,
+  sh24,
+  sh40,
+  sw1,
+  sw16,
+  sw40,
+  sw8,
+} from "../../styles";
 import { IconButton } from "../Touchables/Icon";
 import { CustomSpacer } from "./Spacer";
 
@@ -10,27 +25,44 @@ const { PAGINATION } = Language.PAGE;
 
 interface PaginationProps {
   itemsPerPage: number;
+  label?: boolean;
   onPressNext: () => void;
   onPressPrev: () => void;
   page: number;
   totalItems: number;
+  totalPages: number;
 }
 
-export const Pagination = ({ itemsPerPage, onPressNext, onPressPrev, page, totalItems }: PaginationProps) => {
+export const Pagination: FunctionComponent<PaginationProps> = ({
+  itemsPerPage,
+  label,
+  onPressNext,
+  onPressPrev,
+  page,
+  totalItems,
+  totalPages,
+}: PaginationProps) => {
   const buttonStyle: ViewStyle = circleBorder(sw40, sw1, colorGray._3);
   const currentMinItems = page * itemsPerPage - itemsPerPage + 1;
   const currentMaxItems = page * itemsPerPage;
   const lastItem = currentMaxItems > totalItems ? totalItems : currentMaxItems;
   const currentItems = `${currentMinItems}-${lastItem}`;
+  const pageCounter = `${page}/${totalPages}`;
 
-  const label = `${PAGINATION.LABEL_SHOWING} ${currentItems} ${PAGINATION.LABEL_OUT_OF} ${totalItems} ${PAGINATION.LABEL_RESULTS}`;
+  const showListLabel = `${PAGINATION.LABEL_SHOWING} ${currentItems} ${PAGINATION.LABEL_OUT_OF} ${totalItems} ${PAGINATION.LABEL_RESULTS}`;
 
   return (
-    <View style={{ ...centerVertical, ...flexRow }}>
-      <Text style={fs10RegBlue25}>{label}</Text>
-      <CustomSpacer isHorizontal={true} space={sw16} />
+    <View style={{ ...centerVertical, ...flexRow, height: sh40 }}>
+      {label === false ? null : (
+        <Fragment>
+          <Text style={fs10RegBlue25}>{showListLabel}</Text>
+          <CustomSpacer isHorizontal={true} space={sw16} />
+        </Fragment>
+      )}
       <View style={flexRow}>
         <IconButton color={colorBlack._1} name="caret-left" onPress={onPressPrev} size={sh24} style={buttonStyle} />
+        <CustomSpacer isHorizontal={true} space={sw8} />
+        <Text style={{ ...fs12SemiBoldGray8, ...alignSelfCenter }}>{pageCounter}</Text>
         <CustomSpacer isHorizontal={true} space={sw8} />
         <IconButton color={colorBlack._1} name="caret-right" onPress={onPressNext} size={sh24} style={buttonStyle} />
       </View>

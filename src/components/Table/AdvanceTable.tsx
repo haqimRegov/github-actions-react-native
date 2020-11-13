62198e1c0a615904d4548732291e0d943076017b
Aldrin Castro <aldrin.castro@regovtech.com>
2020-11-13 20:53:06 +0800

    added RenderCustomItem and some enhancements

--- src/components/Table/AdvanceTable.tsx
+++ src/components/Table/AdvanceTable.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { View } from "react-native";
 
-import { sh16 } from "../../styles";
+import { sh8 } from "../../styles";
 import { CustomSpacer } from "../Views/Spacer";
 import { AdvanceTableRow } from "./AdvanceTableRow";
 import { TableHeader } from "./TableHeader";
@@ -10,8 +10,10 @@ interface AdvanceTableProps {
   activeAccordion?: number[];
   columns: ITableColumn[];
   data: ITableData[];
+  handleRowNavigation?: (item: ITableData) => void;
   onRowSelect?: (record: ITableData[]) => void;
   RenderAccordion?: (record: ITableData, index: number) => JSX.Element;
+  RenderCustomItem?: (customItem: ITableCustomItem) => JSX.Element;
   RenderOptions?: (props: ITableOptions) => JSX.Element;
   rowSelection?: ITableData[];
   rowSelectionLabel?: string;
@@ -22,8 +24,10 @@ export const AdvanceTable: FunctionComponent<AdvanceTableProps> = ({
   activeAccordion,
   columns,
   data,
+  handleRowNavigation,
   onRowSelect,
   RenderAccordion,
+  RenderCustomItem,
   RenderOptions,
   rowSelection,
   rowSelectionLabel,
@@ -32,17 +36,20 @@ export const AdvanceTable: FunctionComponent<AdvanceTableProps> = ({
   return (
     <View>
       <TableHeader columns={columns} rowSelectionLabel={rowSelectionLabel} withActions={RenderOptions !== undefined} />
-      <CustomSpacer space={spaceToHeader || sh16} />
+      <CustomSpacer space={spaceToHeader || sh8} />
       {data.map((row: ITableData, index: number) => {
         return (
           <AdvanceTableRow
-            key={index}
             activeAccordion={activeAccordion}
             columns={columns}
-            RenderAccordion={RenderAccordion}
+            handleRowNavigation={handleRowNavigation}
             index={index}
             item={row}
+            key={index}
+            lastIndex={index === data.length - 1}
             onRowSelect={onRowSelect}
+            RenderAccordion={RenderAccordion}
+            RenderCustomItem={RenderCustomItem}
             RenderOptions={RenderOptions}
             rowSelection={rowSelection}
           />

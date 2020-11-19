593e3114bd2c514e200075f8e0dd900a0795d1ae
Nithin <johnnithin08@gmail.com>
2020-11-19 11:34:21 +0800

    update: divided edit pdf to view and functions

--- src/pages/Onboarding/TermsAndConditions/EditPDF.tsx
+++ src/pages/Onboarding/TermsAndConditions/EditPDF.tsx
@@ -3,76 +3,46 @@ import { Buffer } from "buffer";
 import moment from "moment";
 import { PDFDocument, rgb } from "pdf-lib/cjs";
 import React, { FunctionComponent, useEffect, useState } from "react";
-import { Dimensions, GestureResponderEvent, Platform, ScrollView, Text, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
-import PDFView from "react-native-view-pdf";
+import { Dimensions, GestureResponderEvent, Platform, ScrollView } from "react-native";
 import { connect } from "react-redux";
 
-import { CustomSpacer, IconButton, SignatureModal } from "../../../components";
 import { Base64 } from "../../../constants";
 import { Language } from "../../../constants/language";
-import { IcoMoon } from "../../../icons";
 import { ReactFileSystem } from "../../../integrations/file-system/functions";
 import { ClientMapDispatchToProps, ClientMapStateToProps } from "../../../store";
 import {
-  absolutePosition,
-  centerHV,
-  centerVertical,
-  circleBorder,
-  colorBlack,
-  colorBlue,
-  colorGray,
-  colorTransparent,
-  colorWhite,
-  flexChild,
-  flexGrow,
-  flexRow,
-  fs12RegBlue1,
-  fs24BoldBlack2,
-  px,
   sh100,
   sh12,
-  sh132,
   sh15,
   sh155,
-  sh1600,
   sh220,
   sh25,
-  sh26,
   sh30,
   sh300,
-  sh32,
-  sh34,
   sh370,
-  sh4,
   sh40,
   sh800,
   sh90,
-  shadowBlue5,
-  sw1,
-  sw145,
   sw180,
   sw20,
   sw200,
-  sw24,
   sw245,
   sw25,
   sw275,
-  sw33,
   sw470,
-  sw595,
-  sw8,
-  sw80,
-  sw82,
-  sw84,
 } from "../../../styles";
 import { GetEmbeddedBase64 } from "../../../utils";
+import { PdfView, Signer } from "./EditPDFView";
 import { PDFListProps } from "./PDFList";
 
 const { TERMS_AND_CONDITIONS } = Language.PAGE;
 
-const adviserSignPosition = { x: sw20, y: sh155 };
-const principalSignPosition = { x: sw275, y: sh155 };
-const jointSignPosition = { x: sw20, y: sh300 };
+const signPosition = {
+  adviser: { x: sw20, y: sh155 },
+  principal: { x: sw275, y: sh155 },
+  joint: { x: sw20, y: sh300 },
+};
+
 export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
   accountType,
   currentPdf,
@@ -86,11 +56,11 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
   const [jointSignature, setJointSignature] = useState<string>("");
   const [numberOfPdfPages, setNumberOfPdfPages] = useState<number>(1);
   const [showSignPdf, setShowSignPdf] = useState<boolean>(false);
-  const [signer, setSigner] = useState<number>();
+  const [signer, setSigner] = useState<Signer>(undefined);
   const [scrollRef, setScrollRef] = useState<ScrollView | null>(null);
 
   const modifyPdf = async (value: string) => {
-    if (value !== "" && currentPdf !== undefined) {
+    if (value !== "" && currentPdf !== undefined && signer !== undefined) {
       const dataUri = GetEmbeddedBase64(currentPdf.pdf);
       const loadPdf = await PDFDocument.load(dataUri);
       const fileData = await ReactFileSystem.readFileMainBundle("NunitoSans-SemiBold.ttf");
@@ -102,120 +72,74 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
       const pages = loadPdf.getPages();
       const selectedPage = pages[numberOfPdfPages - 1];
       const { height } = selectedPage.getSize();
-      const position = { x: 0, y: 0 };
+      const whiteBGConfig = {
+        height: sh40,
+        opacity: 1,
+        width: sw180,
+        x: signPosition[signer].x,
+        y: height - signPosition[signer].y,
+      };
       const textPosition = { x: 0, y: 0 };
-      if (signer === 0) {
-        position.x = adviserSignPosition.x;
-        position.y = adviserSignPosition.y;
+      if (signer === "adviser") {
         if (principalSignature === "") {
-          textPosition.x = principalSignPosition.x;
-          textPosition.y = principalSignPosition.y;
-          selectedPage.moveTo(principalSignPosition.x, height - principalSignPosition.y + sh30);
-          selectedPage.drawImage(whiteImage, {
-            height: sh40,
-            opacity: 1,
-            width: sw180,
-            x: principalSignPosition.x,
-            y: height - principalSignPosition.y,
-          });
+          textPosition.x = signPosition.principal.x;
+          textPosition.y = signPosition.principal.y;
         } else if (accountType === "Joint" && jointSignature === "") {
-          textPosition.x = jointSignPosition.x;
-          textPosition.y = jointSignPosition.y;
-          selectedPage.drawImage(whiteImage, {
-            height: sh40,
-            opacity: 1,
-            width: sw180,
-            x: jointSignPosition.x,
-            y: height - jointSignPosition.y,
-          });
-          selectedPage.moveTo(jointSignPosition.x, height - jointSignPosition.y + sh30);
+          textPosition.x = signPosition.joint.x;
+          textPosition.y = signPosition.joint.y;
         }
-      } else if (signer === 1) {
-        position.x = principalSignPosition.x;
-        position.y = principalSignPosition.y;
+      } else if (signer === "principal") {
         if (adviserSignature === "") {
-          textPosition.x = adviserSignPosition.x;
-          textPosition.y = adviserSignPosition.y;
-          selectedPage.drawImage(whiteImage, {
-            height: sh40,
-            opacity: 1,
-            width: sw180,
-            x: adviserSignPosition.x,
-            y: height - adviserSignPosition.y,
-          });
-          selectedPage.moveTo(adviserSignPosition.x, height - adviserSignPosition.y + sh30);
+          textPosition.x = signPosition.adviser.x;
+          textPosition.y = signPosition.adviser.y;
         } else if (accountType === "Joint" && jointSignature === "") {
-          textPosition.x = jointSignPosition.x;
-          textPosition.y = jointSignPosition.y;
-          selectedPage.drawImage(whiteImage, {
-            height: sh40,
-            opacity: 1,
-            width: sw180,
-            x: jointSignPosition.x,
-            y: height - jointSignPosition.y,
-          });
-          selectedPage.moveTo(jointSignPosition.x, height - jointSignPosition.y + sh30);
+          textPosition.x = signPosition.joint.x;
+          textPosition.y = signPosition.joint.y;
         }
-      } else if (signer === 2) {
-        position.x = jointSignPosition.x;
-        position.y = jointSignPosition.y;
+      } else if (signer === "joint") {
         if (adviserSignature === "") {
-          textPosition.x = adviserSignPosition.x;
-          textPosition.y = adviserSignPosition.y;
-          selectedPage.drawImage(whiteImage, {
-            height: sh40,
-            opacity: 1,
-            width: sw180,
-            x: adviserSignPosition.x,
-            y: height - adviserSignPosition.y,
-          });
-          selectedPage.moveTo(adviserSignPosition.x, height - adviserSignPosition.y + sh30);
+          textPosition.x = signPosition.adviser.x;
+          textPosition.y = signPosition.adviser.y;
         } else if (principalSignature === "") {
-          textPosition.x = principalSignPosition.x;
-          textPosition.y = principalSignPosition.y;
-          selectedPage.drawImage(whiteImage, {
-            height: sh40,
-            opacity: 1,
-            width: sw180,
-            x: principalSignPosition.x,
-            y: height - principalSignPosition.y,
-          });
-          selectedPage.moveTo(principalSignPosition.x, height - principalSignPosition.y + sh30);
+          textPosition.x = signPosition.principal.x;
+          textPosition.y = signPosition.principal.y;
         }
       }
+
+      selectedPage.moveTo(textPosition.x, height - textPosition.y + sh30);
+      selectedPage.drawImage(whiteImage, whiteBGConfig);
+      selectedPage.drawImage(whiteImage, {
+        ...whiteBGConfig,
+        x: textPosition.x,
+        y: height - textPosition.y,
+      });
+
       const svgPath = [
         "M772.378 753.020h-487.099c-31.605 0-57.306-25.702-57.306-57.306s25.702-57.306 57.306-57.306h57.306c15.816 0 28.653-12.834 28.653-28.651s-12.837-28.655-28.653-28.655h-57.306c-63.209 0-114.612 51.405-114.612 114.611 0 63.211 51.403 114.611 114.612 114.611h487.099c15.817 0 28.655-12.834 28.655-28.651s-12.838-28.655-28.655-28.655z",
         "M821.312 245.708c-42.752-42.693-112.32-42.75-155.157 0l-43.84 43.84c-0.542 0.459-1.203 0.631-1.715 1.146-0.516 0.516-0.691 1.175-1.148 1.719l-147.017 147.016c-28.309 28.369-43.9 66.074-43.9 106.163v64.179c0 15.817 12.838 28.655 28.655 28.655h64.183c40.085 0 77.79-15.586 106.214-43.925l193.694-193.694c42.722-42.75 42.722-112.349 0.030-155.099zM587.102 553.954c-17.562 17.51-40.917 27.166-65.728 27.166h-35.533v-35.529c0-24.815 9.655-48.166 27.136-65.702l128.162-128.166 74.099 74.096-128.137 128.135zM780.766 360.291l-25.011 25.014-74.099-74.096 24.986-24.985c20.429-20.401 53.696-20.372 74.095 0 20.403 20.429 20.403 53.666 0.030 74.067z",
       ];
       selectedPage.drawSvgPath(svgPath[0], { color: rgb(0, 0.537, 0.925), scale: 0.02 });
       selectedPage.drawSvgPath(svgPath[1], { color: rgb(0, 0.537, 0.925), scale: 0.02 });
-      selectedPage.drawText("Sign Document Now", {
+      selectedPage.drawText(TERMS_AND_CONDITIONS.LABEL_SIGN_NOW, {
         x: textPosition.x + sw25,
         y: height - textPosition.y + sh15,
         size: textHeight,
         font: customFont,
         color: rgb(0, 0.537, 0.925),
       });
-      selectedPage.drawImage(whiteImage, {
-        height: sh40,
-        opacity: 1,
-        width: sw180,
-        x: position.x,
-        y: height - position.y,
-      });
       if (Platform.OS === "ios") {
         selectedPage.drawImage(signatureImage, {
           height: sh40,
           width: sw180,
-          x: position.x,
-          y: height - position.y,
+          x: signPosition[signer].x,
+          y: height - signPosition[signer].y,
         });
       } else {
         selectedPage.drawImage(signatureImage, {
           height: sh100,
           width: sw200,
-          x: (selectedPage.getWidth() * position.x) / Dimensions.get("window").width,
-          y: selectedPage.getHeight() - (selectedPage.getHeight() * position.y) / Dimensions.get("window").height - sh25,
+          x: (selectedPage.getWidth() * signPosition[signer].x) / Dimensions.get("window").width,
+          y: selectedPage.getHeight() - (selectedPage.getHeight() * signPosition[signer].y) / Dimensions.get("window").height - sh25,
         });
       }
       const pdfBytes = await loadPdf.save();
@@ -239,20 +163,20 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
     }
     if (coordinateY > sh90 && coordinateY < sh220) {
       if (coordinateX < sw245 && coordinateX > 0) {
-        setSigner(0);
+        setSigner("adviser");
       } else if (coordinateX > sw245 && coordinateX < sw470) {
-        setSigner(1);
+        setSigner("principal");
       }
       setShowSignPdf(true);
-    } else if (coordinateX < sw245 && coordinateY > sh220 && coordinateY < sh370) {
-      setSigner(2);
+    } else if (coordinateX < sw245 && coordinateY > sh220 && coordinateY < sh370 && accountType === "Joint") {
+      setSigner("joint");
       setShowSignPdf(true);
     }
   };
 
   const handlePosition = (e: GestureResponderEvent) => {
     const { locationY, locationX } = e.nativeEvent;
-    const positionY = locationY - (numberOfPdfPages - 1) * 800;
+    const positionY = locationY - (numberOfPdfPages - 1) * sh800;
     calculatePosition(locationX, positionY);
   };
 
@@ -289,27 +213,27 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
   const handleConfirm = () => {
     if (showSignPdf === true) {
       setTimeout(() => {
-        if (signer === 0) {
+        if (signer === "adviser") {
           if (principalSignature === "") {
-            setSigner(1);
-          } else if (principalSignature !== "" && accountType === "Joint") {
-            setSigner(2);
+            setSigner("principal");
+          } else if (principalSignature !== "" && accountType === "Joint" && jointSignature === "") {
+            setSigner("joint");
           } else {
             setShowSignPdf(false);
           }
-        } else if (signer === 1) {
+        } else if (signer === "principal") {
           if (adviserSignature === "") {
-            setSigner(0);
+            setSigner("adviser");
           } else if (adviserSignature !== "" && accountType === "Joint" && jointSignature === "") {
-            setSigner(2);
+            setSigner("joint");
           } else {
             setShowSignPdf(false);
           }
-        } else if (signer === 2) {
+        } else if (signer === "joint") {
           if (adviserSignature === "") {
-            setSigner(0);
+            setSigner("adviser");
           } else if (principalSignature === "") {
-            setSigner(1);
+            setSigner("principal");
           } else {
             setShowSignPdf(false);
           }
@@ -325,11 +249,11 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
   };
 
   const handleSignature = (value: string) => {
-    if (signer === 0) {
+    if (signer === "adviser") {
       setAdviserSignature(value);
-    } else if (signer === 1) {
+    } else if (signer === "principal") {
       setPrincipalSignature(value);
-    } else if (signer === 2) {
+    } else if (signer === "joint") {
       setJointSignature(value);
     }
     modifyPdf(value);
@@ -340,11 +264,11 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
       scrollRef.scrollToEnd();
       if (signer === undefined) {
         if (adviserSignature === "") {
-          setSigner(0);
+          setSigner("adviser");
         } else if (principalSignature === "") {
-          setSigner(1);
+          setSigner("principal");
         } else {
-          setSigner(2);
+          setSigner("joint");
         }
       } else {
         setTimeout(() => {
@@ -353,42 +277,27 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
       }
     }
   };
-
-  let signerLabel = "";
-  let signatureToDisplay = "";
-  if (signer !== undefined) {
-    switch (signer) {
-      case 0:
-        signerLabel = TERMS_AND_CONDITIONS.LABEL_ADVISER_SIGNATURE;
-        signatureToDisplay = adviserSignature;
-        break;
-      case 1:
-        signerLabel = TERMS_AND_CONDITIONS.LABEL_PRINCIPAL_SIGNATURE;
-        signatureToDisplay = principalSignature;
-        break;
-      case 2:
-        signerLabel = TERMS_AND_CONDITIONS.LABEL_JOINT_SIGNATURE;
-        signatureToDisplay = jointSignature;
-        break;
-      default:
-        signerLabel = TERMS_AND_CONDITIONS.LABEL_ADVISER_SIGNATURE;
-        break;
-    }
-  }
-
-  const pdfViewContainer: ViewStyle = { ...px(sw8), width: sw595, height: numberOfPdfPages * sh800 };
-  const pdfContainer: ViewStyle = { height: numberOfPdfPages * sh1600 }; // To display the page number correctly in the viewer
-  const toolTipStyle: ViewStyle = { ...absolutePosition, zIndex: 1, top: -25, width: sw84 };
-  const toolTipLabelStyle: ViewStyle = {
-    ...centerHV,
-    position: "absolute",
-    backgroundColor: colorWhite._1,
-    left: sw1,
-    right: sw1,
-    width: sw82,
-    height: sh26,
-    borderRadius: sw8,
-    top: 1,
+  const completed =
+    accountType === "Individual"
+      ? adviserSignature !== "" && principalSignature !== ""
+      : adviserSignature !== "" && principalSignature !== "" && jointSignature !== "";
+  const props = {
+    adviserSignature: adviserSignature,
+    completed: completed,
+    currentPdf: currentPdf,
+    principalSignature: principalSignature,
+    jointSignature: jointSignature,
+    numberOfPdfPages: numberOfPdfPages,
+    showSignPdf: showSignPdf,
+    signer: signer,
+    scrollRef: scrollRef,
+    handleScroll: handleScroll,
+    handleBack: handleBack,
+    handleSignature: handleSignature,
+    handleClose: handleClose,
+    handleConfirm: handleConfirm,
+    handlePosition: handlePosition,
+    setScrollRef: setScrollRef,
   };
 
   useEffect(() => {
@@ -421,72 +330,15 @@ export const EditPdfComponent: FunctionComponent<PDFListProps> = ({
     }
   }, [signer]);
 
-  return (
-    <View style={flexChild}>
-      <View style={flexRow}>
-        <View>
-          <ScrollView
-            ref={setScrollRef}
-            contentContainerStyle={flexGrow}
-            keyboardShouldPersistTaps="handled"
-            showsVerticalScrollIndicator={false}>
-            <View style={flexRow}>
-              <View>
-                <CustomSpacer space={sh32} />
-                <View style={px(sw24)}>
-                  <View style={{ ...centerVertical, ...flexRow }}>
-                    <IcoMoon color={colorBlack._1} name="arrow-left" onPress={handleBack} size={sw24} />
-                    <CustomSpacer isHorizontal={true} space={sw20} />
-                    <Text style={fs24BoldBlack2}>{currentPdf?.pdf.name}</Text>
-                  </View>
-                </View>
-                <CustomSpacer space={sh4} />
-                <TouchableWithoutFeedback onPress={handlePosition}>
-                  <View style={pdfViewContainer}>
-                    <View pointerEvents="none">
-                      <PDFView fadeInDuration={350} style={pdfContainer} resource={currentPdf.pdf.base64!} resourceType="base64" />
-                    </View>
-                  </View>
-                </TouchableWithoutFeedback>
-              </View>
-              <CustomSpacer isHorizontal={true} space={sw145} />
-            </View>
-          </ScrollView>
-        </View>
-        <View>
-          <View style={{ ...absolutePosition, top: sh132 }}>
-            <View style={toolTipStyle}>
-              <View style={toolTipLabelStyle}>
-                <Text style={{ ...fs12RegBlue1 }}>{TERMS_AND_CONDITIONS.LABEL_ADD_SIGN}</Text>
-              </View>
-              <IcoMoon
-                style={{ ...shadowBlue5, backgroundColor: colorTransparent }}
-                color={colorGray._1}
-                name="filter-tooltip"
-                size={sh34}
-              />
-            </View>
-            <IconButton
-              color={colorBlue._2}
-              name="sign"
-              onPress={handleScroll}
-              size={sw33}
-              style={circleBorder(sw80, sw1, colorGray._3, colorWhite._1)}
-            />
-          </View>
-        </View>
-      </View>
+  useEffect(() => {
+    if (scrollRef !== null && completed === true) {
+      setTimeout(() => {
+        scrollRef.scrollToEnd();
+      }, 1000);
+    }
+  }, [completed]);
 
-      <SignatureModal
-        handleClose={handleClose}
-        handleConfirm={handleConfirm}
-        handleSignature={handleSignature}
-        signature={signatureToDisplay}
-        title={signerLabel}
-        visible={showSignPdf}
-      />
-    </View>
-  );
+  return <PdfView {...props} />;
 };
 
 export const EditPdf = connect(ClientMapStateToProps, ClientMapDispatchToProps)(EditPdfComponent);
---
+++ src/pages/Onboarding/TermsAndConditions/EditPDFView.tsx
@@ -0,0 +1,205 @@
+import React, { FunctionComponent } from "react";
+import { GestureResponderEvent, ScrollView, Text, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
+import PDFView from "react-native-view-pdf";
+
+import { CustomSpacer, IconButton, RoundedButton, SignatureModal } from "../../../components";
+import { Language } from "../../../constants/language";
+import { IcoMoon } from "../../../icons";
+import {
+  absolutePosition,
+  centerHV,
+  centerVertical,
+  circleBorder,
+  colorBlack,
+  colorBlue,
+  colorGray,
+  colorTransparent,
+  colorWhite,
+  flexChild,
+  flexGrow,
+  flexRow,
+  fs12RegBlue1,
+  fs24BoldBlack2,
+  px,
+  sh1,
+  sh130,
+  sh14,
+  sh1600,
+  sh24,
+  sh26,
+  sh32,
+  sh34,
+  sh4,
+  sh40,
+  sh800,
+  sh92,
+  shadowBlue5,
+  sw1,
+  sw20,
+  sw24,
+  sw33,
+  sw595,
+  sw8,
+  sw80,
+  sw82,
+  sw84,
+  sw96,
+} from "../../../styles";
+
+const { TERMS_AND_CONDITIONS } = Language.PAGE;
+
+export type Signer = "adviser" | "principal" | "joint" | undefined;
+
+declare interface PDFViewProps {
+  adviserSignature: string;
+  completed: boolean;
+  currentPdf: PdfWithIndex;
+  principalSignature: string;
+  jointSignature: string;
+  numberOfPdfPages: number;
+  showSignPdf: boolean;
+  signer: Signer;
+  handleScroll: () => void;
+  handleBack: () => void;
+  handleSignature: (signature: string) => void;
+  handleClose: () => void;
+  handleConfirm: () => void;
+  handlePosition: (e: GestureResponderEvent) => void;
+  setScrollRef: (ref: ScrollView) => void;
+}
+
+export const PdfView: FunctionComponent<PDFViewProps> = ({
+  adviserSignature,
+  currentPdf,
+  completed,
+  principalSignature,
+  jointSignature,
+  numberOfPdfPages,
+  showSignPdf,
+  signer,
+  handleScroll,
+  handleBack,
+  handleSignature,
+  handleClose,
+  handleConfirm,
+  handlePosition,
+  setScrollRef,
+}: PDFViewProps) => {
+  let signerLabel = "";
+  let signatureToDisplay = "";
+
+  if (signer !== undefined) {
+    switch (signer) {
+      case "adviser":
+        signerLabel = TERMS_AND_CONDITIONS.LABEL_ADVISER_SIGNATURE;
+        signatureToDisplay = adviserSignature;
+        break;
+      case "principal":
+        signerLabel = TERMS_AND_CONDITIONS.LABEL_PRINCIPAL_SIGNATURE;
+        signatureToDisplay = principalSignature;
+        break;
+      case "joint":
+        signerLabel = TERMS_AND_CONDITIONS.LABEL_JOINT_SIGNATURE;
+        signatureToDisplay = jointSignature;
+        break;
+      default:
+        signerLabel = TERMS_AND_CONDITIONS.LABEL_ADVISER_SIGNATURE;
+        break;
+    }
+  }
+  const pdfViewContainer: ViewStyle = { ...px(sw8), width: sw595, height: numberOfPdfPages * sh800 };
+  const pdfContainer: ViewStyle = { height: numberOfPdfPages * sh1600 }; // To display the page number correctly in the viewer
+  const toolTipStyle: ViewStyle = { ...absolutePosition, zIndex: 1, top: sh14, width: sw84, left: sw20 };
+  const toolTipLabelStyle: ViewStyle = {
+    ...centerHV,
+    position: "absolute",
+    backgroundColor: colorWhite._1,
+    left: sw1,
+    right: sw1,
+    width: sw82,
+    height: sh26,
+    borderRadius: sw8,
+    top: sh1,
+  };
+
+  return (
+    <View style={flexChild}>
+      <View style={flexRow}>
+        <View>
+          <ScrollView
+            ref={setScrollRef}
+            contentContainerStyle={flexGrow}
+            keyboardShouldPersistTaps="handled"
+            showsVerticalScrollIndicator={false}>
+            <View>
+              <View style={flexRow}>
+                <View>
+                  <CustomSpacer space={sh32} />
+                  <View style={px(sw24)}>
+                    <View style={{ ...centerVertical, ...flexRow }}>
+                      <IcoMoon color={colorBlack._1} name="arrow-left" onPress={handleBack} size={sw24} />
+                      <CustomSpacer isHorizontal={true} space={sw20} />
+                      <Text style={fs24BoldBlack2}>{currentPdf?.pdf.name}</Text>
+                    </View>
+                  </View>
+                  <CustomSpacer space={sh4} />
+                  <TouchableWithoutFeedback onPress={handlePosition}>
+                    <View style={pdfViewContainer}>
+                      <View pointerEvents="none">
+                        <PDFView fadeInDuration={350} style={pdfContainer} resource={currentPdf.pdf.base64!} resourceType="base64" />
+                      </View>
+                    </View>
+                  </TouchableWithoutFeedback>
+                </View>
+                <CustomSpacer isHorizontal={true} space={sw96} />
+              </View>
+              {completed === true ? (
+                <View style={px(sw20)}>
+                  <CustomSpacer space={sh24} />
+                  <RoundedButton onPress={handleBack} text={TERMS_AND_CONDITIONS.BUTTON_CONTINUE} />
+                  <CustomSpacer space={sh130} />
+                </View>
+              ) : null}
+            </View>
+          </ScrollView>
+        </View>
+        {completed === false ? (
+          <View>
+            <TouchableWithoutFeedback onPress={handleScroll}>
+              <View style={{ ...absolutePosition, top: sh92, ...px(sw20) }}>
+                <CustomSpacer space={sh40} />
+                <View style={toolTipStyle}>
+                  <View style={toolTipLabelStyle}>
+                    <Text style={{ ...fs12RegBlue1 }}>{TERMS_AND_CONDITIONS.LABEL_ADD_SIGN}</Text>
+                  </View>
+                  <IcoMoon
+                    style={{ ...shadowBlue5, backgroundColor: colorTransparent }}
+                    color={colorGray._1}
+                    name="filter-tooltip"
+                    size={sh34}
+                  />
+                </View>
+                <IconButton
+                  color={colorBlue._2}
+                  name="sign"
+                  onPress={handleScroll}
+                  size={sw33}
+                  style={circleBorder(sw80, sw1, colorGray._3, colorWhite._1)}
+                />
+                <CustomSpacer space={sh40} />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        ) : null}
+      </View>
+      <SignatureModal
+        handleClose={handleClose}
+        handleConfirm={handleConfirm}
+        handleSignature={handleSignature}
+        signature={signatureToDisplay}
+        title={signerLabel}
+        visible={showSignPdf}
+      />
+    </View>
+  );
+};

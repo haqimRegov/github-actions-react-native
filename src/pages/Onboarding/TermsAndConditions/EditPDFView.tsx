44c0a1ac0e21b97fc5a7de4fbe15a2d545fe8bef
Aldrin Castro <aldrin.castro@regovtech.com>
2020-11-19 14:32:38 +0800

    enhanced add sign button

--- src/pages/Onboarding/TermsAndConditions/EditPDFView.tsx
+++ src/pages/Onboarding/TermsAndConditions/EditPDFView.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from "react";
 import { GestureResponderEvent, ScrollView, Text, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 import PDFView from "react-native-view-pdf";
 
-import { CustomSpacer, IconButton, RoundedButton, SignatureModal } from "../../../components";
+import { CustomSpacer, IconButton, RoundedButton, SafeAreaPage, SignatureModal } from "../../../components";
 import { Language } from "../../../constants/language";
 import { IcoMoon } from "../../../icons";
 import {
@@ -15,34 +15,32 @@ import {
   colorGray,
   colorTransparent,
   colorWhite,
-  flexChild,
   flexGrow,
   flexRow,
   fs12RegBlue1,
   fs24BoldBlack2,
   px,
   sh1,
-  sh130,
-  sh14,
+  sh120,
   sh1600,
   sh24,
   sh26,
   sh32,
   sh34,
   sh4,
-  sh40,
+  sh56,
+  sh8,
   sh800,
-  sh92,
   shadowBlue5,
   sw1,
   sw20,
   sw24,
   sw33,
+  sw40,
   sw595,
   sw8,
   sw80,
   sw82,
-  sw84,
   sw96,
 } from "../../../styles";
 
@@ -109,7 +107,7 @@ export const PdfView: FunctionComponent<PDFViewProps> = ({
   }
   const pdfViewContainer: ViewStyle = { ...px(sw8), width: sw595, height: numberOfPdfPages * sh800 };
   const pdfContainer: ViewStyle = { height: numberOfPdfPages * sh1600 }; // To display the page number correctly in the viewer
-  const toolTipStyle: ViewStyle = { ...absolutePosition, zIndex: 1, top: sh14, width: sw84, left: sw20 };
+  const toolTipStyle: ViewStyle = { top: sh8, zIndex: 1 };
   const toolTipLabelStyle: ViewStyle = {
     ...centerHV,
     position: "absolute",
@@ -123,73 +121,67 @@ export const PdfView: FunctionComponent<PDFViewProps> = ({
   };
 
   return (
-    <View style={flexChild}>
-      <View style={flexRow}>
-        <View>
-          <ScrollView
-            ref={setScrollRef}
-            contentContainerStyle={flexGrow}
-            keyboardShouldPersistTaps="handled"
-            showsVerticalScrollIndicator={false}>
-            <View>
-              <View style={flexRow}>
-                <View>
-                  <CustomSpacer space={sh32} />
-                  <View style={px(sw24)}>
-                    <View style={{ ...centerVertical, ...flexRow }}>
-                      <IcoMoon color={colorBlack._1} name="arrow-left" onPress={handleBack} size={sw24} />
-                      <CustomSpacer isHorizontal={true} space={sw20} />
-                      <Text style={fs24BoldBlack2}>{currentPdf?.pdf.name}</Text>
-                    </View>
-                  </View>
-                  <CustomSpacer space={sh4} />
-                  <TouchableWithoutFeedback onPress={handlePosition}>
-                    <View style={pdfViewContainer}>
-                      <View pointerEvents="none">
-                        <PDFView fadeInDuration={350} style={pdfContainer} resource={currentPdf.pdf.base64!} resourceType="base64" />
-                      </View>
-                    </View>
-                  </TouchableWithoutFeedback>
-                </View>
-                <CustomSpacer isHorizontal={true} space={sw96} />
-              </View>
-              {completed === true ? (
-                <View style={px(sw20)}>
-                  <CustomSpacer space={sh24} />
-                  <RoundedButton onPress={handleBack} text={TERMS_AND_CONDITIONS.BUTTON_CONTINUE} />
-                  <CustomSpacer space={sh130} />
-                </View>
-              ) : null}
-            </View>
-          </ScrollView>
-        </View>
-        {completed === false ? (
+    <SafeAreaPage>
+      <View>
+        <ScrollView
+          ref={setScrollRef}
+          contentContainerStyle={flexGrow}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}>
           <View>
-            <TouchableWithoutFeedback onPress={handleScroll}>
-              <View style={{ ...absolutePosition, top: sh92, ...px(sw20) }}>
-                <CustomSpacer space={sh40} />
-                <View style={toolTipStyle}>
-                  <View style={toolTipLabelStyle}>
-                    <Text style={{ ...fs12RegBlue1 }}>{TERMS_AND_CONDITIONS.LABEL_ADD_SIGN}</Text>
+            <View style={flexRow}>
+              <View>
+                <CustomSpacer space={sh32} />
+                <View style={px(sw24)}>
+                  <View style={{ ...centerVertical, ...flexRow }}>
+                    <IcoMoon color={colorBlack._1} name="arrow-left" onPress={handleBack} size={sw24} />
+                    <CustomSpacer isHorizontal={true} space={sw20} />
+                    <Text style={fs24BoldBlack2}>{currentPdf?.pdf.name}</Text>
                   </View>
-                  <IcoMoon
-                    style={{ ...shadowBlue5, backgroundColor: colorTransparent }}
-                    color={colorGray._1}
-                    name="filter-tooltip"
-                    size={sh34}
-                  />
                 </View>
-                <IconButton
-                  color={colorBlue._2}
-                  name="sign"
-                  onPress={handleScroll}
-                  size={sw33}
-                  style={circleBorder(sw80, sw1, colorGray._3, colorWhite._1)}
-                />
-                <CustomSpacer space={sh40} />
+                <CustomSpacer space={sh4} />
+                <TouchableWithoutFeedback onPress={handlePosition}>
+                  <View style={pdfViewContainer}>
+                    <View pointerEvents="none">
+                      <PDFView fadeInDuration={350} style={pdfContainer} resource={currentPdf.pdf.base64!} resourceType="base64" />
+                    </View>
+                  </View>
+                </TouchableWithoutFeedback>
               </View>
-            </TouchableWithoutFeedback>
+              <CustomSpacer isHorizontal={true} space={sw96} />
+            </View>
+            {completed === true ? (
+              <View style={px(sw20)}>
+                <CustomSpacer space={sh24} />
+                <RoundedButton onPress={handleBack} text={TERMS_AND_CONDITIONS.BUTTON_CONTINUE} />
+                <CustomSpacer space={sh56} />
+              </View>
+            ) : null}
           </View>
+        </ScrollView>
+        {completed === false ? (
+          <TouchableWithoutFeedback onPress={handleScroll}>
+            <View style={{ ...absolutePosition, right: sw40, top: sh120 }}>
+              <View style={toolTipStyle}>
+                <View style={toolTipLabelStyle}>
+                  <Text style={fs12RegBlue1}>{TERMS_AND_CONDITIONS.LABEL_ADD_SIGN}</Text>
+                </View>
+                <IcoMoon
+                  style={{ ...shadowBlue5, backgroundColor: colorTransparent }}
+                  color={colorGray._1}
+                  name="filter-tooltip"
+                  size={sh34}
+                />
+              </View>
+              <IconButton
+                color={colorBlue._2}
+                name="sign"
+                onPress={handleScroll}
+                size={sw33}
+                style={circleBorder(sw80, sw1, colorGray._3, colorWhite._1)}
+              />
+            </View>
+          </TouchableWithoutFeedback>
         ) : null}
       </View>
       <SignatureModal
@@ -200,6 +192,6 @@ export const PdfView: FunctionComponent<PDFViewProps> = ({
         title={signerLabel}
         visible={showSignPdf}
       />
-    </View>
+    </SafeAreaPage>
   );
 };

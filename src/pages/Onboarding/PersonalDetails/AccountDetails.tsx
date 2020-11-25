7162f9d3102ad0e5366fea36f282377c479803f7
Aldrin Castro <aldrin.castro@regovtech.com>
2020-11-25 20:11:53 +0800

    updated joint account details

--- src/pages/Onboarding/PersonalDetails/AccountDetails.tsx
+++ src/pages/Onboarding/PersonalDetails/AccountDetails.tsx
@@ -1,9 +1,9 @@
 import React, { Fragment, FunctionComponent } from "react";
-import { View } from "react-native";
+import { Text, View, ViewStyle } from "react-native";
 
-import { CustomSpacer, LabeledTitle, RadioButtonGroup, TextSpaceArea } from "../../../components";
+import { AccountHeader, CustomSpacer, RadioButtonGroup, TextSpaceArea } from "../../../components";
 import { Language } from "../../../constants";
-import { borderBottomBlack21, fs10BoldBlack2, fs16SemiBoldBlack2, fs24BoldBlack2, px, sh16, sh24, sh32, sw24, sw48 } from "../../../styles";
+import { borderBottomBlack21, fs16SemiBoldBlack2, fs24BoldBlack2, px, sh16, sh24, sh32, sh48, sw24, sw48 } from "../../../styles";
 
 const { PERSONAL_DETAILS } = Language.PAGE;
 
@@ -32,39 +32,41 @@ export const AccountDetails: FunctionComponent<AccountDetailsProps> = ({
     accountType === "Joint"
       ? `${personalInfo.principal!.personalDetails!.name!} ${PERSONAL_DETAILS.LABEL_AND} ${personalInfo.joint!.personalDetails!.name!}`
       : "";
-
+  const subtitleLabel = `${PERSONAL_DETAILS.LABEL_PRINCIPAL} ${PERSONAL_DETAILS.LABEL_AND} ${PERSONAL_DETAILS.LABEL_JOINT}`;
   const space = accountType === "Joint" ? sh24 : 0;
+  const padding: ViewStyle = accountType === "Joint" ? px(sw24) : {};
+  const spaceToAdditional = accountType === "Joint" ? sh48 : sh32;
 
   return (
     <Fragment>
       <CustomSpacer space={sh32} />
       <View style={borderBottomBlack21} />
+      <CustomSpacer space={spaceToAdditional} />
       <View style={px(sw24)}>
-        <CustomSpacer space={sh32} />
-        {accountType === "Individual" ? null : (
-          <LabeledTitle label={names} labelStyle={fs10BoldBlack2} title={PERSONAL_DETAILS.LABEL_ADDITIONAL} titleStyle={fs24BoldBlack2} />
-        )}
-        <TextSpaceArea spaceToBottom={sh16} spaceToTop={space} style={fs16SemiBoldBlack2} text={PERSONAL_DETAILS.LABEL_DISTRIBUTION} />
-        <RadioButtonGroup
-          direction="row"
-          options={[PERSONAL_DETAILS.OPTION_DISTRIBUTION_PAYOUT, PERSONAL_DETAILS.OPTION_DISTRIBUTION_REINVEST]}
-          selected={inputDistribution}
-          setSelected={setInputDistribution}
-          space={sw48}
-        />
-        {accountType === "Individual" ? null : (
-          <Fragment>
-            <CustomSpacer space={sh32} />
-            <TextSpaceArea spaceToBottom={sh16} spaceToTop={sh32} style={fs16SemiBoldBlack2} text={PERSONAL_DETAILS.LABEL_SIGN} />
-            <RadioButtonGroup
-              direction="row"
-              options={OPTION_SIGN}
-              selected={inputSignatory}
-              setSelected={setInputSignatory}
-              space={sw48}
-            />
-          </Fragment>
-        )}
+        {accountType === "Individual" ? null : <AccountHeader subtitle={subtitleLabel} title={names} />}
+        <View style={padding}>
+          {accountType === "Joint" ? <Text style={fs24BoldBlack2}>{PERSONAL_DETAILS.LABEL_ADDITIONAL}</Text> : null}
+          <TextSpaceArea spaceToBottom={sh16} spaceToTop={space} style={fs16SemiBoldBlack2} text={PERSONAL_DETAILS.LABEL_DISTRIBUTION} />
+          <RadioButtonGroup
+            direction="row"
+            options={[PERSONAL_DETAILS.OPTION_DISTRIBUTION_PAYOUT, PERSONAL_DETAILS.OPTION_DISTRIBUTION_REINVEST]}
+            selected={inputDistribution}
+            setSelected={setInputDistribution}
+            space={sw48}
+          />
+          {accountType === "Individual" ? null : (
+            <Fragment>
+              <TextSpaceArea spaceToBottom={sh16} spaceToTop={sh32} style={fs16SemiBoldBlack2} text={PERSONAL_DETAILS.LABEL_SIGN} />
+              <RadioButtonGroup
+                direction="row"
+                options={OPTION_SIGN}
+                selected={inputSignatory}
+                setSelected={setInputSignatory}
+                space={sw48}
+              />
+            </Fragment>
+          )}
+        </View>
       </View>
     </Fragment>
   );

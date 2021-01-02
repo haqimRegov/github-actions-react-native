fe23c7a488d1a2bf848f070295b24f64b4db04a1
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-02 20:20:14 +0800

    updated data dictionary

---
+++ src/data/dictionary/kib-bank-accounts.ts
@@ -0,0 +1,51 @@
+export const DICTIONARY_KIB_BANK_ACCOUNTS: IKibBankAccounts[] = [
+  {
+    currency: "MYR",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Account",
+    bankAccountNumber: "312143587267",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+  {
+    currency: "EURO",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Account",
+    bankAccountNumber: "312193740360",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+  {
+    currency: "AUD",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Trust Account",
+    bankAccountNumber: "312194472378",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+  {
+    currency: "SGD",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Trust Account",
+    bankAccountNumber: "312194472351",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+  {
+    currency: "JPY",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Trust Account",
+    bankAccountNumber: "312194472343",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+  {
+    currency: "USD",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Trust Account",
+    bankAccountNumber: "312194472335",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+  {
+    currency: "GBP",
+    bankName: "Standard Chartered",
+    bankAccountName: "Kenanga Investors Berhad - Collection Trust Account",
+    bankAccountNumber: "312193179235",
+    bankSwiftCode: "SCBLMYKXXXX",
+  },
+];

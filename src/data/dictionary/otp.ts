4bda0f2f592a62ec500940c47e6596a73ac37a80
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-04 19:43:13 +0800

    updated temporary otp cool off

--- src/data/dictionary/otp.ts
+++ src/data/dictionary/otp.ts
@@ -1,5 +1,5 @@
 // TODO temporary actual 300
-export const DICTIONARY_OTP_COOL_OFF = 60;
+export const DICTIONARY_OTP_COOL_OFF = 120;
 export const DICTIONARY_OTP_EXPIRY = 120;
 export const DICTIONARY_OTP_LENGTH = 6;
 export const DICTIONARY_OTP_MAX_RETRY = 3;

5eed5356f38163a5a1bfa4a086e2ec25f9172165
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-05 10:37:57 +0800

    added kib links

--- src/data/dictionary/links.ts
+++ src/data/dictionary/links.ts
@@ -1,1 +1,3 @@
 export const DICTIONARY_LINK_TAX_RESIDENT = "http://lampiran.hasil.gov.my/pdf/pdfam/3656.pdf";
+export const DICTIONARY_LINK_PRIVACY = "https://www.kenangainvestors.com.my/KIB/KIB.Application.Web/KIBfundsdefault/PrivacyPolicy.aspx";
+export const DICTIONARY_LINK_TERMS = "https://www.kenangainvestors.com.my/KIB/KIB.Application.Web/KIBFundsDefault/TermsAndConditions.aspx";

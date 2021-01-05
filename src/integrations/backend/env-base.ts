a4f874f72c8de4626f3719b23351531de26daaf0
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-05 19:03:04 +0800

    cognito config for sit

--- src/integrations/backend/env-base.ts
+++ src/integrations/backend/env-base.ts
@@ -15,9 +15,16 @@ export const { baseURL, configBaseURL } = api[env];
 
 export const X_API_KEY = "nvtORmuuhZ35UzQNhsqs41OfDS7D2UXx22VqlQNK";
 
+// export const COGNITO_CONFIG_DEV = {
+//   aws_cognito_region: "ap-southeast-1",
+//   aws_user_pools_id: "ap-southeast-1_AWYEhim4C",
+//   aws_user_pools_web_client_id: "2gemjbqdmba9av5i7n8nkdnam9",
+//   aws_cognito_identity_pool_id: "ap-southeast-1:15dbb055-d600-4daa-92b0-d8cce48142fc",
+// };
+
 export const COGNITO_CONFIG = {
   aws_cognito_region: "ap-southeast-1",
-  aws_user_pools_id: "ap-southeast-1_AWYEhim4C",
-  aws_user_pools_web_client_id: "2gemjbqdmba9av5i7n8nkdnam9",
-  aws_cognito_identity_pool_id: "ap-southeast-1:15dbb055-d600-4daa-92b0-d8cce48142fc",
+  aws_user_pools_id: "ap-southeast-1_EOa6qSu6R",
+  aws_user_pools_web_client_id: "3objln3im1icdndh1qui7v7i29",
+  aws_cognito_identity_pool_id: "ap-southeast-1:2d1e3a4b-2f0a-4ed0-bffc-fa81999080f7",
 };

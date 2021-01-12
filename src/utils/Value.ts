48e2cbe28412c0184054059ad757af3030f5a8ad
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-12 18:59:43 +0800

    updated utils

--- src/utils/Value.ts
+++ src/utils/Value.ts
@@ -34,3 +34,10 @@ export const isObjectEqual = (firstObject: object, secondObject: object) => {
 
   return equal;
 };
+
+export const titleCaseString = (text: string) => {
+  return text
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase())
+    .join(" ");
+};

1b85ad835b013b216562e3217b3b001a0e44337c
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-14 21:18:19 +0800

    updated data dictionary

--- src/data/dictionary/epf.ts
+++ src/data/dictionary/epf.ts
@@ -2,3 +2,6 @@ export const DICTIONARY_EPF_TYPE: TypeEpfLabelValue[] = [
   { label: "Conventional", value: "Conventional" },
   { label: "Shariah", value: "Shariah" },
 ];
+
+// EPF Maximum Age in Months (54 years and 11 months old)
+export const DICTIONARY_EPF_AGE = 659;

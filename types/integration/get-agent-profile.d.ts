c79c74775ef1a55251d43990213b7afdbd75ba5e
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-20 19:04:24 +0800

    updated types

---
+++ types/integration/get-agent-profile.d.ts
@@ -0,0 +1,46 @@
+declare interface IGetAgentProfileRequest {}
+
+declare interface IGetAgentProfileMessage {
+  createdOn: string;
+  isRead: boolean;
+  message: string;
+  notificationId: string;
+  searchKey: string;
+  searchType: string;
+  senderName: string;
+  source: AvatarType;
+  title: string;
+  updatedAt: string;
+}
+
+declare interface IGetAgentProfileResult {
+  address: {
+    address: string;
+    city: string;
+    postCode: string;
+    state: string;
+    country: string;
+  };
+  agency: string;
+  agentCode: string;
+  agentId: string;
+  agentName: string;
+  bdmEmail: string;
+  bdmName: string;
+  branchName: string;
+  channel: string;
+  email: string;
+  licenseCode: string;
+  mobileNo: string;
+  nric: string;
+  omniEnabled: string;
+  rank: string;
+  region: string;
+  status: string;
+}
+
+declare type IGetAgentProfileResponse = IQueryResponse<IGetAgentProfileResult> | undefined;
+
+declare interface IGetAgentProfileQuery {
+  agentProfile: IGetAgentProfileResponse;
+}

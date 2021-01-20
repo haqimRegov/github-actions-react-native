61e3cd59b71615730c2dca29a5526c80a62786f5
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-20 14:29:54 +0800

    integrated inbox

--- src/pages/Dashboard/Inbox.tsx
+++ src/pages/Dashboard/Inbox.tsx
@@ -1,12 +1,16 @@
 import moment from "moment";
 import React, { Fragment, FunctionComponent, useEffect, useState } from "react";
-import { Alert, ScrollView, View, ViewStyle } from "react-native";
+import { ActivityIndicator, Alert, ScrollView, View, ViewStyle } from "react-native";
+import { connect } from "react-redux";
 
-import { CollapsibleHeader, CustomFlexSpacer, CustomSpacer, NotificationList, Pagination, Tab } from "../../components";
+import { AvatarProps, CollapsibleHeader, CustomFlexSpacer, CustomSpacer, NotificationList, Pagination, Tab } from "../../components";
 import { FULL_DATE_FORMAT, Language } from "../../constants";
-import { SAMPLE_INBOX } from "../../mocks";
+import { getInbox, updateInbox } from "../../network-actions";
+import { GlobalMapDispatchToProps, GlobalMapStateToProps, GlobalStoreProps } from "../../store";
 import {
   borderBottomBlack21,
+  centerHV,
+  colorGray,
   colorWhite,
   flexChild,
   flexGrow,
@@ -23,13 +27,19 @@ import {
 
 const { INBOX } = Language.PAGE;
 
-interface InboxPageProps {
+interface InboxPageProps extends GlobalStoreProps {
   navigation: IStackNavigationProp;
   handleRoute: (route: string) => void;
 }
 
-export const InboxPage: FunctionComponent<InboxPageProps> = () => {
-  const [inboxList, setInboxList] = useState<INotificationList[]>([]);
+const InboxPageComponent: FunctionComponent<InboxPageProps> = ({ unreadMessages, updatedUnreadMessages }: InboxPageProps) => {
+  const [inboxList, setInboxList] = useState<IInbox>({
+    notifications: [],
+    newMessageCount: "",
+    page: "1",
+    pages: "1",
+  });
+  const [initialLoading, setInitialLoading] = useState<boolean>(false);
   const [inputSearch, setInputSearch] = useState<string>("");
   const [filterVisible, setFilterVisible] = useState<boolean>(false);
 
@@ -37,16 +47,113 @@ export const InboxPage: FunctionComponent<InboxPageProps> = () => {
     setFilterVisible(!filterVisible);
   };
 
+  const handleFetch = async (newPage: string) => {
+    const request: IGetInboxRequest = { page: newPage, search: inputSearch };
+    // eslint-disable-next-line no-console
+    console.log("request", request);
+    const response: IGetInboxResponse = await getInbox(request);
+    if (response !== undefined) {
+      const { data, error } = response;
+      if (error === null && data !== null) {
+        // eslint-disable-next-line no-console
+        console.log("data", data);
+        let notificationBucket = {};
+        data.result.inbox.forEach((message: IGetInboxMessage) => {
+          const dateLabel = moment(message.createdOn, "x").format(FULL_DATE_FORMAT);
+          const item: INotificationItem = {
+            createdAt: message.createdOn,
+            id: message.notificationId,
+            isRead: message.isRead,
+            message: message.message,
+            title: message.title,
+            sender: message.senderName,
+            source: message.source,
+          };
+          if (notificationBucket[dateLabel]) {
+            notificationBucket[dateLabel].messages.push(item);
+          } else {
+            notificationBucket = { ...notificationBucket, [dateLabel]: { date: dateLabel, messages: [item] } };
+          }
+          return notificationBucket;
+        });
+
+        const newNotifications: INotificationList[] = Object.values(notificationBucket);
+        setInboxList({
+          notifications: newNotifications,
+          newMessageCount: data.result.newMessageCount,
+          page: data.result.page,
+          pages: data.result.pages,
+        });
+        updatedUnreadMessages(data.result.newMessageCount);
+      }
+
+      if (error !== null) {
+        Alert.alert(`${error.message} - ${error.errorList?.join(" ")}`);
+      }
+    }
+  };
+
+  const handleInitialFetch = async () => {
+    setInitialLoading(true);
+    await handleFetch("1");
+    setInitialLoading(false);
+  };
+
+  const handleSearch = async () => {
+    await handleFetch(inboxList.page);
+  };
+
+  const handleRead = async (id: string) => {
+    const request: IUpdateInboxRequest = { notificationIds: [id] };
+    // eslint-disable-next-line no-console
+    console.log("request", request);
+    const response: IUpdateInboxResponse = await updateInbox(request);
+    // setLoading(false);
+    if (response !== undefined) {
+      const { data, error } = response;
+      if (error === null && data !== null) {
+        // eslint-disable-next-line no-console
+        console.log("data", data);
+        handleFetch(inboxList.page);
+      }
+      if (error !== null) {
+        Alert.alert(`${error.message} - ${error.errorList?.join(" ")}`);
+      }
+    }
+  };
+
+  const handleMessage = async (notification: INotificationItem) => {
+    if (notification.isRead === false) {
+      await handleRead(notification.id);
+    }
+  };
+
   const handleNext = () => {
-    Alert.alert("handlePress");
+    if (inboxList !== undefined) {
+      const newPage = parseInt(inboxList.page, 10) + 1;
+      handleFetch(newPage.toString());
+    }
   };
 
   const handlePrev = () => {
-    Alert.alert("handlePress");
+    if (inboxList !== undefined) {
+      const newPage = parseInt(inboxList.page, 10) - 1;
+      handleFetch(newPage.toString());
+    }
   };
 
-  const handleMessage = () => {
-    // TODO integration
+  const handleAvatar = ({ sender, source }: INotificationItem) => {
+    const initials =
+      sender !== undefined
+        ? sender
+            .split(" ")
+            .filter((text) => text !== "")
+            .map((text, index) => (index < 2 ? text.substr(0, 1) : ""))
+            .join("")
+        : "OMNI";
+    const defaultInitials = sender === "system" ? "KIB" : initials;
+    const avatar: AvatarProps = { text: defaultInitials, type: source as AvatarType };
+    return avatar;
   };
 
   const cardStyle: ViewStyle = {
@@ -59,22 +166,14 @@ export const InboxPage: FunctionComponent<InboxPageProps> = () => {
   };
 
   useEffect(() => {
-    // disable lint because of reduce accumulator issue
-    /* eslint-disable no-param-reassign */
-    const messagesByDate = SAMPLE_INBOX.reduce((notifications, item) => {
-      const dateLabel = moment(item.createdAt).format(FULL_DATE_FORMAT);
-      if (notifications[dateLabel]) {
-        notifications[dateLabel].messages.push(item);
-      } else {
-        notifications = { ...notifications, [dateLabel]: { date: dateLabel, messages: [item] } };
-      }
-      return notifications;
-    }, {});
-
-    setInboxList(Object.values(messagesByDate));
+    handleInitialFetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const badgeCount = inboxList.newMessageCount === "" ? parseInt(unreadMessages!, 10) : parseInt(inboxList.newMessageCount, 10);
+
   return (
-    <ScrollView contentContainerStyle={flexGrow} keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
+    <ScrollView contentContainerStyle={flexGrow} showsVerticalScrollIndicator={false}>
       <View style={cardStyle}>
         <CustomSpacer space={sh153} />
         <CustomSpacer space={sh16} />
@@ -83,31 +182,46 @@ export const InboxPage: FunctionComponent<InboxPageProps> = () => {
           handleFilter={handleFilter}
           inputSearch={inputSearch}
           noFilter={true}
+          onSubmitEditing={handleSearch}
           placeholder={INBOX.PLACEHOLDER_SEARCH}
           label={INBOX.HEADER}
           setInputSearch={setInputSearch}
         />
         <View style={flexRow}>
-          <Tab selected={true} style={{ height: sh48 }} text={INBOX.TAB_NOTIFICATIONS} />
+          <Tab badgeCount={badgeCount} selected={true} style={{ height: sh48 }} text={INBOX.TAB_NOTIFICATIONS} />
           <CustomFlexSpacer />
-          <Pagination onPressNext={handleNext} onPressPrev={handlePrev} page={1} totalItems={36} totalPages={5} itemsPerPage={20} />
+          <Pagination
+            onPressNext={handleNext}
+            onPressPrev={handlePrev}
+            page={parseInt(inboxList.page, 10)}
+            totalPages={parseInt(inboxList.pages, 10)}
+          />
           <CustomSpacer isHorizontal={true} space={sw24} />
         </View>
         <View style={borderBottomBlack21} />
         <View style={{ ...px(sw24), ...flexChild }}>
           <CustomSpacer space={sh24} />
-          {inboxList.map((inbox: INotificationList, index: number) => {
-            const label = moment().diff(inbox.date, "days") === 0 ? "Today" : moment(inbox.date, FULL_DATE_FORMAT).format(FULL_DATE_FORMAT);
-            return (
-              <Fragment key={index}>
-                {index === 0 ? null : <CustomSpacer space={sh32} />}
-                <NotificationList items={inbox.messages} label={label} onPress={handleMessage} />
-              </Fragment>
-            );
-          })}
+          {initialLoading === true ? (
+            <View style={{ ...centerHV, ...flexChild }}>
+              <ActivityIndicator color={colorGray._7} size="small" />
+            </View>
+          ) : (
+            inboxList.notifications.map((inbox: INotificationList, index: number) => {
+              const label =
+                moment().diff(inbox.date, "days") === 0 ? "Today" : moment(inbox.date, FULL_DATE_FORMAT).format(FULL_DATE_FORMAT);
+              return (
+                <Fragment key={index}>
+                  {index === 0 ? null : <CustomSpacer space={sh32} />}
+                  <NotificationList avatarProps={handleAvatar} items={inbox.messages} label={label} onPress={handleMessage} />
+                </Fragment>
+              );
+            })
+          )}
         </View>
         <CustomSpacer space={sh24} />
       </View>
     </ScrollView>
   );
 };
+
+export const InboxPage = connect(GlobalMapStateToProps, GlobalMapDispatchToProps)(InboxPageComponent);

0f6bac4046910795064f4ec57f8deff143bf801d
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-21 19:46:25 +0800

    added empty state in table

--- src/components/Table/AdvanceTable.tsx
+++ src/components/Table/AdvanceTable.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { View } from "react-native";
 
-import { sh8 } from "../../styles";
+import { flexChild, sh8 } from "../../styles";
 import { CustomSpacer } from "../Views/Spacer";
 import { AdvanceTableRow } from "./AdvanceTableRow";
 import { TableHeader } from "./TableHeader";
@@ -15,14 +15,17 @@ export const AdvanceTable: FunctionComponent<AdvanceTableProps> = ({
   RenderAccordion,
   RenderCustomHeader,
   RenderCustomItem,
+  RenderEmptyState,
   RenderOptions,
   rowSelection,
   RowSelectionItem,
   rowSelectionKey,
   spaceToHeader,
 }: AdvanceTableProps) => {
+  const emptyState = RenderEmptyState !== undefined ? <RenderEmptyState /> : <View />;
+
   return (
-    <View>
+    <View style={flexChild}>
       <TableHeader
         columns={columns}
         RenderCustomHeader={RenderCustomHeader}
@@ -30,25 +33,27 @@ export const AdvanceTable: FunctionComponent<AdvanceTableProps> = ({
         withActions={RenderOptions !== undefined}
       />
       <CustomSpacer space={spaceToHeader || sh8} />
-      {data.map((row: ITableData, index: number) => {
-        return (
-          <AdvanceTableRow
-            activeAccordion={activeAccordion}
-            columns={columns}
-            handleRowNavigation={handleRowNavigation}
-            index={index}
-            item={row}
-            key={index}
-            lastIndex={index === data.length - 1}
-            onRowSelect={onRowSelect}
-            RenderAccordion={RenderAccordion}
-            RenderCustomItem={RenderCustomItem}
-            RenderOptions={RenderOptions}
-            rowSelection={rowSelection}
-            rowSelectionKey={rowSelectionKey}
-          />
-        );
-      })}
+      {data.length === 0
+        ? emptyState
+        : data.map((row: ITableData, index: number) => {
+            return (
+              <AdvanceTableRow
+                activeAccordion={activeAccordion}
+                columns={columns}
+                handleRowNavigation={handleRowNavigation}
+                index={index}
+                item={row}
+                key={index}
+                lastIndex={index === data.length - 1}
+                onRowSelect={onRowSelect}
+                RenderAccordion={RenderAccordion}
+                RenderCustomItem={RenderCustomItem}
+                RenderOptions={RenderOptions}
+                rowSelection={rowSelection}
+                rowSelectionKey={rowSelectionKey}
+              />
+            );
+          })}
     </View>
   );
 };

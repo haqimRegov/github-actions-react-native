9152734835e2ade171d917d5178987e92fe9105b
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-21 19:46:17 +0800

    updated household typo

--- src/data/dictionary/household-income.ts
+++ src/data/dictionary/household-income.ts
@@ -3,7 +3,7 @@ export const DICTIONARY_HOUSEHOLD_INCOME: TypeHouseholdIncomeLabelValue[] = [
   { label: "RM 1,501 - RM 3,000", value: "RM 1,501 - RM 3,000" },
   { label: "RM 3,001 - RM 5,000", value: "RM 3,001 - RM 5,000" },
   { label: "RM 5,001 - RM 8,000", value: "RM 5,001 - RM 8,000" },
-  { label: "RM 8,001 - RM 1,5000", value: "RM 8,001 - RM 1,5000" },
+  { label: "RM 8,001 - RM 15,000", value: "RM 8,001 - RM 15,000" },
   { label: "RM 15,001 - RM 20,000", value: "RM 15,001 - RM 20,000" },
   { label: "RM 20,001 - RM 50,000", value: "RM 20,001 - RM 50,000" },
   { label: "RM 50,001 - RM 100,000", value: "RM 50,001 - RM 100,000" },

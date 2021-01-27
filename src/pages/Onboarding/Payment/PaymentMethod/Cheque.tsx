fea0c277191bc559bbd68f83865811e4bb84fe0c
Aldrin Castro <aldrin.castro@regovtech.com>
2021-01-27 14:50:14 +0800

    updated bank list

--- src/pages/Onboarding/Payment/PaymentMethod/Cheque.tsx
+++ src/pages/Onboarding/Payment/PaymentMethod/Cheque.tsx
@@ -3,7 +3,7 @@ import { View } from "react-native";
 
 import { AdvancedDropdown, CustomDatePicker, CustomSpacer, CustomTextInput, LabeledTitle, TextSpaceArea } from "../../../../components";
 import { Language } from "../../../../constants";
-import { DICTIONARY_MALAYSIA_BANK } from "../../../../data/dictionary";
+import { DICTIONARY_MALAYSIA_BANK_BASE } from "../../../../data/dictionary";
 import { colorTransparent, flexRow, fs16BoldBlack2, px, sh143, sh24, sh32, sh8, sw16, sw24, sw360, sw48, sw64 } from "../../../../styles";
 
 const { PAYMENT } = Language.PAGE;
@@ -35,7 +35,12 @@ export const Cheque: FunctionComponent<ChequeProps> = ({
     <View>
       <View style={px(sw24)}>
         <View style={flexRow}>
-          <AdvancedDropdown items={DICTIONARY_MALAYSIA_BANK} handleChange={setBankName} label={PAYMENT.LABEL_BANK_NAME} value={bankName} />
+          <AdvancedDropdown
+            items={DICTIONARY_MALAYSIA_BANK_BASE}
+            handleChange={setBankName}
+            label={PAYMENT.LABEL_BANK_NAME}
+            value={bankName}
+          />
           <CustomSpacer isHorizontal={true} space={sw64} />
           <CustomTextInput label={PAYMENT.LABEL_CHEQUE_NO} onChangeText={setCheckNumber} value={checkNumber} />
         </View>

8c2834516545a6fa6c308f2e7a644e8c7a61b6b8
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-02 21:28:27 +0800

    implemented Dashboard Filter with layout animation

--- src/pages/Dashboard/Transactions/ApplicationHistory/Header.tsx
+++ src/pages/Dashboard/Transactions/ApplicationHistory/Header.tsx
@@ -1,9 +1,7 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Text, TextInput, TextStyle, View, ViewStyle } from "react-native";
-import Collapsible from "react-native-collapsible";
-import { ScrollView } from "react-native-gesture-handler";
 
-import { ActionButtons, ButtonSelectionList, CustomSpacer, IconButton, IconInput, LinkText } from "../../../../components";
+import { ActionButtons, CustomSpacer, IconButton, IconInput } from "../../../../components";
 import { Language } from "../../../../constants";
 import {
   centerHorizontal,
@@ -14,16 +12,15 @@ import {
   colorGray,
   colorTransparent,
   colorWhite,
-  flexGrow,
   flexRow,
   fs16SemiBoldBlack2,
   fs24BoldBlue2,
-  fullHeight,
   fullWidth,
   px,
   sh16,
   sh24,
   sh32,
+  sh40,
   sh48,
   shadowBlack5,
   sw1,
@@ -31,79 +28,89 @@ import {
   sw218,
   sw24,
   sw40,
-  sw729,
-  sw8,
 } from "../../../../styles";
+import { AnimationUtils } from "../../../../utils";
+import { TransactionsFilter } from "./Filter";
 
-const { PRODUCT_FILTER, DASHBOARD_HOME } = Language.PAGE;
+const { DASHBOARD_FILTER, DASHBOARD_HOME } = Language.PAGE;
 interface ApplicationHistoryHeaderProps {
+  activeTab: TransactionsTabType;
+  filter: ITransactionsFilter;
   filterVisible: boolean;
-  handleFilter: () => void;
+  handleCancel: () => void;
+  handleConfirm: () => void;
   handleSearch: () => void;
+  handleShowFilter: () => void;
   inputSearch: string;
-  selectedFilter: IProductFilter;
+  setFilter: (value: ITransactionsFilter) => void;
   setInputSearch: (value: string) => void;
-  setSelectedFilter: (value: IProductFilter) => void;
 }
 
 export const ApplicationHistoryHeader: FunctionComponent<ApplicationHistoryHeaderProps> = ({
+  activeTab,
   filterVisible,
-  handleFilter,
+  handleCancel,
+  handleShowFilter,
+  handleConfirm,
   handleSearch,
   inputSearch,
-  selectedFilter,
+  filter,
   setInputSearch,
-  setSelectedFilter,
+  setFilter,
 }: ApplicationHistoryHeaderProps) => {
   // TODO hide show all pills is only 1 line
   const [searchInputRef, setSearchInputRef] = useState<TextInput | null>(null);
-  const [showMorePills, setShowMorePills] = useState<boolean>(false);
-  const [filter, setFilter] = useState<IProductFilter>(selectedFilter);
+  // const [showMorePills, setShowMorePills] = useState<boolean>(false);
+  // const [filter, setFilter] = useState<ITransactionsFilter>(selectedFilter);
 
-  const filterValues = Object.values(selectedFilter)
-    .flat(1)
-    .filter((value) => value !== "");
+  // const filterValues = Object.values(filter)
+  //   .flat(1)
+  //   .filter((value) => value !== "");
 
   const handlePressFilter = () => {
     if (searchInputRef !== null) {
       searchInputRef.blur();
     }
-    handleFilter();
-  };
-
-  const handleShowAllFilter = () => {
-    setShowMorePills(!showMorePills);
-  };
-
-  const handleRemoveFilter = (index: number) => {
-    const keys = Object.keys(selectedFilter);
-    const [deletedKey] = keys.filter((key: string) => selectedFilter[key].includes(filterValues[index]));
-    const filterClone: IProductFilter = { ...selectedFilter };
-    const updatedFilter = filterClone[deletedKey].filter((filterValue: string) => filterValue !== filterValues[index]);
-    filterClone[deletedKey] = updatedFilter;
-    const values = Object.values(updatedFilter);
-    if (values.flat(1).length === 0) {
-      setShowMorePills(false);
-    }
-    setSelectedFilter(filterClone);
+    handleShowFilter();
+    AnimationUtils.layout({ duration: 180 });
   };
 
   const handleCancelFilter = () => {
-    setFilter(selectedFilter);
-    handleFilter();
+    // Alert.alert("cancel");
+    handleCancel();
+    // setFilter(products[productList].filters);
+    handleShowFilter();
   };
 
-  const handleContinueFilter = () => {
-    setSelectedFilter(filter);
-    handleFilter();
+  const handleApplyFilter = () => {
+    handleConfirm();
+    handleShowFilter();
+    // handlePills();
   };
 
-  const overlay = filterVisible ? fullHeight : {};
-  const overflow: ViewStyle = showMorePills ? {} : { height: sh48, overflow: "hidden" };
+  // const handleShowAllFilter = () => {
+  //   setShowMorePills(!showMorePills);
+  // };
+
+  // const handleRemoveFilter = (index: number) => {
+  //   const keys = Object.keys(filter);
+  //   const [deletedKey] = keys.filter((key: string) => filter[key].includes(filterValues[index]));
+  //   const filterClone: ITransactionsFilter = { ...filter };
+  //   const updatedFilter = filterClone[deletedKey].filter((filterValue: string) => filterValue !== filterValues[index]);
+  //   filterClone[deletedKey] = updatedFilter;
+  //   const values = Object.values(updatedFilter);
+  //   if (values.flat(1).length === 0) {
+  //     setShowMorePills(false);
+  //   }
+  //   setFilter(filterClone);
+  // };
+
+  // const overlay = filterVisible ? fullHeight : {};
+  // const overflow: ViewStyle = showMorePills ? {} : { height: sh48, overflow: "hidden" };
 
   const pageContainer: ViewStyle = {
     ...fullWidth,
-    ...overlay,
+    // ...overlay,
     backgroundColor: colorTransparent,
     left: 0,
     position: "absolute",
@@ -125,75 +132,75 @@ export const ApplicationHistoryHeader: FunctionComponent<ApplicationHistoryHeade
 
   const filterContainer: ViewStyle = { ...centerHV, ...circleBorder(sw40, sw1, filterBorderColor), backgroundColor: filterBGColor };
   const inputStyle: TextStyle = { ...fs16SemiBoldBlack2, letterSpacing: -0.39 };
-  const showLabel = showMorePills ? PRODUCT_FILTER.LABEL_SHOW_LESS : PRODUCT_FILTER.LABEL_SHOW_ALL;
-
-  useEffect(() => {
-    setFilter(selectedFilter);
-  }, [selectedFilter]);
+  // const showLabel = showMorePills ? PRODUCT_FILTER.LABEL_SHOW_LESS : PRODUCT_FILTER.LABEL_SHOW_ALL;
 
   return (
     <View style={pageContainer}>
-      <ScrollView bounces={true} contentContainerStyle={flexGrow} keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
-        <CustomSpacer space={sh24} />
-        <View style={container}>
-          <View>
-            <View style={px(sw24)}>
-              <CustomSpacer space={sh32} />
-              <Text style={fs24BoldBlue2}>{DASHBOARD_HOME.LABEL_HEADER}</Text>
-              <CustomSpacer space={sh16} />
-              <View style={{ ...centerVertical, ...flexRow }}>
-                <IconInput
-                  autoCorrect={false}
-                  icon="search"
-                  iconInputRef={searchInputRef}
-                  onChangeText={setInputSearch}
-                  onSubmitEditing={handleSearch}
-                  placeholder={DASHBOARD_HOME.LABEL_FILTER_PENDING}
-                  placeholderTextColor={colorBlue._3_6}
-                  returnKeyType="search"
-                  setIconInputRef={setSearchInputRef}
-                  style={inputStyle}
-                  value={inputSearch}
-                  viewStyle={{ borderRadius: sw100, height: sh48 }}
-                />
-                <CustomSpacer isHorizontal={true} space={sw40} />
-                <IconButton color={filterColor} onPress={handlePressFilter} name="filter" size={sh24} style={filterContainer} />
-              </View>
+      <CustomSpacer space={sh24} />
+      <View style={container}>
+        <View>
+          <View style={px(sw24)}>
+            <CustomSpacer space={sh32} />
+            <Text style={fs24BoldBlue2}>{DASHBOARD_HOME.LABEL_HEADER}</Text>
+            <CustomSpacer space={sh16} />
+            <View style={{ ...centerVertical, ...flexRow }}>
+              <IconInput
+                autoCorrect={false}
+                icon="search"
+                iconInputRef={searchInputRef}
+                onChangeText={setInputSearch}
+                onSubmitEditing={handleSearch}
+                placeholder={DASHBOARD_HOME.LABEL_FILTER_PENDING}
+                placeholderTextColor={colorBlue._3_6}
+                returnKeyType="search"
+                setIconInputRef={setSearchInputRef}
+                style={inputStyle}
+                value={inputSearch}
+                viewStyle={{ borderRadius: sw100, height: sh48 }}
+              />
+              <CustomSpacer isHorizontal={true} space={sw40} />
+              <IconButton color={filterColor} onPress={handlePressFilter} name="filter" size={sh24} style={filterContainer} />
             </View>
-            {filterVisible || filterValues.length === 0 ? null : (
-              <View style={px(sw24)}>
-                <View style={flexRow}>
-                  <ButtonSelectionList
-                    buttonStyle={{ marginRight: undefined, marginTop: sh16 }}
-                    data={filterValues}
-                    icon="close-circle"
-                    onPress={handleRemoveFilter}
-                    spaceBetween={sw8}
-                    style={{ ...overflow, width: sw729 }}
-                  />
-                  <View>
-                    <CustomSpacer space={sh24} />
-                    <LinkText onPress={handleShowAllFilter} text={showLabel} />
-                  </View>
+          </View>
+          {/* {filterVisible || filterValues.length === 0 ? null : (
+            <View style={px(sw24)}>
+              <View style={flexRow}>
+                <ButtonSelectionList
+                  buttonStyle={{ marginRight: undefined, marginTop: sh16 }}
+                  data={filterValues}
+                  icon="close-circle"
+                  onPress={handleRemoveFilter}
+                  spaceBetween={sw8}
+                  style={{ ...overflow, width: sw729 }}
+                />
+                <View>
+                  <CustomSpacer space={sh24} />
+                  <LinkText onPress={handleShowAllFilter} text={showLabel} />
                 </View>
               </View>
-            )}
-            <Collapsible collapsed={!filterVisible} duration={300}>
-              <CustomSpacer space={sh32} />
+            </View>
+          )} */}
+          {/* <Collapsible collapsed={!filterVisible} duration={300}> */}
+          {filterVisible ? (
+            <View>
+              <TransactionsFilter filter={filter} activeTab={activeTab} setFilter={setFilter} />
+              <CustomSpacer space={sh40} />
               <ActionButtons
                 buttonContainerStyle={centerHorizontal}
                 cancelButtonStyle={{ width: sw218 }}
                 continueButtonStyle={{ width: sw218 }}
                 handleCancel={handleCancelFilter}
-                handleContinue={handleContinueFilter}
+                handleContinue={handleApplyFilter}
+                labelContinue={DASHBOARD_FILTER.BUTTON_APPLY}
               />
               <CustomSpacer space={sh16} />
-            </Collapsible>
-            <CustomSpacer space={sh24} />
-          </View>
+              {/* </Collapsible> */}
+            </View>
+          ) : null}
+          <CustomSpacer space={sh24} />
         </View>
-        {filterVisible ? <CustomSpacer space={sh24} /> : null}
-      </ScrollView>
+      </View>
+      {filterVisible ? <CustomSpacer space={sh24} /> : null}
     </View>
   );
 };

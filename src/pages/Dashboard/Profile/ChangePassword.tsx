d8831b11be4d210a84c41c9a400c69763c49c2ba
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-02 21:21:01 +0800

    loading cleanup

--- src/pages/Dashboard/Profile/ChangePassword.tsx
+++ src/pages/Dashboard/Profile/ChangePassword.tsx
@@ -64,6 +64,7 @@ const ChangePasswordComponent: FunctionComponent<ChangePasswordProps> = ({ confi
       }
       if (error !== null) {
         setPrompt(false);
+        setIsLoading(false);
         setInput1Error(error.message);
       }
     }

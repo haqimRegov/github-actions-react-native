a835a6df647db94ef41bafdfc8b33e9dcfb6d732
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-02 20:33:40 +0800

    updated data dictionary

--- src/data/dictionary/order-status.ts
+++ src/data/dictionary/order-status.ts
@@ -1,4 +1,4 @@
-export const DICTIONARY_ORDER_STATUS: OrderStatusLabelValue = {
+export const DICTIONARY_ORDER_STATUS: IOrderStatus = {
   completed: "Completed",
   pendingDoc: "Pending Doc",
   pendingDocAndPayment: "Pending Doc & Payment",
@@ -10,3 +10,19 @@ export const DICTIONARY_ORDER_STATUS: OrderStatusLabelValue = {
   submitted: "Submitted",
   void: "Void",
 };
+
+export const DICTIONARY_PENDING_STATUS_FILTER: OrderStatusLabelValue[] = [
+  { label: "Pending Doc", value: "Pending Doc" },
+  { label: "Pending Hardcopy", value: "Pending Hardcopy" },
+  { label: "Pending Payment", value: "Pending Payment" },
+  { label: "BR - Rerouted", value: "BR - Rerouted" },
+  { label: "HQ - Rerouted", value: "HQ - Rerouted" },
+  { label: "Submitted", value: "Submitted" },
+];
+
+export const DICTIONARY_APPROVED_STATUS_FILTER: OrderStatusLabelValue[] = [{ label: "Completed", value: "Completed" }];
+
+export const DICTIONARY_REJECTED_STATUS_FILTER: OrderStatusLabelValue[] = [
+  { label: "Rejected", value: "Rejected" },
+  { label: "Void", value: "Void" },
+];

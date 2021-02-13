29994eeb7fdbc21b4677cb6f66031867fbd74ba5
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-13 18:30:26 +0800

    added firebase messaging and push notification functions

---
+++ src/integrations/react-native-firebase-messaging/functions.ts
@@ -0,0 +1,27 @@
+import messaging from "@react-native-firebase/messaging";
+
+import { RNPushNotification } from "../react-native-push-notification";
+
+const getToken = async () => {
+  const token = await messaging().getToken();
+  // eslint-disable-next-line no-console
+  console.log("token", token);
+  return token;
+};
+
+const onMessage = () => {
+  messaging().onMessage(async (remoteMessage) => {
+    const notification = {
+      data: { ...remoteMessage.data },
+      notification: { title: remoteMessage.notification!.title!, body: remoteMessage.notification!.body! },
+    };
+    // eslint-disable-next-line no-console
+    console.log("notification", notification);
+    RNPushNotification.localNotification(notification);
+  });
+};
+
+export const RNFirebase = {
+  getToken,
+  onMessage,
+};

eaf0d2a202c2acc1d04e64fe07df12d09063c07e
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-16 19:52:12 +0800

    updated regex

--- src/data/dictionary/regex.ts
+++ src/data/dictionary/regex.ts
@@ -1,3 +1,3 @@
-export const DICTIONARY_AMOUNT_REGEX = /^\d*\.?\d*$/;
+export const DICTIONARY_AMOUNT_REGEX = /^(?:\d{1,3}(?:,\d{3})*|\d+)(?:\.\d+)?$/;
 export const DICTIONARY_EMAIL_REGEX = /^[a-zA-Z0-9_.]+@[a-zA-Z]{1,10}\.[a-zA-Z]{2,4}$/;
 export const DICTIONARY_NON_NUMBER_REGEX = /^([^0-9]*)$/;

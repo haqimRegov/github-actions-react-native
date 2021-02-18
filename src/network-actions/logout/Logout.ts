96b2df6e69e026540132348b3e83ae8f6133a3f7
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-18 15:17:00 +0800

    convert to async function

--- src/network-actions/logout/Logout.ts
+++ src/network-actions/logout/Logout.ts
@@ -2,18 +2,17 @@ import { Auth } from "aws-amplify";
 
 import { ERRORS } from "../../data/dictionary";
 
-export const logout = () => {
+export const logout = async () => {
   // try {
-  return Auth.currentAuthenticatedUser()
-    .then((user) => {
-      user.signOut({ global: true });
-      return undefined;
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.log("Auth Sign Out Error", error);
-      return ERRORS.unauthenticated;
-    });
+  try {
+    const user = await Auth.currentAuthenticatedUser();
+    user.signOut({ global: true });
+    return undefined;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log("Auth Sign Out Error", error);
+    return ERRORS.unauthenticated;
+  }
   // } catch (error) {
   //   // eslint-disable-next-line no-console
   //   console.log("Error in logout at Logout.ts", error);

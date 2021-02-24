e29c999f8494a91d6e70b6f20ea21bae98740afd
Aldrin Castro <aldrin.castro@regovtech.com>
2021-02-24 19:55:47 +0800

    updated dictionary

--- src/data/dictionary/recurring.ts
+++ src/data/dictionary/recurring.ts
@@ -4,5 +4,36 @@ export const DICTIONARY_RECURRING_FREQUENCY: TypeRecurringFrequencyLabelValue[] 
 ];
 
 export const DICTIONARY_DDA_BANK = [{ label: "Public Bank", value: "Public Bank" }];
+export const DICTIONARY_FPX_BANK = [
+  { label: "Affin Bank Berhad", value: "Affin Bank Berhad" },
+  { label: "Al Rajhi Banking & Investment Corporation", value: "Al Rajhi Banking & Investment Corporation" },
+  { label: "Alliance Bank Malaysian Berhad", value: "Alliance Bank Malaysian Berhad" },
+  { label: "AmBank Malaysia Berhad", value: "AmBank Malaysia Berhad" },
+  { label: "Bank Islam Malaysia Berhad", value: "Bank Islam Malaysia Berhad" },
+  { label: "Bank Kerjasama Rakyat Malaysia B2C", value: "Bank Kerjasama Rakyat Malaysia B2C" },
+  { label: "Bank Muamalat Malaysia Berhad", value: "Bank Muamalat Malaysia Berhad" },
+  { label: "Bank of America (M) Berhad", value: "Bank of America (M) Berhad" },
+  { label: "Bank Of China ( Malaysia) Berhad", value: "Bank Of China ( Malaysia) Berhad" },
+  { label: "Bank Pertanian (Agrobank)", value: "Bank Pertanian (Agrobank)" },
+  { label: "Bank Simpanan Nasional", value: "Bank Simpanan Nasional" },
+  { label: "BNP Paribas", value: "BNP Paribas" },
+  { label: "CIMB Bank Berhad", value: "CIMB Bank Berhad" },
+  { label: "Citibank Berhad", value: "Citibank Berhad" },
+  { label: "Deutsche Bank (Malaysia) Berhad", value: "Deutsche Bank (Malaysia) Berhad" },
+  { label: "Hong Leong Bank Berhad", value: "Hong Leong Bank Berhad" },
+  { label: "HSBC Bank Berhad FPX", value: "HSBC Bank Berhad FPX" },
+  { label: "Industrial and Commercial Bank of China", value: "Industrial and Commercial Bank of China" },
+  { label: "J.P Morgan Chase Bank", value: "J.P Morgan Chase Bank" },
+  { label: "Kuwait Finance House", value: "Kuwait Finance House" },
+  { label: "Malayan Banking Berhad MBB0227", value: "Malayan Banking Berhad MBB0227" },
+  { label: "Mizuho Bank (Malaysia) Berhad", value: "Mizuho Bank (Malaysia) Berhad" },
+  { label: "MUFG Bank (Malaysia) Berhad", value: "MUFG Bank (Malaysia) Berhad" },
+  { label: "OCBC Bank Malaysia Berhad", value: "OCBC Bank Malaysia Berhad" },
+  { label: "Public Bank Berhad", value: "Public Bank Berhad" },
+  { label: "RHB Bank Berhad", value: "RHB Bank Berhad" },
+  { label: "Standard Chartered Bank Malaysia Berhad", value: "Standard Chartered Bank Malaysia Berhad" },
+  { label: "Sumitomo Mitsui Banking Corporation Malaysia Berhad", value: "Sumitomo Mitsui Banking Corporation Malaysia Berhad" },
+  { label: "United Overseas Bank", value: "United Overseas Bank" },
+];
 
 export const DICTIONARY_RECURRING_CURRENCY = "MYR";

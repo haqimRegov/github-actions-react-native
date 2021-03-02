0da9aa839c408977e8e74b3d04466913b319eba3
Aldrin Castro <aldrin.castro@regovtech.com>
2021-03-02 18:38:16 +0800

    added bank account name dropdown

--- src/templates/Payment/PaymentMethod/ClientTrustAccount.tsx
+++ src/templates/Payment/PaymentMethod/ClientTrustAccount.tsx
@@ -1,37 +1,59 @@
 import React, { FunctionComponent } from "react";
 import { View } from "react-native";
 
-import { CustomSpacer, CustomTextInput } from "../../../components";
+import { AdvancedDropdown, CustomCard, CustomTextInput } from "../../../components";
 import { Language } from "../../../constants";
-import { flexRow, px, sw24, sw64 } from "../../../styles";
+import { px, sh24, sw24, sw64 } from "../../../styles";
 
 const { PAYMENT } = Language.PAGE;
 
 export interface ClientTrustAccountProps {
+  bankNames: TypeLabelValue[];
   clientName: string;
   clientTrustAccountNumber: string;
+  combinedName: string;
   setClientName: (value: string) => void;
   setClientTrust: (value: string) => void;
+  setCombinedName: (value: string) => void;
 }
 
 export const ClientTrustAccount: FunctionComponent<ClientTrustAccountProps> = ({
+  bankNames,
   clientName,
   clientTrustAccountNumber,
+  combinedName,
   setClientName,
   setClientTrust,
+  setCombinedName,
 }: ClientTrustAccountProps) => {
+  const handleClientName = (value: string) => {
+    if (value !== "Combined" && combinedName !== "") {
+      setCombinedName("");
+    }
+    setClientName(value);
+  };
+
+  const items = [
+    <AdvancedDropdown items={bankNames} handleChange={handleClientName} label={PAYMENT.LABEL_CLIENT_NAME} value={clientName} />,
+    <CustomTextInput
+      keyboardType="numeric"
+      label={PAYMENT.LABEL_TRUST_ACCOUNT_NO}
+      onChangeText={setClientTrust}
+      value={clientTrustAccountNumber}
+    />,
+  ];
+
+  if (clientName === "Combined") {
+    items.splice(
+      1,
+      0,
+      <CustomTextInput autoCapitalize="words" label={PAYMENT.LABEL_COMBINED} onChangeText={setCombinedName} value={combinedName} />,
+    );
+  }
+
   return (
     <View style={px(sw24)}>
-      <View style={flexRow}>
-        <CustomTextInput autoCapitalize="words" label={PAYMENT.LABEL_CLIENT_NAME} onChangeText={setClientName} value={clientName} />
-        <CustomSpacer isHorizontal={true} space={sw64} />
-        <CustomTextInput
-          keyboardType="numeric"
-          label={PAYMENT.LABEL_TRUST_ACCOUNT_NO}
-          onChangeText={setClientTrust}
-          value={clientTrustAccountNumber}
-        />
-      </View>
+      <CustomCard spaceBetweenGroup={sh24} spaceBetweenItem={sw64} items={items} />
     </View>
   );
 };

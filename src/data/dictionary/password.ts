5e6714ab716010a90f8cabf85862cf730198adcb
Aldrin Castro <aldrin.castro@regovtech.com>
2021-03-04 16:59:22 +0800

    updated data dictionary

--- src/data/dictionary/password.ts
+++ src/data/dictionary/password.ts
@@ -1,5 +1,5 @@
 export const DICTIONARY_PASSWORD = [
-  { label: "Must be of 8-20 characters", validation: /(?=^.{8,20}$)/ },
+  { label: "Minimum of 8 and maximum of 20 characters", validation: /(?=^.{8,20}$)/ },
   { label: "At least one upper-case letter (A-Z)", validation: /(?=.*[A-Z])/ },
   { label: "At least one lower-case letter (a-z)", validation: /(?=.*[a-z])/ },
   { label: "At least one number (0-9)", validation: /(?=.*[0-9])/ },

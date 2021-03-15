c87877d3bbd7740dceac62c19a2b2d02083fef5a
Aldrin Castro <aldrin.castro@regovtech.com>
2021-03-15 19:14:44 +0800

    updated dashboard network action

--- src/network-actions/dashboard/Dashboard.ts
+++ src/network-actions/dashboard/Dashboard.ts
@@ -1,14 +1,18 @@
 import { GQL_QUERIES } from "../../integrations";
-import { gqlOperation } from "../../integrations/graphql/functions";
+import { responseHandler } from "../../utils/ResponseHandler";
 
-export const getDashboard = async (variables: IDashboardRequest, handleError?: ResponseErrorType) => {
+export const getDashboard = async (variables: IDashboardRequest, navigation: IStackNavigationProp, handleError?: ResponseErrorType) => {
   try {
-    const data: IDashboardQuery = await gqlOperation<IDashboardQuery, IDashboardRequest>(
+    const data: IDashboardQuery = await responseHandler<IDashboardQuery, IDashboardRequest>(
       GQL_QUERIES.dashboard,
       variables,
       undefined,
+      navigation,
       handleError,
     );
+    if (data === undefined || "dashboard" in data === false) {
+      throw data;
+    }
     return data.dashboard;
   } catch (error) {
     // eslint-disable-next-line no-console

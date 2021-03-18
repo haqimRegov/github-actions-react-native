5ba67f888cb02b6484a085847d5f1802c794cb8f
Aldrin Castro <aldrin.castro@regovtech.com>
2021-03-18 16:26:09 +0800

    updated regex to include hyphen in domain (trailing and leading hypen is currently allowed)

--- src/data/dictionary/regex.ts
+++ src/data/dictionary/regex.ts
@@ -1,3 +1,3 @@
 export const DICTIONARY_AMOUNT_REGEX = /^(?:\d{1,3}(?:,\d{3})*|\d+)(?:\.\d+)?$/;
-export const DICTIONARY_EMAIL_REGEX = /^[a-zA-Z0-9_.-]+@[a-zA-Z]{1,10}(\.[a-zA-Z]{2,})+$/;
+export const DICTIONARY_EMAIL_REGEX = /^[a-zA-Z0-9_.-]+@[a-zA-Z-]{1,}(\.[a-zA-Z]{2,})+$/;
 export const DICTIONARY_NON_NUMBER_REGEX = /^([^0-9]*)$/;

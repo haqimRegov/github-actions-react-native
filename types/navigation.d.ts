bfd6fdd85a054bd1ec1684c72a755dad0e589cb2
Aldrin Castro <aldrin.castro@regovtech.com>
2021-04-12 16:07:55 +0800

    separated Private and Public stack type

--- types/navigation.d.ts
+++ types/navigation.d.ts
@@ -5,6 +5,18 @@ declare type RootNavigatorType = {
   Onboarding: undefined;
 };
 
-declare type IStackNavigationProp = import("@react-navigation/stack").StackNavigationProp<RootNavigatorType>;
+declare type PrivateNavigatorType = {
+  Dashboard: undefined;
+  Logout: undefined;
+  Onboarding: undefined;
+  Public: undefined;
+};
+
+declare type PublicNavigatorType = {
+  Auth: undefined;
+  Private: undefined;
+};
+
+declare type IStackNavigationProp = import("@react-navigation/stack").StackNavigationProp<PrivateNavigatorType>;
 
 declare type TypeLoginPages = "LOGIN" | "PASSWORD_RECOVERY" | "FIRST_TIME_LOGIN" | "LOCKED_ACCOUNT" | "LOCKED_PASSWORD";

d45741933f926f5fea5a65e1be8b0b705b247cf3
Aldrin Castro <aldrin.castro@regovtech.com>
2021-05-07 10:27:59 +0800

    updated income dictionary

--- src/data/dictionary/gross-income.ts
+++ src/data/dictionary/gross-income.ts
@@ -1,7 +1,7 @@
 export const DICTIONARY_GROSS_INCOME: TypeGrossIncomeLabelValue[] = [
-  { label: "> RM 300,000", value: "> RM 300,000" },
-  { label: "RM 200,001 - RM 300,000", value: "RM 200,001 - RM 300,000" },
-  { label: "RM 100,001 - RM 200,000", value: "RM 100,001 - RM 200,000" },
-  { label: "RM 50,001 - RM 100,000", value: "RM 50,001 - RM 100,000" },
-  { label: "< RM 50,000", value: "< RM 50,000" },
+  { label: "> 300,000", value: "> 300,000" },
+  { label: "200,001 - 300,000", value: "200,001 - 300,000" },
+  { label: "100,001 - 200,000", value: "100,001 - 200,000" },
+  { label: "50,001 - 100,000", value: "50,001 - 100,000" },
+  { label: "< 50,000", value: "< 50,000" },
 ];

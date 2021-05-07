f2394608ada58af7bc1986a5f8b530903d034e34
Aldrin Castro <aldrin.castro@regovtech.com>
2021-05-07 10:27:41 +0800

    updated income types

--- types/gross-income.d.ts
+++ types/gross-income.d.ts
@@ -1,8 +1,3 @@
-declare type TypeGrossIncome =
-  | "> RM 300,000"
-  | "RM 200,001 - RM 300,000"
-  | "RM 100,001 - RM 200,000"
-  | "RM 50,001 - RM 100,000"
-  | "< RM 50,000";
+declare type TypeGrossIncome = "> 300,000" | "200,001 - 300,000" | "100,001 - 200,000" | "50,001 - 100,000" | "< 50,000";
 
 declare type TypeGrossIncomeLabelValue = { label: string; value: TypeGrossIncome };

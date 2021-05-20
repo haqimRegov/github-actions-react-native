601b60abc249d739070a5ced848e9b4705616ba3
Aldrin Castro <aldrin.castro@regovtech.com>
2021-05-20 15:50:54 +0800

    cleaned data dictionary

--- src/data/dictionary/id-type.ts
+++ src/data/dictionary/id-type.ts
@@ -1,14 +1,10 @@
 export const DICTIONARY_ID_TYPE: TypeIDChoices[] = ["NRIC", "Passport", "Other"];
+
 export const DICTIONARY_ID_OTHER_TYPE: TypeIDOtherLabelValue[] = [
   { label: "Army", value: "Army" },
   { label: "Police", value: "Police" },
 ];
-export const DICTIONARY_ALL_ID = [
-  { label: "NRIC", value: "NRIC" },
-  { label: "Passport", value: "Passport" },
-  { label: "Army", value: "Army" },
-  { label: "Police", value: "Police" },
-];
+
 export const DICTIONARY_ALL_ID_TYPE: TypeClientID[] = ["NRIC", "Passport", "Army", "Police"];
 
 export const DICTIONARY_NRIC_LENGTH = 12;

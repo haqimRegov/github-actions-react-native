f293eb0df201f62317fe9536d8581e8285bda255
Aldrin Castro <aldrin.castro@regovtech.com>
2021-06-21 16:52:23 +0800

    removed StatusBar in SafeAreaPage

--- src/components/CommonPages/SafeAreaPage.tsx
+++ src/components/CommonPages/SafeAreaPage.tsx
@@ -1,20 +1,18 @@
 import React, { Fragment, ReactNode } from "react";
-import { StatusBar, StatusBarStyle, View, ViewStyle } from "react-native";
+import { View, ViewStyle } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { colorGray, flexChild } from "../../styles";
 
 interface SafeArePageProps {
-  barStyle?: StatusBarStyle;
   bottomBackgroundColor?: string;
   bottomSpace?: number;
   children: ReactNode;
   topBackgroundColor?: string;
 }
 
-export const SafeAreaPage = ({ barStyle, bottomBackgroundColor, bottomSpace, children, topBackgroundColor }: SafeArePageProps) => {
+export const SafeAreaPage = ({ bottomBackgroundColor, bottomSpace, children, topBackgroundColor }: SafeArePageProps) => {
   const { top } = useSafeAreaInsets();
-  const defaultBarStyle = barStyle !== undefined ? barStyle : "default";
 
   const topStyle: ViewStyle = {
     backgroundColor: topBackgroundColor !== undefined ? topBackgroundColor : colorGray._5,
@@ -28,7 +26,6 @@ export const SafeAreaPage = ({ barStyle, bottomBackgroundColor, bottomSpace, chi
 
   return (
     <Fragment>
-      <StatusBar barStyle={defaultBarStyle} />
       <View style={flexChild}>
         <View style={topStyle} />
         <View style={{ ...flexChild, backgroundColor: bottomBackgroundColor !== undefined ? bottomBackgroundColor : colorGray._5 }}>

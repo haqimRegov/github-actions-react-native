1fe09660b9cc7f4d48afed60261c55dbc273ef2c
Aldrin Castro <aldrin.castro@regovtech.com>
2021-09-09 17:23:00 +0800

    updated text input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,32 +1,37 @@
-import React, { Fragment, FunctionComponent } from "react";
-import { Text, TextInput, TextInputProps, TextStyle, View, ViewStyle } from "react-native";
+import React, { Fragment, FunctionComponent, RefObject, useState } from "react";
+import { NativeSyntheticEvent, Text, TextInput, TextInputFocusEventData, TextInputProps, TextStyle, View, ViewStyle } from "react-native";
 
+import { NunitoBold, NunitoRegular } from "../../constants";
 import { IcoMoon } from "../../icons";
 import {
   border,
   centerVertical,
   colorBlack,
+  colorBlue,
   colorGray,
   colorRed,
   colorTransparent,
   colorWhite,
+  customShadow,
   flexChild,
   flexRow,
   fs12BoldBlack2,
-  fs12SemiBoldRed2,
-  fs16BoldBlack2,
+  fs12RegRed2,
   fs16RegBlack2,
-  fsNoLineHeight,
   px,
   sh16,
-  sh38,
-  sh40,
-  sh8,
+  sh4,
+  sh48,
+  sh50,
   sw1,
+  sw15,
   sw16,
-  sw20,
-  sw304,
+  sw2,
+  sw24,
+  sw32,
+  sw336,
   sw360,
+  sw4,
   sw8,
 } from "../../styles";
 import { CustomSpacer } from "../Views/Spacer";
@@ -38,18 +43,17 @@ export interface CustomTextInputProps extends TextInputProps {
   inputPrefix?: string;
   label?: string;
   labelStyle?: TextStyle;
+  leftIcon?: IIcon;
   noBorder?: boolean;
   onPressLabel?: () => void;
   prefixStyle?: TextStyle;
-  rightIcon?: string;
-  rightIconColor?: string;
-  rightIconPress?: () => void;
-  rightIconSize?: number;
-  setRef?: React.MutableRefObject<TextInput | null> | null | ((instance: TextInput | null) => void);
+  rightIcon?: IIcon;
+  setRef?: string | ((instance: TextInput | null) => void) | RefObject<TextInput> | null | undefined;
   spaceToBottom?: number;
   spaceToLabel?: number;
   spaceToTop?: number;
   style?: TextStyle;
+  testID?: string;
   viewStyle?: ViewStyle;
 }
 
@@ -60,86 +64,136 @@ export const CustomTextInput: FunctionComponent<CustomTextInputProps> = ({
   inputPrefix,
   label,
   labelStyle,
+  leftIcon,
   noBorder,
+  onBlur,
+  onFocus,
   onLayout,
   onPressLabel,
+  placeholder,
   prefixStyle,
   rightIcon,
-  rightIconColor,
-  rightIconPress,
-  rightIconSize,
   setRef,
   spaceToBottom,
-  spaceToLabel,
   spaceToTop,
   style,
+  testID,
   value,
   viewStyle,
-  ...rest
+  ...textInputProps
 }: CustomTextInputProps) => {
-  const borderWidth = noBorder === true ? 0 : sw1;
-  const borderColor = error !== undefined ? colorRed._2 : colorGray._7;
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const borderWidth = noBorder === true ? { borderWidth: 0 } : {};
+  const disabledStyle = disabled === true ? { backgroundColor: colorGray._5, opacity: 0.6 } : {};
+  const errorStyle: ViewStyle = error !== undefined ? { backgroundColor: colorRed._3_08, borderWidth: sw2, borderColor: colorRed._2 } : {};
+  const focusedShadow = isFocused ? customShadow(colorBlue._2, 0, 0, 0.02, sw4) : {};
+
+  const defaultContainerStyle: ViewStyle = {
+    paddingTop: spaceToTop !== undefined ? spaceToTop : 0,
+    paddingBottom: spaceToBottom !== undefined ? spaceToBottom : 0,
+  };
+
   const defaultInputStyle: ViewStyle = {
+    ...border(isFocused ? colorBlue._2 : colorWhite._3, isFocused ? sw2 : sw1, sw32),
     ...centerVertical,
     ...flexRow,
-    ...px(sw16),
-    ...border(borderColor, borderWidth, sw20),
+    ...px(isFocused ? sw15 : sw16),
     backgroundColor: colorWhite._1,
-    height: sh40,
+    height: sh48,
     width: sw360,
+    ...borderWidth,
+    ...focusedShadow,
+    ...errorStyle,
+    ...disabledStyle,
     ...viewStyle,
   };
-  const defaultLabelSpace = spaceToLabel === undefined ? 0 : spaceToLabel;
-  const defaultIconColor = rightIconColor !== undefined ? rightIconColor : colorBlack._2;
-  const disabledStyle = disabled === true ? { opacity: 0.5 } : {};
+
+  const inputStyle = {
+    ...flexChild,
+    fontFamily: placeholder !== undefined && !value ? NunitoRegular : NunitoBold,
+    fontSize: sh16,
+    height: isFocused ? sh50 : sh48, // height is more than the input view size to adjust the keyboard avoiding view
+    ...style,
+  };
+
+  const handleBlur = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    if (onBlur) {
+      onBlur(event);
+    }
+    setIsFocused(false);
+  };
+
+  const handleFocus = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    if (onFocus) {
+      onFocus(event);
+    }
+    setIsFocused(true);
+  };
 
   return (
-    <View onLayout={onLayout} style={{ ...flexChild, width: sw360, ...containerStyle }}>
-      {spaceToTop !== undefined ? <CustomSpacer space={spaceToTop} /> : null}
+    <View onLayout={onLayout} style={{ width: sw360, ...defaultContainerStyle, ...containerStyle }}>
       {label === undefined ? null : (
-        <Fragment>
-          <Text onPress={onPressLabel} style={{ ...fs12BoldBlack2, ...labelStyle, ...disabledStyle }}>
-            {label}
-          </Text>
-          <CustomSpacer space={defaultLabelSpace} />
-        </Fragment>
+        <Text onPress={onPressLabel} style={{ ...fs12BoldBlack2, ...labelStyle }}>
+          {label}
+        </Text>
       )}
       <View style={defaultInputStyle}>
+        {leftIcon === undefined ? null : (
+          <Fragment>
+            <IcoMoon
+              color={leftIcon.color || colorBlue._2}
+              name={leftIcon.name}
+              onPress={disabled === true ? undefined : leftIcon.onPress}
+              size={leftIcon.size || sw24}
+            />
+            <CustomSpacer isHorizontal={true} space={sw16} />
+          </Fragment>
+        )}
         {inputPrefix !== undefined ? (
           <Fragment>
-            <Text style={{ ...fs16RegBlack2, ...disabledStyle, ...prefixStyle }}>{inputPrefix}</Text>
+            <Text style={{ ...fs16RegBlack2, ...prefixStyle }}>{inputPrefix}</Text>
             <CustomSpacer isHorizontal={true} space={sw8} />
           </Fragment>
         ) : null}
         <TextInput
           autoCorrect={false}
           editable={!disabled}
-          placeholder={rest.placeholder}
-          placeholderTextColor={colorGray._7}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          placeholder={placeholder}
+          placeholderTextColor={colorGray._9}
           ref={setRef}
           selectionColor={colorBlack._2}
           spellCheck={false}
-          style={{ ...fs16BoldBlack2, ...flexChild, height: sh38, ...disabledStyle, ...style, ...fsNoLineHeight }}
+          style={inputStyle}
+          testID={testID}
           underlineColorAndroid={colorTransparent}
           value={value}
-          {...rest}
+          {...textInputProps}
         />
         {rightIcon === undefined ? null : (
-          <IcoMoon color={defaultIconColor} name={rightIcon} onPress={rightIconPress} size={rightIconSize || sw20} style={disabledStyle} />
+          <Fragment>
+            <CustomSpacer isHorizontal={true} space={sw16} />
+            <IcoMoon
+              color={rightIcon.color || colorBlue._2}
+              name={rightIcon.name}
+              onPress={disabled === true ? undefined : rightIcon.onPress}
+              size={rightIcon.size || sw24}
+            />
+          </Fragment>
         )}
       </View>
       {error === undefined ? null : (
-        <Fragment>
-          <CustomSpacer space={sh8} />
+        <View>
+          <CustomSpacer space={sh4} />
           <View style={flexRow}>
-            <CustomSpacer isHorizontal={true} space={sw16} />
-            <IcoMoon color={colorRed._2} name="error" size={sh16} />
+            <IcoMoon color={colorRed._2} name="error-filled" size={sw16} />
             <CustomSpacer isHorizontal={true} space={sw8} />
-            <Text style={{ ...fs12SemiBoldRed2, width: sw304 }}>{error}</Text>
+            <Text style={{ ...fs12RegRed2, width: sw336 }}>{error}</Text>
           </View>
-        </Fragment>
+        </View>
       )}
-      {spaceToBottom !== undefined ? <CustomSpacer space={spaceToBottom} /> : null}
     </View>
   );
 };

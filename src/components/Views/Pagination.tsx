04d2f90b46b5ad212c6a5f12fc73cbce0b49e5e2
Aldrin Castro <aldrin.castro@regovtech.com>
2021-09-10 16:51:06 +0800

    updated pagination

--- src/components/Views/Pagination.tsx
+++ src/components/Views/Pagination.tsx
@@ -3,21 +3,22 @@ import { Text, View, ViewStyle } from "react-native";
 
 import { Language } from "../../constants";
 import {
-  centerHV,
   centerVertical,
   circleBorder,
   colorBlue,
   colorGray,
   flexRow,
   fs10RegBlue25,
-  fs12SemiBoldGray8,
-  sh24,
+  fs12BoldBlack3,
+  fs12RegGray9,
+  sh16,
   sh40,
   sw1,
   sw16,
-  sw36,
-  sw4,
+  sw2,
+  sw24,
   sw40,
+  sw8,
 } from "../../styles";
 import { IconButton } from "../Touchables/Icon";
 import { CustomSpacer } from "./Spacer";
@@ -55,7 +56,7 @@ export const Pagination: FunctionComponent<PaginationProps> = ({
     lastItem = currentMaxItems > totalItems ? totalItems : currentMaxItems;
     currentItems = `${currentMinItems}-${lastItem}`;
   }
-  const pageCounter = `${page}/${totalPages}`;
+  const totalPage = `of ${totalPages}`;
 
   const showListLabel = `${PAGINATION.LABEL_SHOWING} ${currentItems} ${PAGINATION.LABEL_OUT_OF} ${totalItems} ${PAGINATION.LABEL_RESULTS}`;
   const prevEnabled = page > 1;
@@ -75,9 +76,6 @@ export const Pagination: FunctionComponent<PaginationProps> = ({
     }
   };
 
-  const prevColor = prevEnabled ? colorBlue._2 : colorGray._3;
-  const nextColor = nextEnabled ? colorBlue._2 : colorGray._3;
-
   return (
     <View style={{ ...centerVertical, ...flexRow, height: sh40 }}>
       {withLabel === true ? (
@@ -86,28 +84,32 @@ export const Pagination: FunctionComponent<PaginationProps> = ({
           <CustomSpacer isHorizontal={true} space={sw16} />
         </Fragment>
       ) : null}
-      <View style={flexRow}>
-        <IconButton
-          color={prevColor}
-          name="caret-left"
-          onPress={handlePrev}
-          size={sh24}
-          style={{ ...buttonStyle, ...prevStyle }}
-          withDebounce={true}
-        />
-        <CustomSpacer isHorizontal={true} space={sw4} />
-        <View style={{ ...centerHV, width: sw36 }}>
-          <Text style={fs12SemiBoldGray8}>{pageCounter}</Text>
+      <View style={{ ...centerVertical, ...flexRow }}>
+        <View style={{ ...centerVertical, ...flexRow, height: sh16 }}>
+          <Text style={{ ...fs12BoldBlack3, lineHeight: sh16 }}>{page}</Text>
+          <CustomSpacer isHorizontal={true} space={sw2} />
+          <Text style={{ ...fs12RegGray9, lineHeight: sh16 }}>{totalPage}</Text>
         </View>
-        <CustomSpacer isHorizontal={true} space={sw4} />
-        <IconButton
-          color={nextColor}
-          name="caret-right"
-          onPress={handleNext}
-          size={sh24}
-          style={{ ...buttonStyle, ...nextStyle }}
-          withDebounce={true}
-        />
+        <CustomSpacer isHorizontal={true} space={sw24} />
+        <View style={flexRow}>
+          <IconButton
+            color={colorBlue._2}
+            name="caret-left"
+            onPress={handlePrev}
+            size={sw24}
+            style={{ ...buttonStyle, ...prevStyle }}
+            withDebounce={true}
+          />
+          <CustomSpacer isHorizontal={true} space={sw8} />
+          <IconButton
+            color={colorBlue._2}
+            name="caret-right"
+            onPress={handleNext}
+            size={sw24}
+            style={{ ...buttonStyle, ...nextStyle }}
+            withDebounce={true}
+          />
+        </View>
       </View>
     </View>
   );

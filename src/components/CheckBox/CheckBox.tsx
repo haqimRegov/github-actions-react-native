bcda4ea107a5d7ba5f86b60c6aac9273373bce25
Aldrin Castro <aldrin.castro@regovtech.com>
2021-09-27 17:23:12 +0800

    updated checkbox

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,62 +1,79 @@
 import React, { Fragment, FunctionComponent } from "react";
 import { Text, TextStyle, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
 
+import { NunitoBold, NunitoRegular } from "../../constants";
 import { IcoMoon } from "../../icons";
 import {
   centerHV,
   centerVertical,
   colorBlue,
+  colorGray,
   colorRed,
   colorWhite,
   flexRow,
   fs12RegBlack2,
-  sh12,
   sh24,
   sw1,
   sw10,
-  sw16,
+  sw14,
+  sw18,
   sw4,
 } from "../../styles";
 import { CustomSpacer } from "../Views/Spacer";
 
 export interface CheckBoxProps {
+  boxStyle?: ViewStyle;
   checkboxStyle?: ViewStyle;
   disabled?: boolean;
+  iconSize?: number;
   label?: string;
   labelStyle?: TextStyle;
   numberOfLines?: number;
   onPress: () => void;
   spaceToLabel?: number;
+  spaceToSubLabel?: number;
+  subLabel?: string;
+  subLabelStyle?: TextStyle;
   style?: ViewStyle;
   toggle: boolean;
   toggleColor?: string;
 }
 export const CheckBox: FunctionComponent<CheckBoxProps> = ({
+  boxStyle,
   checkboxStyle,
   disabled,
+  iconSize,
   label,
   labelStyle,
   numberOfLines,
   onPress,
+  subLabel,
+  subLabelStyle,
   spaceToLabel,
+  spaceToSubLabel,
   style,
   toggle,
   toggleColor,
 }: CheckBoxProps) => {
   const defaultColor = toggleColor !== undefined ? toggleColor : colorRed._1;
   const selectedStyle: ViewStyle = toggle ? { backgroundColor: defaultColor, borderColor: defaultColor } : {};
-  const disabledStyle: ViewStyle = disabled === true ? { opacity: 0.5 } : {};
+  const disabledBackground: TextStyle = disabled === true ? { backgroundColor: colorGray._9 } : {};
+  const disabledStyle: TextStyle = disabled === true ? { opacity: 0.6 } : {};
+  const defaultIconSize: number = iconSize !== undefined ? iconSize : sw14;
 
   const toggleStyle: ViewStyle = {
     ...centerHV,
     borderColor: colorBlue._2,
     borderRadius: sw4,
     borderWidth: sw1,
-    height: sw16,
-    width: sw16,
+    height: sw18,
+    width: sw18,
+    ...boxStyle,
+    ...disabledBackground,
     ...selectedStyle,
     ...disabledStyle,
   };
+  const fontFamily = toggle ? NunitoBold : NunitoRegular;
 
   const defaultSpace = spaceToLabel !== undefined ? spaceToLabel : sw10;
 
@@ -71,14 +88,24 @@ export const CheckBox: FunctionComponent<CheckBoxProps> = ({
       <TouchableWithoutFeedback onPress={handlePress}>
         <View style={{ ...centerVertical, ...flexRow, ...style }}>
           <View style={checkboxStyle}>
-            <View style={toggleStyle}>{toggle ? <IcoMoon color={colorWhite._1} name="check" size={sh12} /> : null}</View>
+            <View style={toggleStyle}>{toggle ? <IcoMoon color={colorWhite._1} name="success" size={defaultIconSize} /> : null}</View>
           </View>
           {label === undefined ? null : (
             <Fragment>
               <CustomSpacer isHorizontal={true} space={defaultSpace} />
-              <Text numberOfLines={numberOfLines} style={{ ...fs12RegBlack2, lineHeight: sh24, ...disabledStyle, ...labelStyle }}>
-                {label}
-              </Text>
+              <View>
+                <Text
+                  numberOfLines={numberOfLines}
+                  style={{ ...fs12RegBlack2, fontFamily, lineHeight: sh24, ...disabledStyle, ...labelStyle }}>
+                  {label}
+                </Text>
+                {subLabel !== undefined ? (
+                  <Fragment>
+                    {spaceToSubLabel !== undefined ? <CustomSpacer space={spaceToSubLabel} /> : null}
+                    <Text style={{ ...fs12RegBlack2, ...subLabelStyle }}>{subLabel}</Text>
+                  </Fragment>
+                ) : null}
+              </View>
             </Fragment>
           )}
         </View>

62c778da1736fc3672a65413178844bc9fa712f1
Aldrin Castro <aldrin.castro@regovtech.com>
2021-10-05 11:47:15 +0800

    use NewMobileInput

--- src/pages/Onboarding/PersonalDetails/ContactDetails.tsx
+++ src/pages/Onboarding/PersonalDetails/ContactDetails.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, FunctionComponent } from "react";
 import { View } from "react-native";
 
-import { CustomFlexSpacer, CustomSpacer, IconButton, MobileInput, OutlineButton } from "../../../components";
+import { CustomFlexSpacer, CustomSpacer, IconButton, NewMobileInput, OutlineButton } from "../../../components";
 import { Language } from "../../../constants";
 import { DICTIONARY_MOBILE_CODE, ERROR } from "../../../data/dictionary";
 import { centerVertical, colorBlack, colorBlue, flexRow, fs12BoldBlue2, px, py, sh16, sh24, sh8, sw16, sw24 } from "../../../styles";
@@ -72,18 +72,16 @@ export const ContactDetails: FunctionComponent<ContactDetailsProps> = ({
 
           return (
             <View key={index} style={{ ...centerVertical, ...flexRow }}>
-              <View>
-                <MobileInput
-                  data={item}
-                  handleContactNumber={handleContactNumber}
-                  keyboardType="numeric"
-                  label={optional === true ? `${item.label} ${PERSONAL_DETAILS.LABEL_OPTIONAL}` : undefined}
-                  maxLength={15}
-                  onBlur={onBlur}
-                  placeholder="12 3456 7890"
-                  spaceToTop={sh24}
-                />
-              </View>
+              <NewMobileInput
+                data={item}
+                handleContactNumber={handleContactNumber}
+                keyboardType="numeric"
+                label={optional === true ? `${item.label} ${PERSONAL_DETAILS.LABEL_OPTIONAL}` : undefined}
+                maxLength={15}
+                onBlur={onBlur}
+                placeholder="12 3456 7890"
+                spaceToTop={sh24}
+              />
               <CustomSpacer isHorizontal={true} space={sw16} />
               {index === 0 ? null : (
                 <View>
@@ -102,7 +100,10 @@ export const ContactDetails: FunctionComponent<ContactDetailsProps> = ({
             const checkName = (element: IContactNumber) => element.label === item.label;
 
             const handleAddNumber = () => {
-              const contactNumberClone = [...contactNumber, { ...item, value: "", code: DICTIONARY_MOBILE_CODE[0].value }];
+              const contactNumberClone = [
+                ...contactNumber,
+                { ...item, value: "", code: DICTIONARY_MOBILE_CODE[0].value, id: DICTIONARY_MOBILE_CODE[0].id },
+              ];
               setContactNumber(contactNumberClone);
             };
 

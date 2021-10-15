292b389a26e769cf930f089860a9fcad50d01812
Aldrin Castro <edwardaldrincastro@gmail.com>
2021-10-15 16:09:01 +0800

    updated Badge component

--- src/components/Badge/Check.tsx
+++ src/components/Badge/Check.tsx
@@ -6,15 +6,17 @@ import { centerHV, circle, colorGreen, colorWhite, flexRow, sh6, sw8 } from "../
 
 export interface BadgeProps {
   children: ReactNode;
+  icon?: IIcon;
+  style?: ViewStyle;
 }
 
-export const Badge: FunctionComponent<BadgeProps> = ({ children }: BadgeProps) => {
-  const badgeStyle: ViewStyle = { ...centerHV, ...circle(sw8, colorGreen._1), position: "absolute", right: 0, bottom: 0 };
+export const Badge: FunctionComponent<BadgeProps> = ({ children, icon, style }: BadgeProps) => {
+  const badgeStyle: ViewStyle = { ...centerHV, ...circle(sw8, colorGreen._1), position: "absolute", right: 0, bottom: 0, ...style };
   return (
     <View style={flexRow}>
       {children}
       <View style={badgeStyle}>
-        <IcoMoon color={colorWhite._1} name="success" size={sh6} />
+        <IcoMoon color={colorWhite._1} name="success" size={sh6} {...icon} />
       </View>
     </View>
   );

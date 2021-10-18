0ea8380e1bf02f8f248132ea5fc6e7837bc58556
Nithin <johnnithin08@gmail.com>
2021-10-18 16:02:48 +0800

    [update]: design update

--- src/components/List/NotificationList.tsx
+++ src/components/List/NotificationList.tsx
@@ -1,30 +1,67 @@
 import moment from "moment";
 import React, { Fragment, FunctionComponent } from "react";
-import { TextStyle, ViewStyle } from "react-native";
+import { Text, TextStyle, View, ViewStyle } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 import { DEFAULT_TIME_FORMAT } from "../../constants";
-import { colorWhite, fs12RegBlue25, fs16RegBlack2, sh14, sh8 } from "../../styles";
+import { Language } from "../../constants/language";
+import { IcoMoon } from "../../icons";
+import {
+  centerVertical,
+  colorBlue,
+  colorWhite,
+  flexRow,
+  fs12BoldBlue2,
+  fs12RegBlue25,
+  fs16RegBlack2,
+  sh14,
+  sh16,
+  sh8,
+  sw4,
+} from "../../styles";
 import { AvatarProps } from "../Avatar";
 import { NotificationItem } from "../Items";
-import { CustomSpacer } from "../Views/Spacer";
+import { CustomFlexSpacer, CustomSpacer } from "../Views/Spacer";
 import { TextSpaceArea } from "../Views/TextSpaceArea";
+
+const { INBOX } = Language.PAGE;
 
 interface NotificationListProps {
   avatarProps?: (item: INotificationItem) => AvatarProps;
+  handleReadAll: () => void;
   items: INotificationItem[];
   label?: string;
+  markAll: boolean;
+  notificationsCount: number;
   onPress?: (notification: INotificationItem) => void;
 }
 
 export const NotificationList: FunctionComponent<NotificationListProps> = ({
   avatarProps,
+  handleReadAll,
   items,
+  markAll,
+  notificationsCount,
   label,
   onPress,
 }: NotificationListProps) => {
   return (
     <Fragment>
-      {label !== undefined ? <TextSpaceArea spaceToBottom={sh8} style={{ ...fs12RegBlue25, lineHeight: sh14 }} text={label} /> : null}
+      <View style={flexRow}>
+        {label !== undefined ? <TextSpaceArea spaceToBottom={sh8} style={{ ...fs12RegBlue25, lineHeight: sh14 }} text={label} /> : null}
+        {markAll && notificationsCount > 0 ? (
+          <Fragment>
+            <CustomFlexSpacer />
+            <TouchableWithoutFeedback onPress={handleReadAll}>
+              <View style={{ ...flexRow, ...centerVertical }}>
+                <IcoMoon color={colorBlue._2} name="check" size={sh16} />
+                <CustomSpacer isHorizontal space={sw4} />
+                <Text style={fs12BoldBlue2}>{`${INBOX.READ_ALL} (${notificationsCount})`}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </Fragment>
+        ) : null}
+      </View>
       {items.map((item: INotificationItem, index: number) => {
         const { createdAt, id, isRead, title, message } = item;
         const subtitle = moment(createdAt, "x").format(DEFAULT_TIME_FORMAT);

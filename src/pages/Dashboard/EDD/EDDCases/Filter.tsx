a2f0c5b7597a0d016bf3ca721177ad38219cbc28
Nithin <johnnithin08@gmail.com>
2021-10-18 16:16:33 +0800

    [new]: filter and header

---
+++ src/pages/Dashboard/EDD/EDDCases/Filter.tsx
@@ -0,0 +1,102 @@
+import moment from "moment";
+import React, { FunctionComponent } from "react";
+import { View } from "react-native";
+
+import { CustomSpacer, NewCheckBoxDropdownV2, NewDatePicker, NewDropdown, TextSpaceArea } from "../../../../components";
+import { Language } from "../../../../constants";
+import { DICTIONARY_EDD_DATE, DICTIONARY_HISTORY_TAB_STATUS, DICTIONARY_NEW_TAB_STATUS } from "../../../../data/dictionary/edd";
+import { centerVertical, flexRow, fs16BoldBlack1, px, sh143, sh24, sh32, sh8, sw24, sw64 } from "../../../../styles";
+
+const { DASHBOARD_FILTER } = Language.PAGE;
+interface EDDFilterProps {
+  activeTab: EDDTabType;
+  filter: IEDDFilter;
+  setFilter: (value: IEDDFilter) => void;
+}
+
+export const EDDFilter: FunctionComponent<EDDFilterProps> = ({ activeTab, filter, setFilter }: EDDFilterProps) => {
+  const { dateSorting, startDate, endDate, caseStatus } = filter;
+
+  const handleStartDate = (value?: Date) => {
+    setFilter({ ...filter, startDate: value });
+  };
+
+  const handleEndDate = (value?: Date) => {
+    setFilter({ ...filter, endDate: value });
+  };
+
+  const handleDateSorting = (value: string) => {
+    setFilter({ ...filter, dateSorting: value });
+  };
+
+  const handleCaseStatus = (value: string[]) => {
+    setFilter({ ...filter, caseStatus: value });
+  };
+
+  let caseStatusList: IEDDStatusLabelValue[] = [];
+  switch (activeTab) {
+    case "new":
+      caseStatusList = DICTIONARY_NEW_TAB_STATUS;
+      break;
+    case "history":
+      caseStatusList = DICTIONARY_HISTORY_TAB_STATUS;
+      break;
+
+    default:
+      caseStatusList = DICTIONARY_NEW_TAB_STATUS;
+      break;
+  }
+
+  return (
+    <View>
+      <View style={{ ...px(sw24) }}>
+        <View style={{ ...centerVertical, ...flexRow }}>
+          <TextSpaceArea spaceToBottom={sh8} spaceToTop={sh32} style={fs16BoldBlack1} text={DASHBOARD_FILTER.TITLE} />
+        </View>
+      </View>
+      <View style={{ ...flexRow, ...px(sw24) }}>
+        <NewDropdown
+          handleChange={handleDateSorting}
+          items={DICTIONARY_EDD_DATE}
+          label={DASHBOARD_FILTER.LABEL_DATE_SORTING}
+          value={dateSorting!}
+        />
+      </View>
+      <CustomSpacer space={sh24} />
+      <View style={{ ...flexRow, ...px(sw24) }}>
+        <View>
+          <TextSpaceArea spaceToBottom={sh8} text={DASHBOARD_FILTER.LABEL_START_DATE} />
+          <NewDatePicker
+            datePickerStyle={{ height: sh143 }}
+            mode="date"
+            maximumDate={endDate || moment().toDate()}
+            setValue={handleStartDate}
+            value={startDate}
+          />
+        </View>
+        <CustomSpacer isHorizontal={true} space={sw64} />
+        <View>
+          <TextSpaceArea spaceToBottom={sh8} text={DASHBOARD_FILTER.LABEL_END_DATE} />
+          <NewDatePicker
+            datePickerStyle={{ height: sh143 }}
+            mode="date"
+            minimumDate={startDate}
+            maximumDate={moment().toDate()}
+            setValue={handleEndDate}
+            value={endDate}
+          />
+        </View>
+      </View>
+      <CustomSpacer space={sh24} />
+      <View style={{ ...flexRow, ...px(sw24) }}>
+        <NewCheckBoxDropdownV2
+          handleChange={handleCaseStatus}
+          items={caseStatusList}
+          label={DASHBOARD_FILTER.LABEL_ORDER_STATUS}
+          value={caseStatus!}
+        />
+      </View>
+      <CustomSpacer space={sh24} />
+    </View>
+  );
+};

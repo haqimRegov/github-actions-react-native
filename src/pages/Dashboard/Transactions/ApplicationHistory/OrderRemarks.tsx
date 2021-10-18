e76e2cc9f77afa638d9db6415b22efe1bce748ec
Nithin <johnnithin08@gmail.com>
2021-10-18 16:08:16 +0800

    [update]: order remarks style changes

--- src/pages/Dashboard/Transactions/ApplicationHistory/OrderRemarks.tsx
+++ src/pages/Dashboard/Transactions/ApplicationHistory/OrderRemarks.tsx
@@ -5,62 +5,83 @@ import { CustomSpacer } from "../../../../components";
 import { Language } from "../../../../constants/language";
 import { IcoMoon } from "../../../../icons";
 import {
+  centerHorizontal,
+  centerVertical,
   colorRed,
+  colorWhite,
   flexRow,
   flexWrap,
-  fs10RegBlack2,
   fs12BoldBlack2,
+  fs12RegBlack2,
   fs16BoldBlack1,
   px,
   sh12,
   sh16,
-  sh8,
+  sh4,
   sw12,
   sw16,
-  sw24,
+  sw28,
   sw296,
   sw36,
   sw716,
-  sw8,
 } from "../../../../styles";
 
 const { DASHBOARD_HOME } = Language.PAGE;
 
 declare interface OrderRemarksProps {
   remarks: IDashboardRemark[];
-  status: OrderStatusType;
+  remarkTitle?: string;
+  showIcon?: boolean;
+  status: OrderStatusType | IEDDStatusType;
 }
 
-export const OrderRemarks: FunctionComponent<OrderRemarksProps> = ({ remarks, status }: OrderRemarksProps) => {
+export const OrderRemarks: FunctionComponent<OrderRemarksProps> = ({ remarks, remarkTitle, showIcon, status }: OrderRemarksProps) => {
   const title =
     status === "BR - Rerouted" || status === "HQ - Rerouted"
       ? DASHBOARD_HOME.LABEL_REASONS_REROUTED
       : DASHBOARD_HOME.LABEL_REASONS_REJECTED;
+  const defaultTitle = remarkTitle !== undefined ? remarkTitle : title;
 
   return (
     <Fragment>
-      <CustomSpacer space={sh16} />
-      <View style={px(sw16)}>
-        <Text style={fs16BoldBlack1}>{title}</Text>
-        <CustomSpacer space={sh12} />
-        <View style={{ ...flexWrap, width: sw716, ...flexRow }}>
-          <CustomSpacer space={sh8} />
-          {remarks.map(({ label, remark }: IDashboardRemark, index) => {
-            return (
-              <Fragment key={index}>
-                {index % 2 === 0 ? <CustomSpacer isHorizontal={true} space={sw8} /> : null}
-                <IcoMoon color={colorRed._2} name={"error"} size={sw24} />
-                <CustomSpacer isHorizontal={true} space={sw12} />
-                <View style={{ width: sw296, marginBottom: sh16 }}>
-                  <Text style={fs12BoldBlack2}>{label}</Text>
-                  {remark.map((text: string, remarkIndex: number) => (
-                    <Text key={remarkIndex} style={fs10RegBlack2}>{`• ${text}`}</Text>
-                  ))}
-                </View>
-                {index % 2 === 0 ? <CustomSpacer isHorizontal={true} space={sw36} /> : null}
-              </Fragment>
-            );
-          })}
+      <View style={{ backgroundColor: colorWhite._1, borderBottomLeftRadius: sw12, borderBottomRightRadius: sw12 }}>
+        <CustomSpacer space={sh16} />
+        <View style={{ ...px(sw16) }}>
+          <Text style={fs16BoldBlack1}>{defaultTitle}</Text>
+          <CustomSpacer space={sh4} />
+          <View style={{ ...flexWrap, width: sw716, ...flexRow }}>
+            {remarks.map(({ label, remark }: IDashboardRemark, index) => {
+              return (
+                <Fragment key={index}>
+                  {/* {index % 2 !== 0 && index !== 0 ? <CustomSpacer isHorizontal={true} space={sw8} /> : null} */}
+                  <View style={{ ...flexRow, ...centerVertical, marginBottom: sh16 }}>
+                    {showIcon !== false ? (
+                      <Fragment>
+                        <View style={centerHorizontal}>
+                          <IcoMoon color={colorRed._2} name={"error-octagon"} size={sw28} />
+                        </View>
+                        <CustomSpacer isHorizontal={true} space={sw12} />
+                      </Fragment>
+                    ) : null}
+                    <View style={{ ...centerHorizontal, width: sw296 }}>
+                      <Text style={{ ...fs12BoldBlack2, lineHeight: sh12 }}>{label}</Text>
+                      {remark.length > 0
+                        ? remark.map((text: string, remarkIndex: number) => {
+                            const updatedText = remarkIndex !== 0 ? `• ${text}` : text;
+                            return (
+                              <Text key={remarkIndex} style={{ ...fs12RegBlack2, lineHeight: sh12 }}>
+                                {updatedText}
+                              </Text>
+                            );
+                          })
+                        : null}
+                    </View>
+                  </View>
+                  {index % 2 === 0 ? <CustomSpacer isHorizontal={true} space={sw36} /> : null}
+                </Fragment>
+              );
+            })}
+          </View>
         </View>
       </View>
     </Fragment>

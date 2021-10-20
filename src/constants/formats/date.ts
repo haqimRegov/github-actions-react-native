fcce2cbe7898c8439e1bf587e2963ad26d00b59e
Nithin <johnnithin08@gmail.com>
2021-10-20 14:35:34 +0800

    [update]: added date format

--- src/constants/formats/date.ts
+++ src/constants/formats/date.ts
@@ -1,5 +1,6 @@
 export const DEFAULT_DATE_FORMAT = "DD/MM/YYYY";
 export const DEFAULT_TIME_FORMAT = "h:mm A";
+export const DEFAULT_DATE_TIME_FORMAT = "DD/MM/YYYY, h:mma";
 
 export const DATE_OF_BIRTH_FORMAT = "MM/DD/YYYY";
 export const CALENDAR_FORMAT = "YYYY-MM-DD";

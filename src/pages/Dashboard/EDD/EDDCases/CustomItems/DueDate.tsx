7291e3a92d82d998fe8d4bfd5d8c427279f3c5ac
Nithin <johnnithin08@gmail.com>
2021-10-26 17:41:23 +0800

    bug fix update 72

--- src/pages/Dashboard/EDD/EDDCases/CustomItems/DueDate.tsx
+++ src/pages/Dashboard/EDD/EDDCases/CustomItems/DueDate.tsx
@@ -9,10 +9,11 @@ export interface DueDateProps extends ITableCustomItem {}
 
 export const DueDate: FunctionComponent<DueDateProps> = ({ item }: DueDateProps) => {
   const { targetDate, daysRemaining } = item.rawData as IEDDDashboardCase;
+  const defaultTargetDate = targetDate !== null ? moment(targetDate, "x").format(PAYMENT_DATE_FORMAT) : "-";
   return (
     <View style={centerHorizontal}>
-      <Text style={fs12RegBlue2}>{moment(targetDate, "x").format(PAYMENT_DATE_FORMAT)}</Text>
-      <Text style={fs10RegBlue8}>{daysRemaining}</Text>
+      <Text style={fs12RegBlue2}>{defaultTargetDate}</Text>
+      {daysRemaining !== null ? <Text style={fs10RegBlue8}>{daysRemaining}</Text> : null}
     </View>
   );
 };

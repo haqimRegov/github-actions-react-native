430d902ef7ffeb8a9896a82f8b3dc0b0478baec4
Aldrin Castro <aldrin.castro@regovtech.com>
2022-02-03 11:46:58 +0800

    updated OrderSummary files

---
+++ src/pages/Dashboard/Transactions/OrderSummary/Account.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, useState } from "react";
+
+import { AccountSummary } from "../../../../templates";
+import { structureProfile } from "../../../../utils";
+
+declare interface AccountDetailsProps {
+  data: IDashboardOrderSummary;
+  setFile: (value?: FileBase64) => void;
+}
+
+export const AccountDetails: FunctionComponent<AccountDetailsProps> = ({ data, setFile }: AccountDetailsProps) => {
+  const { profile, transactionDetails } = data;
+  const [accountHolder, setAccountHolder] = useState<TypeAccountHolder>("Principal");
+
+  const accountHolderIndex = accountHolder === "Principal" ? 0 : 1;
+  const { accountType, accountOperationMode, registrationDate } = transactionDetails;
+  const { idNumber, idType, name, uploadedDocument } = profile[accountHolderIndex];
+  const updatedProfile = {
+    ...profile[accountHolderIndex],
+    accountType: accountType,
+    registrationDate: registrationDate,
+    accountOperationMode: accountOperationMode,
+  };
+
+  const structuredData = structureProfile(accountHolder, updatedProfile, setFile);
+
+  const handleViewId = () => {
+    setFile(uploadedDocument[0]);
+  };
+
+  return (
+    <AccountSummary
+      accountHolder={accountHolder}
+      accountType={accountType}
+      data={structuredData}
+      handleViewId={handleViewId}
+      idNumber={idNumber}
+      idType={idType}
+      name={name}
+      setAccountHolder={setAccountHolder}
+    />
+  );
+};

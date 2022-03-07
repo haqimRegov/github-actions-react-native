a18c20562a72941e2c7a2478aca70067d7fbc243
Nithin <johnnithin08@gmail.com>
2022-03-07 19:59:14 +0800

    [update] :added utmc for epf in payment

--- src/pages/Dashboard/Transactions/OrderSummary/OrderDetails.tsx
+++ src/pages/Dashboard/Transactions/OrderSummary/OrderDetails.tsx
@@ -176,6 +176,7 @@ export const OrderDetails: FunctionComponent<OrderDetailsProps> = ({ data, isSch
                   { label: DASHBOARD_ORDER_DETAILS.LABEL_PAYMENT_METHOD, title: `${payment.paymentMethod}`, titleStyle: fsTransformNone },
                   { label: DASHBOARD_ORDER_DETAILS.LABEL_EPF_ACCOUNT, title: payment.epfAccountNumber! },
                   { label: DASHBOARD_ORDER_DETAILS.LABEL_EPF_REFERENCE, title: payment.epfReferenceNo! },
+                  { label: DASHBOARD_ORDER_DETAILS.LABEL_UTMC, title: payment.utmc! },
                 );
               }
 

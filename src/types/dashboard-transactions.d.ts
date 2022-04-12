94c2e83756e537f70f44ff4b6a80a5b054f534bb
Nithin <johnnithin08@gmail.com>
2022-04-12 13:34:44 +0800

    [update]: types

--- src/types/dashboard-transactions.d.ts
+++ src/types/dashboard-transactions.d.ts
@@ -1,14 +1,29 @@
+declare type ITransactionPills = "pending" | "rerouted" | "submitted";
+
 declare interface IDashboardRemark {
   label: string;
   remark: string[];
+}
+
+declare interface IItemWithCount {
+  count: number;
+  document: string;
+}
+
+declare interface IDashboardReason {
+  content: string[];
+  documents: IItemWithCount[];
+  isCompleted?: boolean;
+  isDisabled?: boolean;
+  title: string;
 }
 
 declare interface IDashboardOrder {
   accountType: TypeAccountChoices;
   canProceed: boolean;
   clientId: string;
-  jointId: string | null;
   createdOn: string;
+  documents: IItemWithCount[];
   dueDate: string;
   investorName: {
     principal: string;
@@ -16,8 +31,11 @@ declare interface IDashboardOrder {
   };
   isScheduled: boolean;
   isSeen: boolean;
+  jointId: string | null;
+  label?: string;
   lastUpdated: string;
   orderNumber: string;
+  reason: IDashboardReason[];
   remark: IDashboardRemark[];
   status: OrderStatusType;
   totalInvestment: IOrderAmount[];
@@ -30,16 +48,20 @@ declare interface ITransactionsTab {
   orders: IDashboardOrder[];
   page: number;
   pages: number;
+  pill?: ITransactionPills;
   sort: ITransactionsSort[];
 }
 
 declare interface ITransactionsDashboard {
   approved: ITransactionsTab;
   approvedCount: number;
-  pending: ITransactionsTab;
+  incomplete: ITransactionsTab;
+  incompleteCount: number;
   pendingCount: number;
   rejected: ITransactionsTab;
   rejectedCount: number;
+  reroutedCount: number;
+  submittedCount: number;
 }
 
 declare interface ITransactionsSort {
@@ -74,7 +96,7 @@ declare interface IShowDateBy {
 }
 
 declare type TransactionsSortValueType = "ascending" | "descending";
-declare type TransactionsTabType = "pending" | "approved" | "rejected";
+declare type TransactionsTabType = "incomplete" | "approved" | "rejected";
 declare type TransactionsPageType = "UploadDocuments" | "UploadHardCopy" | "Transactions" | "OrderSummary" | "DashboardPayment";
 declare type DashboardPageType = "Inbox" | "Transactions" | "Profile" | "EDD" | "Investors";
 declare interface IDashboardAll extends IDashboardOrder, IInvestorAccountsData {}

10e6a00f4ab7ce7e60b7fad51e90f98f73312cf1
Nithin <johnnithin08@gmail.com>
2022-04-12 13:42:52 +0800

    [update]: order remarks

--- src/pages/Dashboard/Transactions/ApplicationHistory/OrderRemarks.tsx
+++ src/pages/Dashboard/Transactions/ApplicationHistory/OrderRemarks.tsx
@@ -1,95 +1,232 @@
-import React, { Fragment, FunctionComponent } from "react";
-import { Text, View, ViewStyle } from "react-native";
+import React, { Fragment, FunctionComponent, useState } from "react";
+import { Image, ImageStyle, LayoutChangeEvent, Pressable, Text, View, ViewStyle } from "react-native";
 
-import { CustomSpacer } from "../../../../components";
+import { LocalAssets } from "../../../../assets/images/LocalAssets";
+import { Badge, CustomSpacer, OutlineButton } from "../../../../components";
 import { Language } from "../../../../constants/language";
 import { IcoMoon } from "../../../../icons";
 import {
-  centerHorizontal,
-  centerVertical,
-  colorRed,
+  border,
+  borderBottomGray2,
+  centerHV,
+  circle,
+  colorBlue,
+  colorGreen,
   colorWhite,
+  disabledOpacity6,
   flexRow,
   flexWrap,
-  fs10BoldGray5,
-  fs10RegGray5,
-  fs16BoldBlack2,
+  fs10RegGray6,
+  fs12BoldBlack2,
+  fs12BoldBlue1,
+  fs12RegBlack2,
   px,
-  sh12,
-  sh13,
+  py,
+  rowCenterVertical,
+  sh11,
+  sh14,
   sh16,
   sh4,
-  sw12,
+  sh40,
+  sh8,
+  sw1,
+  sw100,
+  sw14,
   sw16,
   sw24,
-  sw296,
-  sw36,
-  sw716,
+  sw278,
+  sw340,
+  sw4,
+  sw8,
 } from "../../../../styles";
 
 const { DASHBOARD_HOME } = Language.PAGE;
 
-declare interface OrderRemarksProps {
+interface OrderRemarksProps {
   cardStyle?: ViewStyle;
-  remarks: IDashboardRemark[];
+  handleNavigation?: (route: string) => void;
+  remarks: IDashboardReason[];
   remarkTitle?: string;
   showIcon?: boolean;
   status: OrderStatusType | IEDDStatusType;
 }
 
+declare interface ILayoutValues {
+  values: number[];
+}
 export const OrderRemarks: FunctionComponent<OrderRemarksProps> = ({
   cardStyle,
+  handleNavigation,
   remarks,
   remarkTitle,
-  showIcon,
   status,
 }: OrderRemarksProps) => {
-  const title =
-    status === "BR - Rerouted" || status === "HQ - Rerouted"
-      ? DASHBOARD_HOME.LABEL_REASONS_REROUTED
-      : DASHBOARD_HOME.LABEL_REASONS_REJECTED;
-  const defaultTitle = remarkTitle !== undefined ? remarkTitle : title;
+  const [itemLayouts, setItemLayouts] = useState<ILayoutValues[]>([]);
+  const [heights, setHeights] = useState<number[]>([]);
+  const [showAll, setShowAll] = useState<boolean>(false);
+
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
+  const checkTitle =
+    status === "BR - Rerouted" || status === "HQ - Rerouted" ? DASHBOARD_HOME.LABEL_REASON_REROUTED : DASHBOARD_HOME.LABEL_REASON_REJECTED;
+  const defaultTitle = remarkTitle !== undefined ? remarkTitle : checkTitle;
+  const containerStyle: ViewStyle = {
+    ...px(sw16),
+    ...py(sh8),
+    ...border(colorWhite._1, sw1, sw8),
+    backgroundColor: colorWhite._1,
+    width: 370,
+    marginBottom: sh14,
+    ...cardStyle,
+  };
+  const imageStyle: ImageStyle = {
+    width: sw16,
+    height: sw16,
+  };
 
   return (
     <Fragment>
-      <View style={{ backgroundColor: colorWhite._1, borderBottomLeftRadius: sw12, borderBottomRightRadius: sw12 }}>
+      <View>
+        <View style={borderBottomGray2} />
         <CustomSpacer space={sh16} />
-        <View style={{ ...px(sw16) }}>
-          <Text style={fs16BoldBlack2}>{defaultTitle}</Text>
-          <CustomSpacer space={sh4} />
-          <View style={{ ...flexWrap, width: sw716, ...flexRow }}>
-            {remarks.map(({ label, remark }: IDashboardRemark, index) => {
-              return (
-                <Fragment key={index}>
-                  {/* {index % 2 !== 0 && index !== 0 ? <CustomSpacer isHorizontal={true} space={sw8} /> : null} */}
-                  <View style={{ ...flexRow, ...centerVertical, marginBottom: sh16 }}>
-                    {showIcon !== false ? (
+        <Text style={fs12RegBlack2}>{defaultTitle}</Text>
+        <CustomSpacer space={sh4} />
+        <View style={{ ...flexWrap, ...flexRow }}>
+          {remarks.map(({ isCompleted, isDisabled, title, content, documents }: IDashboardReason, index) => {
+            let count = 0;
+            let itemIndex = -1;
+            if (heights[index] > sh40) {
+              const numberOfLines = 2;
+              let countLines = 1;
+              if (itemLayouts.length > 0 && itemLayouts[index] !== undefined) {
+                itemIndex = itemLayouts[index].values.findIndex((eachItem: number, eachItemIndex: number) => {
+                  count = count + eachItem + sw16;
+                  if (count >= sw340 || (countLines === numberOfLines && count >= sw278 && eachItemIndex !== itemLayouts.length - 1)) {
+                    if (countLines !== numberOfLines) {
+                      countLines++;
+                      count = eachItem;
+                    } else if (countLines === numberOfLines) {
+                      return true;
+                    }
+                  }
+                  return false;
+                });
+              }
+            }
+            const disabledStyle: ViewStyle =
+              isDisabled === true || isCompleted === true
+                ? {
+                    ...disabledOpacity6,
+                  }
+                : {};
+
+            const iconName = isCompleted === true ? "success" : "warning";
+
+            return (
+              <Fragment key={index}>
+                <View style={containerStyle}>
+                  <View style={flexRow}>
+                    {title !== "Invalid Information" && title !== "Others" ? (
                       <Fragment>
-                        <View style={centerHorizontal}>
-                          <IcoMoon color={colorRed._2} name="error-octagon" size={sw24} />
-                        </View>
-                        <CustomSpacer isHorizontal={true} space={sw12} />
+                        {isCompleted === true ? (
+                          <View>
+                            <Badge icon={{ name: iconName, size: sh8 }} style={circle(sw16, colorGreen._1)}>
+                              <IcoMoon color={colorWhite._1} name={iconName} size={sw16} />
+                            </Badge>
+                          </View>
+                        ) : (
+                          <Image source={LocalAssets.icon.iconWarning} style={imageStyle} />
+                        )}
                       </Fragment>
-                    ) : null}
-                    <View style={{ ...centerHorizontal, width: sw296, ...cardStyle }}>
-                      <Text style={{ ...fs10BoldGray5, lineHeight: sh13 }}>{label}</Text>
-                      {remark.length > 0
-                        ? remark.map((text: string, remarkIndex: number) => {
-                            const updatedText = remark.length > 1 ? `• ${text}` : text;
+                    ) : (
+                      <View style={{ width: sw14, height: sw14, ...centerHV, borderRadius: sw100, backgroundColor: colorBlue._1 }}>
+                        <Text style={{ fontSize: sh11, color: colorWhite._1 }}>i</Text>
+                      </View>
+                    )}
+                    <CustomSpacer isHorizontal={true} space={sw8} />
+                    <View>
+                      <Text style={fs12BoldBlack2}>{title}</Text>
+                      {content.length > 1 && title !== "Others" ? (
+                        <Fragment>
+                          {content.map((eachContent: string, eachContentIndex: number) => {
                             return (
-                              <Text key={remarkIndex} style={{ ...fs10RegGray5, lineHeight: sh12 }}>
-                                {updatedText}
-                              </Text>
+                              <View key={eachContentIndex} style={rowCenterVertical}>
+                                <CustomSpacer isHorizontal={true} space={sw4} />
+                                <Text style={fs10RegGray6}>{`• ${eachContent}`}</Text>
+                              </View>
                             );
-                          })
-                        : null}
+                          })}
+                        </Fragment>
+                      ) : (
+                        <Text style={fs10RegGray6}>{content[0]}</Text>
+                      )}
                     </View>
                   </View>
-                  {index % 2 === 0 ? <CustomSpacer isHorizontal={true} space={sw36} /> : null}
-                </Fragment>
-              );
-            })}
-          </View>
+                  <CustomSpacer space={sh8} />
+                  {documents !== null && documents.length > 0 ? (
+                    <View
+                      onLayout={(event: LayoutChangeEvent) => {
+                        const { height } = event.nativeEvent.layout;
+                        const updatedHeights = [...heights];
+                        updatedHeights[index] = height;
+                        setHeights(updatedHeights);
+                      }}
+                      style={{ ...rowCenterVertical, ...flexWrap, ...disabledStyle }}
+                      pointerEvents={isDisabled === true || isCompleted === true ? "none" : "auto"}>
+                      {documents.map((eachProcess: IItemWithCount, eachProcessIndex: number) => {
+                        const handlePress = () => {
+                          if (handleNavigation !== undefined) {
+                            handleNavigation(eachProcess.document);
+                          }
+                        };
+                        const checkIcon = isCompleted === false || isCompleted === undefined ? "upload" : undefined;
+
+                        return (
+                          <Fragment key={eachProcessIndex}>
+                            {itemIndex === -1 || eachProcessIndex < itemIndex || showAll === true ? (
+                              <Fragment>
+                                <View
+                                  key={eachProcessIndex}
+                                  onLayout={(event: LayoutChangeEvent) => {
+                                    const { width } = event.nativeEvent.layout;
+                                    const updatedLayouts = [...itemLayouts];
+                                    const updatedValues = updatedLayouts[index] !== undefined ? updatedLayouts[index].values : [];
+                                    updatedValues[eachProcessIndex] = width;
+                                    updatedLayouts[index] = { values: updatedValues };
+                                    setItemLayouts(updatedLayouts);
+                                  }}
+                                  style={{ marginBottom: sh8, marginRight: sw16 }}>
+                                  <OutlineButton
+                                    badgeCount={eachProcess.count > 1 ? eachProcess.count : undefined}
+                                    color={colorBlue._1}
+                                    icon={checkIcon}
+                                    onPress={handlePress}
+                                    text={eachProcess.document}
+                                  />
+                                </View>
+                              </Fragment>
+                            ) : null}
+                          </Fragment>
+                        );
+                      })}
+                      {showAll === false && itemIndex !== -1 ? (
+                        <View style={rowCenterVertical}>
+                          <Pressable onPress={handleShowAll}>
+                            <View>
+                              <Text style={fs12BoldBlue1}>{`+${documents.length - itemIndex} more`}</Text>
+                            </View>
+                          </Pressable>
+                        </View>
+                      ) : null}
+                    </View>
+                  ) : null}
+                </View>
+                {index % 2 === 0 ? <CustomSpacer isHorizontal={true} space={sw24} /> : null}
+              </Fragment>
+            );
+          })}
         </View>
       </View>
     </Fragment>

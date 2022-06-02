d1cb33e57d5b0abf672b19f9f6e19780e4e96ac5
Aldrin Castro <aldrin.castro@regovtech.com>
2022-06-02 11:31:00 +0800

    added style props

--- src/components/Input/NumberPicker.tsx
+++ src/components/Input/NumberPicker.tsx
@@ -51,6 +51,7 @@ export interface NumberPickerProps {
   disableLeftIcon?: boolean;
   disableRightIcon?: boolean;
   error: string | undefined;
+  innerContainerStyle?: ViewStyle;
   interval?: number;
   label?: string;
   labelStyle?: TextStyle;
@@ -76,6 +77,7 @@ export const NumberPicker: FunctionComponent<NumberPickerProps> = ({
   disableLeftIcon,
   disableRightIcon,
   error,
+  innerContainerStyle,
   interval,
   label,
   labelStyle,
@@ -153,6 +155,7 @@ export const NumberPicker: FunctionComponent<NumberPickerProps> = ({
     width: sw136,
     ...errorBorderColor,
     ...errorBGColor,
+    ...innerContainerStyle,
   };
 
   const defaultContainerStyle: ViewStyle = {

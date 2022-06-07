24c4caba8a0a126cd20c963c2d4ff0eac048edf9
Nithin <johnnithin08@gmail.com>
2022-06-07 10:05:23 +0530

    [update]: dashbaord filter

--- src/pages/Dashboard/Transactions/ApplicationHistory/Filter.tsx
+++ src/pages/Dashboard/Transactions/ApplicationHistory/Filter.tsx
@@ -1,28 +1,30 @@
 import moment from "moment";
 import React, { FunctionComponent } from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
-import { CustomSpacer, NewCheckBoxDropdown, NewDatePicker, NewDropdown, TextSpaceArea } from "../../../../components";
+import { CustomSpacer, MultiSelectChips, NewCheckBoxDropdown, NewDatePicker, StatusBadge, TextSpaceArea } from "../../../../components";
 import { Language } from "../../../../constants";
 import {
   DICTIONARY_APPROVED_STATUS_FILTER,
   DICTIONARY_PENDING_STATUS_FILTER,
   DICTIONARY_REJECTED_STATUS_FILTER,
   DICTIONARY_TRANSACTIONS_ACCOUNT_TYPE,
-  DICTIONARY_TRANSACTIONS_DATE,
   DICTIONARY_TRANSACTIONS_TYPE,
 } from "../../../../data/dictionary";
-import { flexRow, fs16BoldGray6, px, sh143, sh16, sh24, sh32, sh4, sw24, sw64 } from "../../../../styles";
+import { flexRow, fs12BoldGray6, fs16BoldGray6, px, sh143, sh16, sh24, sh32, sh4, sw16, sw24, sw64, sw8 } from "../../../../styles";
+import { isNotEmpty, titleCaseString } from "../../../../utils";
 
 const { DASHBOARD_FILTER } = Language.PAGE;
 interface TransactionsFilterProps {
   activeTab: TransactionsTabType;
+  availableFilters: ITransactionsAvailableFilter;
   filter: ITransactionsFilter;
   setFilter: (value: ITransactionsFilter) => void;
 }
 
 export const TransactionsFilter: FunctionComponent<TransactionsFilterProps> = ({
   activeTab,
+  availableFilters,
   filter,
   setFilter,
 }: TransactionsFilterProps) => {
@@ -47,11 +49,11 @@ export const TransactionsFilter: FunctionComponent<TransactionsFilterProps> = ({
     setFilter({ ...filter, orderStatus: value });
   };
 
-  const handleAccountType = (value: string) => {
+  const handleAccountType = (value: string[]) => {
     setFilter({ ...filter, accountType: value });
   };
 
-  const handleTransactionsType = (value: string) => {
+  const handleTransactionsType = (value: string[]) => {
     setFilter({ ...filter, transactionsType: value });
   };
 
@@ -68,17 +70,42 @@ export const TransactionsFilter: FunctionComponent<TransactionsFilterProps> = ({
       orderStatusList = DICTIONARY_PENDING_STATUS_FILTER;
       break;
   }
+  const pills: string[] = ["Created", "Last Updated"];
+  const disabledTransactionTypes: string[] = DICTIONARY_TRANSACTIONS_TYPE.map((eachTransaction) => eachTransaction.value).filter(
+    (eachValue: string) => !availableFilters.transactionType.includes(eachValue),
+  );
+  const disabledAccountTypes: string[] = DICTIONARY_TRANSACTIONS_ACCOUNT_TYPE.map((eachAccountType) => eachAccountType.value).filter(
+    (eachValue: string) => !availableFilters.accountType.map((eachType) => titleCaseString(eachType)).includes(eachValue),
+  );
+  const disabledStatuses: string[] = orderStatusList
+    .map((eachStatus) => eachStatus.value)
+    .filter((eachValue: string) => !availableFilters.orderStatus.includes(eachValue));
 
   return (
     <View>
       <TextSpaceArea spaceToBottom={sh16} spaceToTop={sh32} style={{ ...fs16BoldGray6, ...px(sw24) }} text={DASHBOARD_FILTER.TITLE} />
-      <View style={{ ...flexRow, ...px(sw24) }}>
-        <NewDropdown
-          handleChange={handleDateSorting}
-          items={DICTIONARY_TRANSACTIONS_DATE}
-          label={DASHBOARD_FILTER.LABEL_DATE_SORTING}
-          value={dateSorting!}
-        />
+      <View style={px(sw24)}>
+        <Text style={fs12BoldGray6}>{DASHBOARD_FILTER.LABEL_DATE_SORTING}</Text>
+        <CustomSpacer space={sh4} />
+        <View style={flexRow}>
+          {pills.map((currentPill: string, index: number) => {
+            const handlePress = () => {
+              handleDateSorting(pills[index]);
+            };
+
+            return (
+              <View key={index} style={flexRow}>
+                <StatusBadge
+                  color={isNotEmpty(dateSorting) && dateSorting!.toLowerCase() === currentPill.toLowerCase() ? "primary" : "secondary"}
+                  onPress={handlePress}
+                  style={{ ...px(sw16) }}
+                  text={currentPill}
+                />
+                <CustomSpacer isHorizontal={true} space={sw8} />
+              </View>
+            );
+          })}
+        </View>
       </View>
       <CustomSpacer space={sh24} />
       <View style={{ ...flexRow, ...px(sw24) }}>
@@ -112,7 +139,8 @@ export const TransactionsFilter: FunctionComponent<TransactionsFilterProps> = ({
       </View>
       <CustomSpacer space={sh24} />
       <View style={{ ...flexRow, ...px(sw24) }}>
-        <NewDropdown
+        <NewCheckBoxDropdown
+          disabledValues={disabledTransactionTypes}
           handleChange={handleTransactionsType}
           items={DICTIONARY_TRANSACTIONS_TYPE}
           label={DASHBOARD_FILTER.LABEL_TRANSACTIONS_TYPE}
@@ -120,6 +148,7 @@ export const TransactionsFilter: FunctionComponent<TransactionsFilterProps> = ({
         />
         <CustomSpacer isHorizontal={true} space={sw64} />
         <NewCheckBoxDropdown
+          disabledValues={disabledStatuses}
           handleChange={handleOrderStatus}
           items={orderStatusList}
           label={DASHBOARD_FILTER.LABEL_ORDER_STATUS}
@@ -128,11 +157,13 @@ export const TransactionsFilter: FunctionComponent<TransactionsFilterProps> = ({
       </View>
       <CustomSpacer space={sh24} />
       <View style={{ ...flexRow, ...px(sw24) }}>
-        <NewDropdown
-          handleChange={handleAccountType}
-          items={DICTIONARY_TRANSACTIONS_ACCOUNT_TYPE}
-          label={DASHBOARD_FILTER.LABEL_ACCOUNT_TYPE}
-          value={accountType!}
+        <MultiSelectChips
+          disabledValues={disabledAccountTypes}
+          onSelect={handleAccountType}
+          labels={DICTIONARY_TRANSACTIONS_ACCOUNT_TYPE}
+          header={DASHBOARD_FILTER.LABEL_ACCOUNT_TYPE}
+          space={sw8}
+          values={accountType!}
         />
       </View>
     </View>

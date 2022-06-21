7532178c8168619ce8e9f4aafbb6331df34e83ff
Aldrin Castro <aldrin.castro@regovtech.com>
2022-06-21 12:26:14 +0800

    added new ColorCard

---
+++ src/components/Card/ColorCard.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent, ReactNode } from "react";
+import { View, ViewStyle } from "react-native";
+
+import { border, colorBlue, colorGray, fs12RegGray5, fs16BoldBlack2, px, py, sh12, sh16, sh32, sw1, sw16, sw32, sw8 } from "../../styles";
+import { LabeledTitle } from "../Views";
+
+export interface ColorCardProps {
+  containerStyle?: ViewStyle;
+  content?: ReactNode;
+  contentStyle?: ViewStyle;
+  customHeader?: ReactNode;
+  header: LabeledTitleProps | "custom";
+  headerStyle?: ViewStyle;
+}
+
+export const ColorCard: FunctionComponent<ColorCardProps> = ({
+  containerStyle,
+  content,
+  contentStyle,
+  customHeader,
+  header,
+  headerStyle,
+}: ColorCardProps) => {
+  const defaultContainerStyle: ViewStyle = {
+    ...border(colorGray._2, sw1, sw16),
+  };
+
+  const defaultHeaderStyle: ViewStyle = {
+    ...px(sw32),
+    ...py(sh12),
+    backgroundColor: colorBlue._3,
+    borderTopLeftRadius: sw8,
+    borderTopRightRadius: sw8,
+    ...headerStyle,
+  };
+
+  const defaultContentStyle: ViewStyle = {
+    ...px(sw32),
+    paddingBottom: sh32,
+    paddingTop: sh16,
+    ...contentStyle,
+  };
+
+  const defaultHeader =
+    header !== "custom" ? <LabeledTitle {...header} labelStyle={fs16BoldBlack2} titleStyle={{ ...fs12RegGray5 }} /> : null;
+
+  return (
+    <View style={{ ...defaultContainerStyle, ...containerStyle }}>
+      <View style={defaultHeaderStyle}>{header === "custom" && customHeader !== undefined ? customHeader : defaultHeader}</View>
+      <View style={defaultContentStyle}>{content}</View>
+    </View>
+  );
+};

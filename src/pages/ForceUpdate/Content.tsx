3ccd6c06e28b3e42109d501d885ff96ed5c07da6
Aldrin Castro <aldrin.castro@regovtech.com>
2022-06-25 18:43:54 +0800

    added Force Update Page, content and sample page

---
+++ src/pages/ForceUpdate/Content.tsx
@@ -0,0 +1,81 @@
+import React, { Fragment } from "react";
+import { Text } from "react-native";
+import { connect } from "react-redux";
+
+import { ConfirmationModal } from "../../components";
+import { FORCE_UPDATE_ROUTES, Language } from "../../constants";
+import { ForceUpdateMapDispatchToProps, ForceUpdateMapStateToProps, ForceUpdateStoreProps } from "../../store";
+import { fs16RegGray6 } from "../../styles";
+import { ContactContent } from "./Contact";
+import { ContactSummaryContent } from "./ContactSummary";
+import { CRSContent, DeclarationSummaryContent, FATCAContent } from "./Declarations";
+import { RiskAssessmentContent } from "./RiskAssessment";
+import { Sample } from "./Sample";
+
+const { FORCE_UPDATE } = Language.PAGE;
+interface ForceUpdateProps extends ForceUpdateContentProps, ForceUpdateStoreProps {
+  navigation: IStackNavigationProp;
+  route: string;
+}
+
+const ForceUpdateContentComponent = ({
+  handleCancelForceUpdate,
+  handleResetForceUpdate,
+  cancelForceUpdate,
+  ...props
+}: ForceUpdateProps) => {
+  const newProps = {
+    ...props,
+    handleCancelForceUpdate: handleCancelForceUpdate,
+    handleResetForceUpdate: handleResetForceUpdate,
+  };
+
+  let content: JSX.Element;
+  const backToDashboardLabel = FORCE_UPDATE.LABEL_BACK_FORCE_UPDATE;
+
+  switch (newProps.route) {
+    case FORCE_UPDATE_ROUTES.Contact:
+      content = <ContactContent {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.ContactSummary:
+      content = <ContactSummaryContent {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.RiskAssessment:
+      content = <RiskAssessmentContent {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.FATCADeclaration:
+      content = <FATCAContent {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.CRSDeclaration:
+      content = <CRSContent {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.DeclarationSummary:
+      content = <DeclarationSummaryContent {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.Signatures:
+      content = <Sample title={FORCE_UPDATE_ROUTES.Signatures} {...newProps} />;
+      break;
+    case FORCE_UPDATE_ROUTES.TermsAndConditions:
+      content = <Sample title={FORCE_UPDATE_ROUTES.TermsAndConditions} {...newProps} />;
+      break;
+      break;
+    default:
+      content = <Sample title="sample" {...newProps} />;
+      break;
+  }
+  return (
+    <Fragment>
+      {content}
+      <ConfirmationModal
+        handleCancel={handleCancelForceUpdate}
+        handleContinue={handleResetForceUpdate}
+        labelCancel={FORCE_UPDATE.BUTTON_NO}
+        labelContinue={FORCE_UPDATE.BUTTON_YES}
+        title={FORCE_UPDATE.EDIT_TITLE}
+        visible={cancelForceUpdate!}>
+        <Text style={fs16RegGray6}>{backToDashboardLabel}</Text>
+      </ConfirmationModal>
+    </Fragment>
+  );
+};
+export const ForceUpdateContent = connect(ForceUpdateMapStateToProps, ForceUpdateMapDispatchToProps)(ForceUpdateContentComponent);
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export * from "./Auth";
 export * from "./Dashboard";
+export * from "./ForceUpdate";
 export * from "./Logout";
 export * from "./Onboarding";

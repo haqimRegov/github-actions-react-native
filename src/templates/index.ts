e961dbe5ce5917410561da8b2c55244d9389e104
Aldrin Castro <aldrin.castro@regovtech.com>
2022-07-03 22:33:48 +0800

    updated templates index

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,7 @@
+export * from "./CustomItems";
 export * from "./Dashboard";
 export * from "./EDD";
+export * from "./OrderSubmission";
 export * from "./Onboarding";
 export * from "./Payment";
 export * from "./RiskAssessment";

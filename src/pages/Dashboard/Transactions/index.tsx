e060f02a021b49f00230c47c1ec8e1be01aec39b
Aldrin Castro <aldrin.castro@regovtech.com>
2022-07-04 16:08:07 +0800

    implemented AccountInformation, InvestorProfile, OrderSummary pages for Transactions

--- src/pages/Dashboard/Transactions/index.tsx
+++ src/pages/Dashboard/Transactions/index.tsx
@@ -2,10 +2,12 @@ import React, { FunctionComponent, useState } from "react";
 import { View } from "react-native";
 
 import { flexChild } from "../../../styles";
+import { AccountInformationPage } from "./AccountInformation";
 import { ApplicationHistory } from "./ApplicationHistory";
 import { UploadDocuments, UploadHardCopy } from "./Documents";
-import { DashboardOrderSummary } from "./OrderSummary";
+import { OrderSummaryPage } from "./OrderSummary";
 import { DashboardPayment } from "./Payment/Payment";
+import { InvestorProfilePage } from "./Profile";
 
 interface TransactionsProps {
   handleRoute: (route: DashboardPageType) => void;
@@ -18,10 +20,13 @@ export const Transactions: FunctionComponent<TransactionsProps> = (props: Transa
   const [route, setRoute] = useState<TransactionsPageType>("Transactions");
   const [activeTab, setActiveTab] = useState<TransactionsTabType>("incomplete");
   const [orderSummaryActiveTab, setOrderSummaryActiveTab] = useState<OrderSummaryTabType>("order");
+
   const setScreen = (nextPage: TransactionsPageType) => {
     setRoute(nextPage);
   };
+
   const pageProps = { setScreen: setScreen, navigation: navigation };
+
   let transactionsPage: JSX.Element = (
     <ApplicationHistory
       {...pageProps}
@@ -32,17 +37,29 @@ export const Transactions: FunctionComponent<TransactionsProps> = (props: Transa
       setOrderSummaryActiveTab={setOrderSummaryActiveTab}
     />
   );
+
   if (route === "UploadDocuments") {
     transactionsPage = <UploadDocuments {...pageProps} />;
   }
+
   if (route === "UploadHardCopy") {
     transactionsPage = <UploadHardCopy {...pageProps} />;
   }
+
   if (route === "OrderSummary") {
-    transactionsPage = <DashboardOrderSummary activeTab={orderSummaryActiveTab} setActiveTab={setOrderSummaryActiveTab} {...pageProps} />;
+    transactionsPage = <OrderSummaryPage activeTab={orderSummaryActiveTab} setActiveTab={setOrderSummaryActiveTab} {...pageProps} />;
   }
+
   if (route === "DashboardPayment") {
     transactionsPage = <DashboardPayment {...pageProps} />;
+  }
+
+  if (route === "InvestorProfile") {
+    transactionsPage = <InvestorProfilePage {...pageProps} />;
+  }
+
+  if (route === "AccountInformation") {
+    transactionsPage = <AccountInformationPage {...pageProps} />;
   }
 
   return <View style={flexChild}>{transactionsPage}</View>;

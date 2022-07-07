3985ac550ef3d90f8e4cd3b4d3e423b9449dc1eb
Aldrin Castro <aldrin.castro@regovtech.com>
2022-07-07 10:49:52 +0800

    updated order history table

--- src/templates/AccountInformation/OrderHistoryTab.tsx
+++ src/templates/AccountInformation/OrderHistoryTab.tsx
@@ -3,14 +3,12 @@ import { View } from "react-native";
 
 import { AdvanceTable, CustomSpacer } from "../../components";
 import { Language } from "../../constants";
-import { CustomTableItem } from "../../pages/Dashboard/Transactions/ApplicationHistory/CustomTableItem";
 import {
-  centerHorizontal,
   centerHV,
   colorBlue,
+  fs10BoldBlue1,
   fs12RegBlue1,
   fsTransformNone,
-  justifyContentStart,
   px,
   sh8,
   sw144,
@@ -18,16 +16,16 @@ import {
   sw192,
   sw24,
   sw56,
-  sw8,
   sw96,
 } from "../../styles";
 import { EmptyStateTable } from "../Dashboard";
+import { CustomTableItem } from "../OrderSummary/CustomTableItem";
 
 const { ORDER_HISTORY } = Language.PAGE;
 
 export interface OrderHistoryTabProps {
-  handleViewOrderSummary: (order: IInvestorOrderHistory) => void;
-  orderHistory: IInvestorOrderHistory[] | null;
+  handleViewOrderSummary: (order: IDashboardOrder) => void;
+  orderHistory: IDashboardOrder[] | null;
 }
 
 export const OrderHistoryTab: FunctionComponent<OrderHistoryTabProps> = ({
@@ -35,18 +33,19 @@ export const OrderHistoryTab: FunctionComponent<OrderHistoryTabProps> = ({
   orderHistory,
 }: OrderHistoryTabProps) => {
   const handleView = (item: ITableRowData) => {
-    handleViewOrderSummary(item.rawData as unknown as IInvestorOrderHistory);
+    handleViewOrderSummary(item.rawData as unknown as IDashboardOrder);
   };
 
   const columns: ITableColumn[] = [
     {
-      key: [{ key: "orderNo", textStyle: { ...fs12RegBlue1, ...fsTransformNone } }],
+      key: [{ key: "orderNumber" }],
+      textStyle: { ...fs12RegBlue1, ...fsTransformNone },
       title: ORDER_HISTORY.TABLE_HEADER_ORDER_NUMBER,
       viewStyle: { width: sw152 },
     },
     {
-      key: [{ key: "transactionType", textStyle: fs12RegBlue1 }],
-      textStyle: fsTransformNone,
+      key: [{ key: "transactionType" }],
+      textStyle: { ...fs12RegBlue1, ...fsTransformNone },
       title: ORDER_HISTORY.TABLE_HEADER_TRANSACTION_TYPE,
       titleStyle: fsTransformNone,
       viewStyle: { width: sw144 },
@@ -58,12 +57,11 @@ export const OrderHistoryTab: FunctionComponent<OrderHistoryTabProps> = ({
       viewStyle: { width: sw144 },
     },
     {
-      customHeader: true,
       customItem: true,
-      itemStyle: { ...justifyContentStart, ...px(sw8) },
       key: [{ key: "lastUpdated" }],
       title: ORDER_HISTORY.TABLE_HEADER_LAST_UPDATED,
-      viewStyle: { width: sw96, ...px(0), ...centerHorizontal },
+      titleStyle: fs10BoldBlue1,
+      viewStyle: { width: sw96 },
     },
     {
       customItem: true,
@@ -86,7 +84,13 @@ export const OrderHistoryTab: FunctionComponent<OrderHistoryTabProps> = ({
       <AdvanceTable
         columns={columns}
         data={orderHistory === null ? [] : (orderHistory as unknown as ITableData[])}
-        RenderCustomItem={(customItemData: ITableCustomItem) => <CustomTableItem {...customItemData} sortedColumns={["lastUpdated"]} />}
+        RenderCustomItem={(customItemData: ITableCustomItem) => (
+          <CustomTableItem
+            {...customItemData}
+            item={{ ...customItemData.item, rawData: { ...customItemData.item.rawData, dueDate: null } }}
+            sortedColumns={["lastUpdated"]}
+          />
+        )}
         RenderEmptyState={() => <EmptyStateTable isFetching={false} isNotFiltered={true} noTransactionsYet={false} />}
       />
     </View>

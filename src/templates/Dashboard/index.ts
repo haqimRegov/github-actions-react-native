8bae121404fefd3d652cef406853a7f2e7c71f7d
Aldrin Castro <aldrin.castro@regovtech.com>
2022-07-17 22:42:18 +0800

    added new AccountTab, DocumentsTabNew, updated space for Physical Doc

--- src/templates/Dashboard/index.ts
+++ src/templates/Dashboard/index.ts
@@ -1,5 +1,7 @@
 export * from "./AccountSummary";
+export * from "./AccountTab";
 export * from "./DocumentsTab";
+export * from "./DocumentsTabNew";
 export * from "./EmptyStateTable";
 export * from "./JointDocumentHeader";
 export * from "./SummaryColorCard";

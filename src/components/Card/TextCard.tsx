412a3ffabe4e4f56fcf2f782c35d81bea9aa206b
Aldrin Castro <aldrin.castro@regovtech.com>
2022-07-17 22:41:24 +0800

    added showEmptyDash props

--- src/components/Card/TextCard.tsx
+++ src/components/Card/TextCard.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import { TextStyle, View, ViewStyle } from "react-native";
 
 import { fs12BoldGray5, fsCapitalize, sh16, sh2, sw144, sw16, sw240, sw32 } from "../../styles";
+import { isNotEmpty } from "../../utils";
 import { LabeledTitle } from "../Views/LabeledTitle";
 import { CustomCard } from "./Custom";
 
@@ -11,6 +12,7 @@ export interface TextCardProps {
   labelStyle?: TextStyle;
   spaceToLabel?: number;
   titleStyle?: TextStyle;
+  showEmptyDash?: boolean;
 
   direction?: "row" | "column";
   itemsPerGroup?: number;
@@ -22,6 +24,7 @@ export const TextCard: FunctionComponent<TextCardProps> = ({
   data,
   itemStyle,
   labelStyle,
+  showEmptyDash,
   spaceToLabel,
   titleStyle,
   ...customCardProps
@@ -30,11 +33,13 @@ export const TextCard: FunctionComponent<TextCardProps> = ({
   const items = data.map((item: LabeledTitleProps, index: number) => {
     const itemDefaultStyle: ViewStyle = { width: sw240, ...itemStyle, ...item.style };
     const itemSpaceToLabel = item.spaceToLabel !== undefined ? item.spaceToLabel : defaultSpaceToLabel;
+    const title = showEmptyDash === true && isNotEmpty(item.title) === false ? "-" : item.title;
 
     return (
       <View key={index} style={itemDefaultStyle}>
         <LabeledTitle
           {...item}
+          title={title}
           iconSize={item.iconSize !== undefined ? item.iconSize : sw16}
           labelStyle={{ ...fs12BoldGray5, ...labelStyle, ...item.labelStyle }}
           onPress={item.onPress}

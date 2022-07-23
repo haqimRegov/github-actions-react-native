df1e682b20dff1c66b45609482bf2575762b626b
Aldrin Castro <aldrin.castro@regovtech.com>
2022-07-23 15:59:10 +0800

    updated network actions (added submit and generate pdf transactions)

---
+++ src/types/network-actions/submit-pdf-transactions.d.ts
@@ -0,0 +1,27 @@
+declare interface ISubmitPdfTransactionsDocument {
+  pdf: FileBase64;
+  orderNumber: string;
+  adviserSignature: FileBase64;
+  clientSignature: FileBase64;
+  jointSignature?: FileBase64;
+}
+
+declare interface ISubmitPdfTransactionsRequest {
+  clientId: string;
+  documents: ISubmitPdfTransactionsDocument[];
+  initId: string;
+  isConfirmed: boolean;
+  isEtb: boolean;
+  isForceUpdate: boolean;
+}
+
+declare interface ISubmitPdfTransactionsResult {
+  message: string;
+  status: string;
+}
+
+declare type ISubmitPdfTransactionsResponse = IMutationResponse<ISubmitPdfTransactionsResult> | undefined;
+
+declare interface ISubmitPdfTransactionsMutation {
+  submitPdfTransactions: ISubmitPdfTransactionsResponse;
+}

4c822b1aa7596f1638d19b6e19f1e0f4cc58e2de
Aldrin Castro <aldrin.castro@regovtech.com>
2022-08-09 12:44:54 +0800

    fixed pre uat defect 6, hide resubmit order if reason is payment or document until submitted

--- src/pages/Dashboard/Transactions/ApplicationHistory/Incomplete/Actions.tsx
+++ src/pages/Dashboard/Transactions/ApplicationHistory/Incomplete/Actions.tsx
@@ -74,11 +74,13 @@ export const PendingOrderActions: FunctionComponent<PendingOrderActionsProps> = 
   const labelUploadPayment = isScheduled ? DASHBOARD_HOME.LABEL_UPLOAD_RECURRING : DASHBOARD_HOME.LABEL_UPLOAD_PROOF;
 
   const findPaymentReason =
-    isNotEmpty(reason) === true
-      ? reason.findIndex((eachReason) => eachReason.title === "Invalid Proof of Payment:" && eachReason.isSubmitted === true)
-      : -1;
+    isNotEmpty(reason) === true ? reason.findIndex((eachReason) => eachReason.title === "Invalid Proof of Payment:") : -1;
+  const findDocumentReason = isNotEmpty(reason) === true ? reason.findIndex((eachReason) => eachReason.title === "Invalid Documents:") : -1;
+  const onlyOtherReason = isNotEmpty(reason) === true && reason.length === 1 && reason[0].title === "Others";
 
-  const isReroutedPaymentSubmitted = findPaymentReason !== -1;
+  const isPaymentSubmitted = findPaymentReason !== -1 ? reason![findPaymentReason].isSubmitted === true : true;
+  const isDocumentSubmitted = findDocumentReason !== -1 ? reason![findDocumentReason].isSubmitted === true : true;
+  const isSubmitted = isPaymentSubmitted === true && isDocumentSubmitted === true;
 
   return (
     <View style={{ borderRadius: sw8 }}>
@@ -88,25 +90,20 @@ export const PendingOrderActions: FunctionComponent<PendingOrderActionsProps> = 
       ((status === "BR - Rerouted" || status === "HQ - Rerouted") &&
         remark &&
         remark.findIndex((eachRemark) => eachRemark.label === "Payment" || eachRemark.label === "Others") !== -1 &&
-        isReroutedPaymentSubmitted === false) ? (
+        isPaymentSubmitted === false) ? (
         <IconText name="upload" onPress={handleUploadPayment} style={itemStyle} text={labelUploadPayment} />
       ) : null}
       {status === "Pending Doc" ||
       status === "Pending Doc & Payment" ||
       ((status === "BR - Rerouted" || status === "HQ - Rerouted") &&
         remark &&
-        remark.findIndex((eachRemark) => eachRemark.label === "Document" || eachRemark.label === "Others") !== -1) ? (
+        remark.findIndex((eachRemark) => eachRemark.label === "Document" || eachRemark.label === "Others") !== -1 &&
+        isDocumentSubmitted === false) ? (
         <IconText name="upload" onPress={handleUploadDocs} style={itemStyle} text={DASHBOARD_HOME.LABEL_UPLOAD} />
       ) : null}
       {status === "Pending Physical Doc" && canSubmitHardcopy === true ? (
         <IconText name="file" onPress={handleSubmitHardCopy} style={itemStyle} text={DASHBOARD_HOME.LABEL_SUBMIT_PHYSICAL} />
       ) : null}
-      {/* <IconText
-        name="print"
-        onPress={handlePrintAccountOpening}
-        style={itemStyle}
-        text={DASHBOARD_HOME.LABEL_PRINT_ACCOUNT_OPENING}
-      /> */}
       {status === "Submitted" && withHardcopy === true ? (
         <IconText
           name="print"
@@ -115,7 +112,7 @@ export const PendingOrderActions: FunctionComponent<PendingOrderActionsProps> = 
           text={DASHBOARD_HOME.LABEL_PRINT_SUBMISSION_SUMMARY}
         />
       ) : null}
-      {status === "BR - Rerouted" || status === "HQ - Rerouted" ? (
+      {(status === "BR - Rerouted" || status === "HQ - Rerouted") && (isSubmitted === true || onlyOtherReason === true) ? (
         <IconText name="print" onPress={handleResubmit} style={itemStyle} text={DASHBOARD_HOME.LABEL_RESUBMIT_ORDER} />
       ) : null}
     </View>

3e99fc1bebe0ab0187c4acf8db76b3a96ac74e88
Aldrin Castro <aldrin.castro@regovtech.com>
2022-08-19 14:57:17 +0800

    updated Account Summary page (updated UI, copywriting, renamed step, implemented AMP product)

--- src/pages/NewSales/RiskProfile.tsx
+++ src/pages/NewSales/RiskProfile.tsx
@@ -23,6 +23,7 @@ import { RiskMapDispatchToProps, RiskMapStateToProps, RiskStoreProps } from "../
 import {
   autoWidth,
   border,
+  borderBottomBlue4,
   centerHorizontal,
   circle,
   colorBlue,
@@ -34,12 +35,13 @@ import {
   flexChild,
   flexRow,
   fs10BoldBlue1,
+  fs10BoldGray6,
   fs10RegBlue9,
   fs10RegGray5,
   fs10RegGray6,
   fs12BoldBlack2,
-  fs12BoldBlue1,
   fs12RegBlack2,
+  fs12RegGray5,
   fs16BoldBlue1,
   fs16BoldGray6,
   fs16RegBlack2,
@@ -51,10 +53,11 @@ import {
   px,
   py,
   rowCenterVertical,
+  sh12,
   sh16,
   sh2,
   sh24,
-  sh32,
+  sh4,
   sh8,
   sw05,
   sw1,
@@ -64,6 +67,7 @@ import {
   sw228,
   sw239,
   sw24,
+  sw240,
   sw32,
   sw4,
   sw40,
@@ -77,7 +81,8 @@ const { RISK_ASSESSMENT, NEW_SALES_SUMMARY } = Language.PAGE;
 
 declare interface IRiskSummaryProps extends RiskStoreProps, NewSalesContentProps {}
 
-const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
+const NewSalesRiskSummaryComponent: FunctionComponent<IRiskSummaryProps> = ({
+  addInvestmentDetails,
   client,
   details,
   handleNextStep,
@@ -92,7 +97,7 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
   const { jointHolder, principalHolder } = details!;
   const { accountType } = client;
   const { accountDetails, disabledSteps, finishedSteps } = newSales;
-  const { accountNo, fundType, isEpf } = accountDetails;
+  const { accountNo, ampFund, fundType, isEpf } = accountDetails;
 
   const riskProfile: IRiskProfile = {
     appetite: riskScore.appetite,
@@ -165,20 +170,64 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
     },
   ];
 
+  const handleInvest = () => {
+    const initialStateArray: IProductSales[] = [];
+    if (ampFund !== undefined) {
+      let newMasterClassList: IProductClasses = {};
+      ampFund!.masterList.forEach((list: IProductMasterList) => {
+        const dump = { class: list.class !== null ? list.class : "No Class", currency: list.currency };
+        const findClassIndex = Object.keys(newMasterClassList).indexOf(dump.class);
+        if (findClassIndex === -1) {
+          newMasterClassList = { ...newMasterClassList, [dump.class]: [list] };
+        } else {
+          newMasterClassList[dump.class].push(list);
+        }
+        return dump;
+      });
+
+      const newState: IProductSales = {
+        investment: {
+          fundId: ampFund.masterList[0].fundId,
+          fundPaymentMethod: ampFund.isEpfOnly === "Yes" ? "EPF" : "Cash",
+          investmentAmount: "",
+          investmentSalesCharge: "",
+          fundCurrency: ampFund.masterList[0].currency,
+          fundClass: ampFund.masterList[0].class !== null ? ampFund.masterList[0].class : "No Class",
+          scheduledInvestment: false,
+          prsType: ampFund.prsType,
+        },
+        fundDetails: { ...ampFund },
+        masterClassList: newMasterClassList,
+      };
+
+      initialStateArray.push(newState);
+      addInvestmentDetails(initialStateArray);
+    }
+  };
+
   const handlePageContinue = () => {
-    const updatedFinishedSteps: TypeNewSalesKey[] = [...finishedSteps];
+    const updatedFinishedSteps: TypeNewSalesKey[] = [...new Set(finishedSteps)];
     const updatedDisabledSteps: TypeNewSalesKey[] = [...disabledSteps];
-    updatedFinishedSteps.push("RiskProfile");
-    const findRiskProfile = disabledSteps.indexOf("RiskProfile");
+    const nextStep: TypeNewSalesRoute = accountDetails.ampFund !== undefined ? "ProductsConfirmation" : "ProductsList";
+    updatedFinishedSteps.push("RiskSummary");
+    const findRiskProfile = disabledSteps.indexOf("RiskSummary");
     if (findRiskProfile !== -1) {
       updatedDisabledSteps.splice(findRiskProfile, 1);
     }
     const findProducts = disabledSteps.indexOf("Products");
+    const findProductsConfirmation = disabledSteps.indexOf("ProductsConfirmation");
     if (findProducts !== -1) {
       updatedDisabledSteps.splice(findProducts, 1);
     }
+    if (findProductsConfirmation !== -1 && accountDetails.ampFund !== undefined) {
+      updatedDisabledSteps.splice(findProductsConfirmation, 1);
+    }
+    if (accountDetails.ampFund !== undefined) {
+      handleInvest();
+    }
+
     updateNewSales({ ...newSales, finishedSteps: updatedFinishedSteps, disabledSteps: updatedDisabledSteps });
-    handleNextStep("ProductsList");
+    handleNextStep(nextStep);
   };
 
   const handleEdit = () => {
@@ -189,6 +238,21 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
     setCurrentProfile("Principal");
     setPage(1);
   };
+
+  const ampDetails: LabeledTitleProps[] = [];
+  // TODO change to !==
+  if (fundType === "amp") {
+    ampDetails.push(
+      {
+        label: RISK_ASSESSMENT.LABEL_LANDING_FUND,
+        title: ampFund?.landingFund,
+      },
+      {
+        label: RISK_ASSESSMENT.NEW_SALES_RISK_CATEGORY,
+        title: ampFund?.riskCategory,
+      },
+    );
+  }
 
   const handleJointProfile = () => {
     setCurrentProfile("Joint");
@@ -205,7 +269,6 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
     RNInAppBrowser.openLink(DICTIONARY_LINK_AIMS);
   };
   const checkContinueLabel = isRiskUpdated === true ? RISK_ASSESSMENT.BUTTON_CONTINUE : RISK_ASSESSMENT.BUTTON_NEXT;
-  const heading = RISK_ASSESSMENT.NEW_SALES_HEADING;
   const accountTitle = `${client.accountType} ${RISK_ASSESSMENT.LABEL_ACCOUNT}`;
 
   const buttonStyle: ViewStyle = {
@@ -213,19 +276,20 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
     ...flexRow,
     width: sw120,
     ...px(sw16),
-    ...py(sh8),
+    ...py(sh4),
     ...border(colorBlue._1, sw1, sw24),
     backgroundColor: colorWhite._1,
-    height: sh32,
+    borderColor: colorRed._1,
+    borderWidth: sw1,
+    height: sh24,
   };
 
   const checkEpf = isEpf === true ? "EPF" : "Cash";
   const tags = accountNo !== "" ? [getProductTagType(fundType), checkEpf] : [];
-  const checkJointHeader = RISK_ASSESSMENT.NEW_SALES_HEADING_2_JOINT;
-  const header = accountNo !== "" ? RISK_ASSESSMENT.NEW_SALES_HEADING_2_NEW_FUND : checkJointHeader;
+  // const checkJointHeader = accountType === "Joint" ? RISK_ASSESSMENT.NEW_SALES_HEADING_2_JOINT : RISK_ASSESSMENT.NEW_SALES_HEADING_2;
+  const header = accountNo !== "" ? RISK_ASSESSMENT.NEW_SALES_HEADING_2_SALES : RISK_ASSESSMENT.NEW_SALES_HEADING_2;
   // const checkJointSubtitle = accountType === "Joint" ? RISK_ASSESSMENT.NEW_SALES_HEADING_3_JOINT : RISK_ASSESSMENT.NEW_SALES_HEADING_3;
-  // const subtitle = accountNo !== "" ? RISK_ASSESSMENT.NEW_SALES_HEADING_3_NEW_FUND : RISK_ASSESSMENT.NEW_SALES_HEADING_3;
-  const subtitle = RISK_ASSESSMENT.NEW_SALES_HEADING_3_NEW_FUND;
+  const subtitle = accountNo !== "" ? RISK_ASSESSMENT.NEW_SALES_HEADING_3_SALES : RISK_ASSESSMENT.NEW_SALES_HEADING_3;
   const checkAccountNo = accountNo !== "" ? accountNo : null;
 
   const checkLabel = client.accountType === "Joint" ? NEW_SALES_SUMMARY.LABEL_PRINCIPAL_PROFILE : NEW_SALES_SUMMARY.LABEL_INVESTOR_PROFILE;
@@ -259,7 +323,7 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
   return (
     <View style={flexChild}>
       {page === 0 ? (
-        <ContentPage {...defaultContentProps} heading={heading} headingStyle={headerStyle}>
+        <ContentPage {...defaultContentProps} heading={RISK_ASSESSMENT.NEW_SALES_HEADING} headingStyle={headerStyle}>
           <CustomSpacer space={sh24} />
           <View style={px(sw24)}>
             <View style={flexRow}>
@@ -332,25 +396,48 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
                 borderBottomColor: colorRed._1,
               }}
             />
-            {newSales.accountDetails.accountNo !== "" ? (
+            {accountNo !== "" ? (
               <Fragment>
                 <CustomSpacer space={sh24} />
                 <ColorCard
                   containerStyle={noBorder}
                   content={
                     <View>
-                      <CustomSpacer space={sh8} />
-                      <Text style={fs16RegBlack2}>{RISK_ASSESSMENT.NEW_SALES_PRODUCT_AND_SERVICE_HINT}</Text>
-                      <CustomSpacer space={sh8} />
-                      <Pressable onPress={handleAims} style={buttonStyle}>
-                        <Text style={fs12BoldBlue1}>{RISK_ASSESSMENT.NEW_SALES_PRODUCT_AND_SERVICE_CHECK_AIMS}</Text>
-                        <CustomSpacer isHorizontal={true} space={sw8} />
-                        <IcoMoon color={colorBlue._1} name="external" size={sw16} />
-                      </Pressable>
-                      <CustomSpacer space={sh8} />
+                      <View style={flexRow}>
+                        <Text style={fs16RegBlack2}>{RISK_ASSESSMENT.NEW_SALES_PRODUCT_AND_SERVICE_HINT}</Text>
+                        <CustomFlexSpacer />
+                        <Pressable onPress={handleAims} style={buttonStyle}>
+                          <Text style={fs10BoldGray6}>{RISK_ASSESSMENT.NEW_SALES_PRODUCT_AND_SERVICE_CHECK_AIMS}</Text>
+                          <CustomSpacer isHorizontal={true} space={sw8} />
+                          <IcoMoon color={colorBlue._1} name="external" size={sw16} />
+                        </Pressable>
+                      </View>
+                      {fundType === "amp" ? (
+                        <Fragment>
+                          <CustomSpacer space={sh24} />
+                          <View style={flexRow}>
+                            <IcoMoon color={colorBlue._1} name="fund" size={sw24} />
+                            <CustomSpacer isHorizontal={true} space={sw8} />
+                            <Text style={fs16BoldBlue1}>{ampFund!.fundAbbr}</Text>
+                            <CustomSpacer isHorizontal={true} space={sw16} />
+                            <View style={{ ...flexChild, ...centerHorizontal }}>
+                              <View style={borderBottomBlue4} />
+                            </View>
+                          </View>
+                          <Text style={{ ...fs12RegGray5, paddingLeft: sw32 }}>{ampFund?.issuingHouse}</Text>
+                          <CustomSpacer space={sh12} />
+                          <TextCard
+                            data={ampDetails}
+                            itemsPerGroup={3}
+                            spaceBetweenItem={sw32}
+                            spaceBetweenGroup={0}
+                            itemStyle={{ width: sw240 }}
+                          />
+                        </Fragment>
+                      ) : null}
                     </View>
                   }
-                  contentStyle={{ ...border(colorBlue._3, sw1), ...px(sw24), paddingBottom: sh8 }}
+                  contentStyle={{ ...border(colorBlue._3, sw1), ...px(sw24), ...py(sh24) }}
                   header={{ label: RISK_ASSESSMENT.NEW_SALES_PRODUCT_AND_SERVICE, labelStyle: fs16BoldBlue1 }}
                   headerStyle={{ ...border(colorBlue._3, sw1), backgroundColor: colorWhite._1, ...px(sw24), ...py(sh16) }}
                 />
@@ -400,7 +487,7 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
           </View>
         </ContentPage>
       ) : (
-        <InvestorProfilePage clientId={clientId} setPage={setPage} />
+        <InvestorProfilePage clientId={clientId!} setPage={setPage} />
       )}
       {page === 0 ? (
         <Fragment>
@@ -408,7 +495,7 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
           <SelectionBanner
             cancelOnPress={handleBackToInvestor}
             continueDisabled={riskProfile.appetite === ""}
-            label={RISK_ASSESSMENT.NEW_SALES_RISK_PROFILE_SUMMARY}
+            label={RISK_ASSESSMENT.NEW_SALES_ACCOUNT_SUMMARY}
             labelStyle={fs20BoldBlack2}
             labelCancel={RISK_ASSESSMENT.BUTTON_CANCEL}
             labelSubmit={checkContinueLabel}
@@ -431,4 +518,4 @@ const NewSalesRiskProfileComponent: FunctionComponent<IRiskSummaryProps> = ({
   );
 };
 
-export const NewSalesRiskProfile = connect(RiskMapStateToProps, RiskMapDispatchToProps)(NewSalesRiskProfileComponent);
+export const NewSalesRiskSummary = connect(RiskMapStateToProps, RiskMapDispatchToProps)(NewSalesRiskSummaryComponent);

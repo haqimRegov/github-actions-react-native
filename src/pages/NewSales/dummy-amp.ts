7efe9544bc4d4139550ed117d066a492a07c9acf
Aldrin Castro <aldrin.castro@regovtech.com>
2022-08-19 15:05:39 +0800

    [temp] dummy amp fund

---
+++ src/pages/NewSales/dummy-amp.ts
@@ -0,0 +1,81 @@
+export const ampData: IProduct = {
+  annualTrusteeFee: null,
+  ampCategory: "Moderate",
+  fundCode: "504:amp",
+  fundCurrencies: [],
+  fundClasses: [],
+  fundId: "504",
+  fundAbbr: "AMP 2",
+  fundName: "AMP 2",
+  fundType: "AMP",
+  fundCategory: "MODERATE",
+  issuingHouse: "KENANGA INVESTORS BERHAD",
+  riskCategory: "MEDIUM",
+  isEpf: "No",
+  isEpfOnly: "No",
+  prsType: null,
+  isSyariah: "No",
+  isWholesale: "No",
+  isScheduled: "Yes",
+  ampFee: "0.75",
+  masterList: [
+    {
+      fundId: "504",
+      class: null,
+      currency: "MYR",
+      salesCharge: {
+        epf: {
+          min: "1.50",
+          max: "3.00",
+        },
+        cash: {
+          min: "3.00",
+          max: "3.00",
+        },
+      },
+      newSalesAmount: {
+        epf: {
+          min: null,
+          max: null,
+        },
+        cash: {
+          min: "5000",
+          max: "9999999999",
+        },
+      },
+      topUpAmount: {
+        epf: {
+          min: null,
+          max: null,
+        },
+        cash: {
+          min: "1000",
+          max: "9999999999",
+        },
+      },
+    },
+  ],
+  fundObjective:
+    "The portfolio seeks to deliver potential consistent return through investments in Kenanga Investors Berhad approved money market, fixed income and equities funds.",
+  landingFund: "Kenanga Growth Fund",
+  annualManagementFee: null,
+  performance: "0",
+  docs: [
+    {
+      url: "https://omni-documents-sit.s3.ap-southeast-1.amazonaws.com/products/25/1610106541/KGF%20AR.pdf?AWSAccessKeyId=ASIAXIR5JBNYIVVSILVQ&Expires=1659451250&Signature=Uuxbjk2DlwWyINXmtx41PuzOvdA%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEB0aDmFwLXNvdXRoZWFzdC0xIkYwRAIgINLJnxz%2FkgpSG5O1U31jkt67tsBCUG7BxvLAmAr8m0ACIBud4nlrnVuyiP4uPck9%2B%2BPGI22mzCUrNeHfLtCJ7am3Ku0CCHYQAhoMNDk5NDE4NDY3MTg0Igx9oM%2FUcdfJ2PvQpSwqygLoOLh3FAkh2giYdp1Sfmc3EN4vP%2FoNUZu15wuU6CujRs4FdcSi5ZfQ4JQQmDyZdmZxwsDvFbNvhCdCap5aAv5mT5iUA2rZ%2FSwg3tzKVIhtIAFcoJzJZCCcF9jtCcjs%2BUK2w3ADcqYcA%2F74L90mFpv2%2B89Ku4bjjj6JXxTvPiiCsb8eSX0p01zn7H%2F1%2B5xVRUiN%2FoDgxB%2FvZij8A5sxuqeDSSYPo1rP%2FRgLKf90G3HM6tYkw8SVyfNpGitvUoOVqwDxIS7pWgAuPTsG2mxBDHg5oIxdnFBCCmlR7Mp1QYE5bbgzDnDpfSS6znDIyeNG3RTUjsMiv%2BkwcAH%2FJ7dF%2FUDjfIK2Yc1T1zT%2FuEHrLxRL0lDsgPPT69ZjmX3x8eY6%2BbOh4QbGuFhk%2BE1OVtRNOnUKTcwXVCJQ1knp0DUkX%2FhSNTnFdtcMu2kRTJEw5sKklwY6nwGlW8EFOZhFQS2dYo7oVgzkzoaZruUqv60a3qZ76htpBKcoVY7PUE3iVcTZ6Q%2B8Nm5ONQ7D0L1J1bmNd06IeSwIKXORF0LR3lJfcUcjhAfRkAGteE5%2Fx9Qmol5mWVjoUuWxM3lhsVa7HhyML6xPw0C0ve%2FmeQ75ai4i7TUhoIUfNLAizYHBCloajCGTXEoQ%2FICrqj1smh8OjhNIliUb0ng%3D",
+      name: "annual",
+    },
+    {
+      url: "https://omni-documents-sit.s3.ap-southeast-1.amazonaws.com/products/25/1610106541/KGF%20PHS.pdf?AWSAccessKeyId=ASIAXIR5JBNYIVVSILVQ&Expires=1659451250&Signature=7w3fbP16FVmLLptFBiBMpkft5%2Bg%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEB0aDmFwLXNvdXRoZWFzdC0xIkYwRAIgINLJnxz%2FkgpSG5O1U31jkt67tsBCUG7BxvLAmAr8m0ACIBud4nlrnVuyiP4uPck9%2B%2BPGI22mzCUrNeHfLtCJ7am3Ku0CCHYQAhoMNDk5NDE4NDY3MTg0Igx9oM%2FUcdfJ2PvQpSwqygLoOLh3FAkh2giYdp1Sfmc3EN4vP%2FoNUZu15wuU6CujRs4FdcSi5ZfQ4JQQmDyZdmZxwsDvFbNvhCdCap5aAv5mT5iUA2rZ%2FSwg3tzKVIhtIAFcoJzJZCCcF9jtCcjs%2BUK2w3ADcqYcA%2F74L90mFpv2%2B89Ku4bjjj6JXxTvPiiCsb8eSX0p01zn7H%2F1%2B5xVRUiN%2FoDgxB%2FvZij8A5sxuqeDSSYPo1rP%2FRgLKf90G3HM6tYkw8SVyfNpGitvUoOVqwDxIS7pWgAuPTsG2mxBDHg5oIxdnFBCCmlR7Mp1QYE5bbgzDnDpfSS6znDIyeNG3RTUjsMiv%2BkwcAH%2FJ7dF%2FUDjfIK2Yc1T1zT%2FuEHrLxRL0lDsgPPT69ZjmX3x8eY6%2BbOh4QbGuFhk%2BE1OVtRNOnUKTcwXVCJQ1knp0DUkX%2FhSNTnFdtcMu2kRTJEw5sKklwY6nwGlW8EFOZhFQS2dYo7oVgzkzoaZruUqv60a3qZ76htpBKcoVY7PUE3iVcTZ6Q%2B8Nm5ONQ7D0L1J1bmNd06IeSwIKXORF0LR3lJfcUcjhAfRkAGteE5%2Fx9Qmol5mWVjoUuWxM3lhsVa7HhyML6xPw0C0ve%2FmeQ75ai4i7TUhoIUfNLAizYHBCloajCGTXEoQ%2FICrqj1smh8OjhNIliUb0ng%3D",
+      name: "highlights",
+    },
+    {
+      url: "https://omni-documents-sit.s3.ap-southeast-1.amazonaws.com/products/25/1610106541/KGF%20FFS.pdf?AWSAccessKeyId=ASIAXIR5JBNYIVVSILVQ&Expires=1659451250&Signature=iN0xHLDF3bFfTVPoEuWC9U9de%2BI%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEB0aDmFwLXNvdXRoZWFzdC0xIkYwRAIgINLJnxz%2FkgpSG5O1U31jkt67tsBCUG7BxvLAmAr8m0ACIBud4nlrnVuyiP4uPck9%2B%2BPGI22mzCUrNeHfLtCJ7am3Ku0CCHYQAhoMNDk5NDE4NDY3MTg0Igx9oM%2FUcdfJ2PvQpSwqygLoOLh3FAkh2giYdp1Sfmc3EN4vP%2FoNUZu15wuU6CujRs4FdcSi5ZfQ4JQQmDyZdmZxwsDvFbNvhCdCap5aAv5mT5iUA2rZ%2FSwg3tzKVIhtIAFcoJzJZCCcF9jtCcjs%2BUK2w3ADcqYcA%2F74L90mFpv2%2B89Ku4bjjj6JXxTvPiiCsb8eSX0p01zn7H%2F1%2B5xVRUiN%2FoDgxB%2FvZij8A5sxuqeDSSYPo1rP%2FRgLKf90G3HM6tYkw8SVyfNpGitvUoOVqwDxIS7pWgAuPTsG2mxBDHg5oIxdnFBCCmlR7Mp1QYE5bbgzDnDpfSS6znDIyeNG3RTUjsMiv%2BkwcAH%2FJ7dF%2FUDjfIK2Yc1T1zT%2FuEHrLxRL0lDsgPPT69ZjmX3x8eY6%2BbOh4QbGuFhk%2BE1OVtRNOnUKTcwXVCJQ1knp0DUkX%2FhSNTnFdtcMu2kRTJEw5sKklwY6nwGlW8EFOZhFQS2dYo7oVgzkzoaZruUqv60a3qZ76htpBKcoVY7PUE3iVcTZ6Q%2B8Nm5ONQ7D0L1J1bmNd06IeSwIKXORF0LR3lJfcUcjhAfRkAGteE5%2Fx9Qmol5mWVjoUuWxM3lhsVa7HhyML6xPw0C0ve%2FmeQ75ai4i7TUhoIUfNLAizYHBCloajCGTXEoQ%2FICrqj1smh8OjhNIliUb0ng%3D",
+      name: "fact",
+    },
+    {
+      url: "https://omni-documents-sit.s3.ap-southeast-1.amazonaws.com/products/25/1610106541/KGF%20PI.pdf?AWSAccessKeyId=ASIAXIR5JBNYIVVSILVQ&Expires=1659451250&Signature=BhO6%2BY3X8YsomClMLbs8ZgxTjek%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEB0aDmFwLXNvdXRoZWFzdC0xIkYwRAIgINLJnxz%2FkgpSG5O1U31jkt67tsBCUG7BxvLAmAr8m0ACIBud4nlrnVuyiP4uPck9%2B%2BPGI22mzCUrNeHfLtCJ7am3Ku0CCHYQAhoMNDk5NDE4NDY3MTg0Igx9oM%2FUcdfJ2PvQpSwqygLoOLh3FAkh2giYdp1Sfmc3EN4vP%2FoNUZu15wuU6CujRs4FdcSi5ZfQ4JQQmDyZdmZxwsDvFbNvhCdCap5aAv5mT5iUA2rZ%2FSwg3tzKVIhtIAFcoJzJZCCcF9jtCcjs%2BUK2w3ADcqYcA%2F74L90mFpv2%2B89Ku4bjjj6JXxTvPiiCsb8eSX0p01zn7H%2F1%2B5xVRUiN%2FoDgxB%2FvZij8A5sxuqeDSSYPo1rP%2FRgLKf90G3HM6tYkw8SVyfNpGitvUoOVqwDxIS7pWgAuPTsG2mxBDHg5oIxdnFBCCmlR7Mp1QYE5bbgzDnDpfSS6znDIyeNG3RTUjsMiv%2BkwcAH%2FJ7dF%2FUDjfIK2Yc1T1zT%2FuEHrLxRL0lDsgPPT69ZjmX3x8eY6%2BbOh4QbGuFhk%2BE1OVtRNOnUKTcwXVCJQ1knp0DUkX%2FhSNTnFdtcMu2kRTJEw5sKklwY6nwGlW8EFOZhFQS2dYo7oVgzkzoaZruUqv60a3qZ76htpBKcoVY7PUE3iVcTZ6Q%2B8Nm5ONQ7D0L1J1bmNd06IeSwIKXORF0LR3lJfcUcjhAfRkAGteE5%2Fx9Qmol5mWVjoUuWxM3lhsVa7HhyML6xPw0C0ve%2FmeQ75ai4i7TUhoIUfNLAizYHBCloajCGTXEoQ%2FICrqj1smh8OjhNIliUb0ng%3D",
+      name: "prospectus",
+    },
+  ],
+};

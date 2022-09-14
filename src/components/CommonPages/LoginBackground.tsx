865e9d635a6590c5ac3d1333abc06ff5903807fc
jeremy regov <jeremy.tan@regovtech.com>
2022-09-14 14:41:55 +0800

    [update] Login Background

--- src/components/CommonPages/LoginBackground.tsx
+++ src/components/CommonPages/LoginBackground.tsx
@@ -1,39 +1,37 @@
-import React, { Fragment, ReactNode } from "react";
-import { Image, ImageStyle, ScrollView, TouchableWithoutFeedback, View, ViewStyle } from "react-native";
+import React, { Fragment, ReactNode, version } from "react";
+import { Image, ImageStyle, ScrollView, TouchableWithoutFeedback, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { version } from "../../../package.json";
 import { LocalAssets } from "../../assets/images/LocalAssets";
 import { Language } from "../../constants";
+import { DICTIONARY_LINK_PLATFORM_AGREEMENT, DICTIONARY_LINK_PRIVACY } from "../../data/dictionary";
+import { RNInAppBrowser } from "../../integrations";
 import {
-  border,
-  colorBlue,
   colorWhite,
   DEVICE,
   flexChild,
   flexGrow,
   flexRow,
-  fs12RegBlue1,
-  fs12RegGray6,
-  fs12SemiBoldBlue1,
+  fs10RegGray3,
+  fs10RegGray5,
+  fs12BoldBlue1,
   imageContain,
-  px,
   scaleHeight,
-  sh12,
   sh32,
   sh64,
   sw1,
   sw16,
+  sw160,
   sw164,
   sw168,
   sw24,
   sw48,
   sw532,
   sw56,
-  sw64,
+  sw72,
   sw8,
 } from "../../styles";
-import { IconText, LinkTextGroup, LinkTextProps } from "../Touchables";
+import { LinkTextGroup, LinkTextProps } from "../Touchables";
 import { CustomFlexSpacer, CustomSpacer, TextSpaceArea } from "../Views";
 import { SafeAreaPage } from "./SafeAreaPage";
 
@@ -41,27 +39,21 @@ const { LOGIN } = Language.PAGE;
 
 interface LoginPageProps {
   children: ReactNode;
-  page?: TypeLoginPages;
-  setPage: (page: TypeLoginPages) => void;
 }
 
-export const LoginBackground = ({ children, page, setPage }: LoginPageProps) => {
+export const LoginBackground = ({ children }: LoginPageProps) => {
   const { bottom } = useSafeAreaInsets();
-  // const handlePrivacyPolicy = () => {
-  //   LinkUtils.openLink(DICTIONARY_LINK_PRIVACY);
-  // };
+  const handlePrivacyPolicy = () => {
+    RNInAppBrowser.openLink(DICTIONARY_LINK_PRIVACY);
+  };
+
+  const handlePlatformAgreement = () => {
+    RNInAppBrowser.openLink(DICTIONARY_LINK_PLATFORM_AGREEMENT);
+  };
 
   // const handleTermsAndConditions = () => {
   //   LinkUtils.openLink(DICTIONARY_LINK_TERMS);
   // };
-
-  const handleAgentOnboarding = () => {
-    setPage("FIRST_TIME_LOGIN");
-  };
-
-  const handleBackToLogin = () => {
-    setPage("LOGIN");
-  };
 
   // TODO Module 1A will only support English language
   // const topLinks: LinkTextProps[] = [
@@ -83,31 +75,22 @@ export const LoginBackground = ({ children, page, setPage }: LoginPageProps) => 
   //   style: fs12SemiBoldBlue1,
   // };
 
-  const backToLoginLink = {
-    onPress: handleBackToLogin,
-    text: LOGIN.LINK_BACK_TO_LOGIN,
-    style: fs12SemiBoldBlue1,
-  };
-
   const bottomLinks: LinkTextProps[] = [
-    // {
-    //   onPress: handlePrivacyPolicy,
-    //   text: LOGIN.LINK_PRIVACY_POLICY,
-    //   style: fs12SemiBoldBlue1,
-    // },
     {
-      text: `Build Version ${version}`,
-      style: fs12RegGray6,
+      onPress: handlePlatformAgreement,
+      text: LOGIN.LABEL_PLATFORM_AGREEMENT,
+      style: fs12BoldBlue1,
+    },
+    {
+      onPress: handlePrivacyPolicy,
+      text: LOGIN.LINK_PRIVACY_POLICY,
+      style: fs12BoldBlue1,
     },
   ];
 
   // if (page === "LOGIN") {
   //   bottomLinks.push(agentOnboardingLink);
   // }
-
-  if (page === "PASSWORD_RECOVERY") {
-    bottomLinks.push(backToLoginLink);
-  }
 
   const backgroundStyle: ImageStyle = { width: sw532, height: DEVICE.WINDOW.HEIGHT };
   const logoStyle: ImageStyle = { ...imageContain, height: sh64, width: sw168 };
@@ -141,22 +124,12 @@ export const LoginBackground = ({ children, page, setPage }: LoginPageProps) => 
               <CustomFlexSpacer />
               <View style={flexRow}>
                 <LinkTextGroup links={bottomLinks} style={{ minHeight: sh32 }} spaceToDivider={sw8} />
-                {page === "LOGIN" ? (
-                  <Fragment>
-                    <CustomFlexSpacer />
-                    <IconText
-                      iconPosition="right"
-                      name="profile"
-                      onPress={handleAgentOnboarding}
-                      style={buttonStyle}
-                      text={LOGIN.LINK_AGENT_ONBOARDING}
-                      textStyle={fs12RegBlue1}
-                    />
-                    <CustomSpacer isHorizontal={true} space={sw64} />
-                  </Fragment>
-                ) : null}
               </View>
-              <TextSpaceArea spaceToBottom={bottomSpace} spaceToTop={sh12} style={fs12RegGray6} text={LOGIN.FOOTER_KIB} />
+              <View style={flexRow}>
+                <TextSpaceArea spaceToBottom={bottomSpace} style={fs10RegGray5} text={LOGIN.FOOTER_KIB} />
+                <CustomSpacer isHorizontal space={sw72} />
+                <TextSpaceArea spaceToBottom={bottomSpace} style={fs10RegGray3} text={`Build Version ${version}`} />
+              </View>
             </View>
           </SafeAreaPage>
         </View>

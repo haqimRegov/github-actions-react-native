da82d44a85a8166a653d45d4542d726936563c56
jeremy regov <jeremy.tan@regovtech.com>
2022-09-14 14:40:39 +0800

    [update] english and links

--- src/data/dictionary/links.ts
+++ src/data/dictionary/links.ts
@@ -1,7 +1,8 @@
 export const DICTIONARY_LINK_TAX_RESIDENT = "http://lampiran.hasil.gov.my/pdf/pdfam/3656.pdf";
-export const DICTIONARY_LINK_PRIVACY = "https://www.kenangainvestors.com.my/KIB/KIB.Application.Web/KIBfundsdefault/PrivacyPolicy.aspx";
+export const DICTIONARY_LINK_PRIVACY = "https://www.kenanga.com.my/pdpa/";
 export const DICTIONARY_LINK_TERMS = "https://www.kenangainvestors.com.my/KIB/KIB.Application.Web/KIBFundsDefault/TermsAndConditions.aspx";
-
+export const DICTIONARY_LINK_PLATFORM_AGREEMENT =
+  "https://omni-documents-sit.s3.ap-southeast-1.amazonaws.com/omni-terms-conditions/kenangaPlatformAgreement.pdf";
 export const DICTIONARY_LINK_FULL_TERMS = "https://omni-documents.s3-ap-southeast-1.amazonaws.com/fe-tnc/fullTerms.pdf";
 
 export const DICTIONARY_LINK_AIMS =

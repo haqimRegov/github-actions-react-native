4354e80f74f23d869db966ea13dfdb749bf93c57
Aldrin Castro <aldrin.castro@regovtech.com>
2022-09-27 11:06:09 +0800

    updated NTB RiskAssessment UI

--- src/pages/Onboarding/Questionnaire.tsx
+++ src/pages/Onboarding/Questionnaire.tsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import { LocalAssets } from "../../assets/images/LocalAssets";
 import {
+  ColorCard,
   ConfirmationModal,
   ContentPage,
   CustomSlider,
@@ -11,52 +12,52 @@ import {
   CustomTextInput,
   CustomTooltip,
   LabeledTitle,
-  Question,
 } from "../../components";
-import { Language, NunitoBold, NunitoRegular, ONBOARDING_ROUTES } from "../../constants";
-import { Q2_OPTIONS, Q3_OPTIONS, Q4_OPTIONS, Q5_OPTIONS, Q6_OPTIONS, Q7_OPTIONS, Q8_OPTIONS, Q9_OPTIONS } from "../../data/dictionary";
+import { Language, NunitoBold, NunitoRegular } from "../../constants";
+import { Q2_OPTIONS, Q3_OPTIONS, Q4_OPTIONS_NEW, Q5_OPTIONS, Q6_OPTIONS, Q7_OPTIONS, Q8_OPTIONS, Q9_OPTIONS } from "../../data/dictionary";
 import { IcoMoon } from "../../icons";
 import { getRiskProfile } from "../../network-actions";
 import { RiskMapDispatchToProps, RiskMapStateToProps, RiskStoreProps } from "../../store";
 import {
   alignSelfStart,
   centerHV,
-  centerVertical,
   circleBorder,
   colorBlue,
   colorRed,
   colorWhite,
-  disabledOpacity5,
   flexRow,
-  fs10BoldGray6,
   fs12BoldGray6,
   fs12BoldWhite1,
   fs12RegGray5,
+  fs12RegGray6,
+  fs14RegGray5,
   fs16BoldBlack2,
+  fs16RegBlack2,
   fs16RegGray6,
+  fs18BoldGray6,
   imageContain,
+  rowCenterVertical,
   sh143,
   sh155,
   sh16,
   sh24,
   sh32,
-  sh40,
   sh56,
   sh8,
   sw1,
+  sw11,
   sw12,
   sw140,
+  sw152,
   sw16,
-  sw20,
   sw221,
   sw24,
-  sw256,
-  sw326,
-  sw328,
   sw432,
   sw7,
+  sw72,
   sw8,
 } from "../../styles";
+import { QuestionContent, QuestionHeader } from "../../templates";
 import { isObjectEqual } from "../../utils";
 
 const { RISK_ASSESSMENT } = Language.PAGE;
@@ -104,7 +105,7 @@ const QuestionnaireContentComponent: FunctionComponent<QuestionnaireContentProps
 
   const handleConfirmAssessment = () => {
     resetProducts();
-    handleNextStep(ONBOARDING_ROUTES.ProductRecommendation);
+    handleNextStep("Products");
     const updatedFinishedSteps: TypeOnboardingKey[] = [...finishedSteps];
     const updatedDisabledSteps: TypeOnboardingKey[] = [...disabledSteps];
     updatedFinishedSteps.push("RiskAssessment");
@@ -218,6 +219,8 @@ const QuestionnaireContentComponent: FunctionComponent<QuestionnaireContentProps
     questionEight === -1 ||
     (questionEight !== 0 && questionNine === -1);
 
+  const questionnaireStyle: ViewStyle = { marginLeft: sw24, marginRight: sw72 };
+
   return (
     <Fragment>
       <ContentPage
@@ -225,158 +228,188 @@ const QuestionnaireContentComponent: FunctionComponent<QuestionnaireContentProps
         handleCancel={handleCancelOnboarding}
         handleContinue={handlePageContinue}
         heading={`${RISK_ASSESSMENT.HEADING} ${name},`}
-        headingStyle={{ lineHeight: sh56 }}
-        subheading={RISK_ASSESSMENT.SUBHEADING}>
-        <View style={flexRow}>
+        headingStyle={fs18BoldGray6}
+        subheading={RISK_ASSESSMENT.SUBHEADING}
+        subheadingStyle={fs14RegGray5}>
+        <CustomSpacer space={sh24} />
+        <View style={questionnaireStyle}>
           <CustomSpacer isHorizontal={true} space={sw24} />
           <View>
-            <CustomSpacer space={sh40} />
             <View onStartShouldSetResponderCapture={() => true}>
-              <LabeledTitle
-                label={RISK_ASSESSMENT.LABEL_QUESTION_1}
-                labelStyle={{ ...fs10BoldGray6, ...disabledOpacity5 }}
-                spaceToLabel={sh8}
-                title={RISK_ASSESSMENT.QUESTION_1}
-                titleStyle={disabledOpacity5}
+              <ColorCard
+                header="custom"
+                customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q1} label={RISK_ASSESSMENT.QUESTION_1} />}
+                content={
+                  <CustomTextInput
+                    disabled={true}
+                    label={RISK_ASSESSMENT.LABEL_DATE_OF_BIRTH}
+                    rightIcon={{ name: "calendar" }}
+                    value={dateOfBirth}
+                  />
+                }
               />
               <CustomSpacer space={sh16} />
-              <CustomTextInput
-                disabled={true}
-                label={RISK_ASSESSMENT.LABEL_DATE_OF_BIRTH}
-                rightIcon={{ name: "calendar" }}
-                value={dateOfBirth}
-              />
             </View>
-            <CustomSpacer space={sh32} />
-            <Question
-              label={RISK_ASSESSMENT.LABEL_QUESTION_2}
-              options={Q2_OPTIONS}
-              selected={questionTwo}
-              setSelected={setQ2}
-              title={RISK_ASSESSMENT.QUESTION_2}
-            />
-            <CustomSpacer space={sh32} />
-            <LabeledTitle
-              label={RISK_ASSESSMENT.LABEL_QUESTION_3}
-              labelStyle={fs10BoldGray6}
-              spaceToLabel={sh8}
-              title={RISK_ASSESSMENT.QUESTION_3}
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q2} label={RISK_ASSESSMENT.QUESTION_2} />}
+              content={<QuestionContent contentToRadioSpace={sw11} options={Q2_OPTIONS} selected={questionTwo} setSelected={setQ2} />}
             />
             <CustomSpacer space={sh16} />
-            <CustomSlider disabled={true} options={Q3_OPTIONS} selected={questionThree} setSelected={setQ3} />
-            <CustomSpacer space={sh32} />
-            <Question
-              label={RISK_ASSESSMENT.LABEL_QUESTION_4}
-              options={Q4_OPTIONS}
-              selected={questionFour}
-              setSelected={setQ4}
-              title={RISK_ASSESSMENT.QUESTION_4}
-            />
-            <CustomSpacer space={sh32} />
-            <Question
-              label={RISK_ASSESSMENT.LABEL_QUESTION_5}
-              options={Q5_OPTIONS}
-              right={<Image style={{ ...imageContain, height: sh143, width: sw140 }} source={LocalAssets.graph.risk_assessment_graph_1} />}
-              selected={questionFive}
-              setSelected={setQ5}
-              title={RISK_ASSESSMENT.QUESTION_5}
-            />
-            <CustomSpacer space={sh32} />
-            <Question
-              label={RISK_ASSESSMENT.LABEL_QUESTION_6}
-              options={Q6_OPTIONS}
-              right={<Image style={{ ...imageContain, height: sh155, width: sw221 }} source={LocalAssets.graph.risk_assessment_graph_2} />}
-              selected={questionSix}
-              setSelected={setQ6}
-              title={RISK_ASSESSMENT.QUESTION_6}
-            />
-            <CustomSpacer space={sh32} />
-            <LabeledTitle
-              label={RISK_ASSESSMENT.LABEL_QUESTION_7}
-              labelStyle={fs10BoldGray6}
-              spaceToLabel={sh8}
-              title={RISK_ASSESSMENT.QUESTION_7}
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q3} label={RISK_ASSESSMENT.QUESTION_3} />}
+              content={
+                <View style={{ paddingRight: sw152 }}>
+                  <CustomSlider
+                    bottomSpace={sh16}
+                    disabled={true}
+                    labelStyle={fs12RegGray6}
+                    options={Q3_OPTIONS}
+                    selected={questionThree}
+                    selectedLabelStyle={fs12BoldGray6}
+                    setSelected={setQ3}
+                  />
+                </View>
+              }
             />
             <CustomSpacer space={sh16} />
-            <CustomSlider disabled={true} options={Q7_OPTIONS} selected={questionSeven} setSelected={setQ7} />
-            <CustomSpacer space={sh32} />
-            <Question
-              label={RISK_ASSESSMENT.LABEL_QUESTION_8}
-              options={Q8_OPTIONS}
-              selected={questionEight}
-              setSelected={setQ8}
-              title={RISK_ASSESSMENT.QUESTION_8}
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q4} label={RISK_ASSESSMENT.QUESTION_4} />}
+              content={<QuestionContent contentToRadioSpace={sw11} options={Q4_OPTIONS_NEW} selected={questionFour} setSelected={setQ4} />}
+            />
+            <CustomSpacer space={sh16} />
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q5} label={RISK_ASSESSMENT.QUESTION_5} />}
+              content={
+                <QuestionContent
+                  contentToRadioSpace={sw11}
+                  options={Q5_OPTIONS}
+                  right={
+                    <Image style={{ ...imageContain, height: sh143, width: sw140 }} source={LocalAssets.graph.risk_assessment_graph_1} />
+                  }
+                  selected={questionFive}
+                  setSelected={setQ5}
+                />
+              }
+            />
+            <CustomSpacer space={sh16} />
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q6} label={RISK_ASSESSMENT.QUESTION_6} />}
+              content={
+                <QuestionContent
+                  contentToRadioSpace={sw11}
+                  options={Q6_OPTIONS}
+                  right={
+                    <Image style={{ ...imageContain, height: sh155, width: sw221 }} source={LocalAssets.graph.risk_assessment_graph_2} />
+                  }
+                  selected={questionSix}
+                  setSelected={setQ6}
+                />
+              }
+            />
+            <CustomSpacer space={sh16} />
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q7} label={RISK_ASSESSMENT.QUESTION_7} />}
+              content={
+                <View style={{ paddingRight: sw152 }}>
+                  <CustomSlider
+                    bottomSpace={sh16}
+                    disabled={true}
+                    labelStyle={fs12RegGray6}
+                    options={Q7_OPTIONS}
+                    selected={questionSeven}
+                    selectedLabelStyle={fs12BoldGray6}
+                    setSelected={setQ7}
+                  />
+                </View>
+              }
+            />
+            <CustomSpacer space={sh16} />
+            <ColorCard
+              header="custom"
+              customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q8} label={RISK_ASSESSMENT.QUESTION_8} />}
+              content={<QuestionContent contentToRadioSpace={sw11} options={Q8_OPTIONS} selected={questionEight} setSelected={setQ8} />}
             />
             {questionEight !== -1 && questionEight !== 0 ? (
               <Fragment>
-                <CustomSpacer space={sh32} />
-                <Question
-                  label={RISK_ASSESSMENT.LABEL_QUESTION_9}
-                  options={Q9_OPTIONS}
-                  RenderContent={(renderContentProps) => {
-                    const { options, selected, setSelected } = renderContentProps;
-                    return (
-                      <Fragment>
-                        {options!.map((option: ICheckBoxWithSubLabel, index: number) => {
-                          const infoContent =
-                            index === 1 ? (
-                              <View>
-                                <Text style={fs12BoldWhite1}>{RISK_ASSESSMENT.POPUP_ASSET_1}</Text>
-                                <Text style={fs12BoldWhite1}>{RISK_ASSESSMENT.POPUP_ASSET_2}</Text>
-                              </View>
-                            ) : (
-                              <View>
-                                <Text style={fs12BoldWhite1}>{RISK_ASSESSMENT.POPUP_INCOME}</Text>
-                              </View>
-                            );
-                          const defaultCondition: ReactElement | null = index <= 1 ? infoContent : <View />;
-
-                          const handleSelect = () => {
-                            const newIndex = options!.findIndex((search: ICheckBoxWithSubLabel) => search.label === option.label);
-                            setSelected(newIndex !== questionNine ? newIndex : -1);
-                          };
-
-                          const iconColor = index === selected ? colorWhite._1 : colorBlue._1;
-                          const circleStyle: ViewStyle =
-                            index === selected ? circleBorder(sw24, sw1, colorRed._1, colorRed._1) : circleBorder(sw24, sw1, colorBlue._1);
-                          const fontFamily = index === selected ? NunitoBold : NunitoRegular;
-
-                          return (
-                            <Fragment key={index}>
-                              {index !== 0 ? <CustomSpacer isHorizontal={false} space={sh16} /> : null}
-                              <View style={flexRow}>
-                                <TouchableWithoutFeedback onPress={handleSelect}>
-                                  <View style={{ ...centerVertical, ...flexRow, width: sw328 }}>
-                                    <View style={alignSelfStart}>
-                                      <View style={{ ...centerHV, ...circleStyle }}>
-                                        <IcoMoon name="success" size={sw16} color={iconColor} />
-                                      </View>
-                                    </View>
-                                    <CustomSpacer space={sw8} isHorizontal />
-                                    <Text style={{ ...fs12BoldGray6, fontFamily: fontFamily, maxWidth: sw326 }}>{option.label}</Text>
+                <CustomSpacer space={sh16} />
+                <ColorCard
+                  header="custom"
+                  customHeader={<QuestionHeader avatarText={RISK_ASSESSMENT.LABEL_Q9} label={RISK_ASSESSMENT.QUESTION_9} />}
+                  content={
+                    <QuestionContent
+                      options={Q9_OPTIONS}
+                      RenderContent={(renderContentProps) => {
+                        const { options, selected, setSelected } = renderContentProps;
+                        return (
+                          <Fragment>
+                            {options!.map((option: ICheckBoxWithSubLabel, index: number) => {
+                              const infoContent =
+                                index === 1 ? (
+                                  <View>
+                                    <Text style={fs12BoldWhite1}>{RISK_ASSESSMENT.POPUP_ASSET_1}</Text>
+                                    <Text style={fs12BoldWhite1}>{RISK_ASSESSMENT.POPUP_ASSET_2}</Text>
                                   </View>
-                                </TouchableWithoutFeedback>
-                                {index < 2 ? (
-                                  <Fragment>
-                                    <CustomSpacer isHorizontal={true} space={sw20} />
-                                    <CustomTooltip content={defaultCondition} arrowSize={{ width: sw12, height: sw7 }} />
-                                  </Fragment>
-                                ) : null}
-                              </View>
-                            </Fragment>
-                          );
-                        })}
-                      </Fragment>
-                    );
-                  }}
-                  selected={questionNine}
-                  setSelected={setQ9}
-                  title={RISK_ASSESSMENT.QUESTION_9}
+                                ) : (
+                                  <View>
+                                    <Text style={fs12BoldWhite1}>{RISK_ASSESSMENT.POPUP_INCOME}</Text>
+                                  </View>
+                                );
+                              const defaultCondition: ReactElement | null = index <= 1 ? infoContent : <View />;
+
+                              const handleSelect = () => {
+                                const newIndex = options!.findIndex((search: ICheckBoxWithSubLabel) => search.label === option.label);
+                                setSelected(newIndex !== questionNine ? newIndex : -1);
+                              };
+
+                              const iconColor = index === selected ? colorWhite._1 : colorBlue._1;
+                              const circleStyle: ViewStyle =
+                                index === selected
+                                  ? circleBorder(sw24, sw1, colorRed._1, colorRed._1)
+                                  : circleBorder(sw24, sw1, colorBlue._1);
+                              const fontFamily = index === selected ? NunitoBold : NunitoRegular;
+
+                              return (
+                                <Fragment key={index}>
+                                  {index !== 0 ? <CustomSpacer isHorizontal={false} space={sh16} /> : null}
+                                  <View style={flexRow}>
+                                    <TouchableWithoutFeedback onPress={handleSelect}>
+                                      <View style={rowCenterVertical}>
+                                        <View style={alignSelfStart}>
+                                          <View style={{ ...centerHV, ...circleStyle }}>
+                                            <IcoMoon name="success" size={sw16} color={iconColor} />
+                                          </View>
+                                        </View>
+                                        <CustomSpacer isHorizontal={true} space={sw11} />
+                                        <Text style={{ ...fs16RegBlack2, fontFamily: fontFamily }}>{option.label}</Text>
+                                      </View>
+                                    </TouchableWithoutFeedback>
+                                    <CustomSpacer isHorizontal={true} space={sw8} />
+                                    {index < 2 ? (
+                                      <Fragment>
+                                        <CustomTooltip content={defaultCondition} arrowSize={{ width: sw12, height: sw7 }} theme="dark" />
+                                      </Fragment>
+                                    ) : null}
+                                  </View>
+                                </Fragment>
+                              );
+                            })}
+                          </Fragment>
+                        );
+                      }}
+                      selected={questionNine}
+                      setSelected={setQ9}
+                    />
+                  }
                 />
               </Fragment>
             ) : null}
           </View>
-          <CustomSpacer isHorizontal={true} space={sw256} />
         </View>
       </ContentPage>
       {confirmModal !== undefined ? (

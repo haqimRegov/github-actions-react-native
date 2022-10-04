ba4769dfcb442e2d2f50de4a0b8a9e747eb889d9
Aldrin Castro <aldrin.castro@regovtech.com>
2022-10-04 09:16:11 +0800

    implemented web socket for duplicate login

--- src/navigation/Private.tsx
+++ src/navigation/Private.tsx
@@ -11,12 +11,18 @@ import { connect } from "react-redux";
 import { LocalAssets } from "../assets/images/LocalAssets";
 import { EventModal, PromptModal } from "../components";
 import { Language } from "../constants";
-import { DICTIONARY_INACTIVITY_COUNTDOWN, DICTIONARY_INACTIVITY_COUNTDOWN_SECONDS, DICTIONARY_INACTIVITY_TIMER } from "../data/dictionary";
-import { updateStorageData } from "../integrations";
+import {
+  DICTIONARY_INACTIVITY_COUNTDOWN,
+  DICTIONARY_INACTIVITY_COUNTDOWN_SECONDS,
+  DICTIONARY_INACTIVITY_TIMER,
+  ERRORS,
+} from "../data/dictionary";
+import { updateStorageData, WEB_SOCKET_CONFIG } from "../integrations";
 import { logout } from "../network-actions";
 import { DashboardPage, ForceUpdatePage, LogoutPage, NewSalesPage, OnboardingPage } from "../pages";
 import { GlobalMapDispatchToProps, GlobalMapStateToProps, GlobalStoreProps } from "../store";
 import { sw212 } from "../styles";
+import { AlertDialog, isNotEmpty } from "../utils";
 
 const { INACTIVITY } = Language.PAGE;
 
@@ -27,10 +33,12 @@ const PrivateRouteComponent: FunctionComponent<GlobalStoreProps> = (props: Globa
   const expired = useRef<boolean>(false);
   const lastActive = useRef<number | undefined>(undefined);
   const [inactivityStatus, setInactivityStatus] = useState<boolean>(true);
-  const [isLogout, setIsLogout] = useState<boolean>(false);
   const [dontShowEpf, setDontShowEpf] = useState<boolean>(false);
   const [eventPrompt, setEventPrompt] = useState<boolean>(false);
   const [countdown, setCountdown] = useState(DICTIONARY_INACTIVITY_COUNTDOWN_SECONDS);
+
+  const webSocketUrl = `wss://${WEB_SOCKET_CONFIG.url}/${WEB_SOCKET_CONFIG.stage}?username=${props.agent?.username}&x-api-key=${WEB_SOCKET_CONFIG.apiKey}`;
+  const ws = useRef(new WebSocket(webSocketUrl)).current;
 
   const handleExtend = () => {
     setInactivityStatus(true);
@@ -39,10 +47,18 @@ const PrivateRouteComponent: FunctionComponent<GlobalStoreProps> = (props: Globa
     }, 150);
   };
 
-  const handleLogout = async () => {
-    setIsLogout(true);
+  const handleCloseWS = () => {
+    if (isNotEmpty(ws)) {
+      const wsClose = ws.close();
+      // eslint-disable-next-line no-console
+      console.log("wsClose", wsClose);
+    }
+  };
+
+  const handleLogout = (globalLogout?: boolean) => {
     setInactivityStatus(true);
-    logout(navigation);
+    handleCloseWS();
+    logout(navigation, globalLogout);
   };
 
   const handleEvents = async () => {
@@ -71,6 +87,51 @@ const PrivateRouteComponent: FunctionComponent<GlobalStoreProps> = (props: Globa
   };
 
   useEffect(() => {
+    let webSocketInterval: ReturnType<typeof setInterval>;
+    const handlePing = () => {
+      if (ws !== undefined) {
+        ws.send("PING");
+        webSocketInterval = setInterval(() => {
+          ws.send("PING");
+        }, 10000);
+      }
+    };
+    ws.onopen = () => {
+      // connection opened
+      // eslint-disable-next-line no-console
+      console.log("onopen");
+      handlePing();
+    };
+    ws.onclose = (e) => {
+      // connection closed
+      // eslint-disable-next-line no-console
+      console.log("closed", e);
+    };
+    ws.onerror = (e) => {
+      // an error occurred
+      // eslint-disable-next-line no-console
+      console.log("error", e);
+    };
+    ws.onmessage = (e) => {
+      // a message was received
+      // eslint-disable-next-line no-console
+      console.log("message", e?.data);
+      if (isNotEmpty(e) && isNotEmpty(e?.data) && e?.data !== "PONG") {
+        const response = JSON.parse(e?.data) as IResponseError;
+        if (response.message === ERRORS.duplicateLogin.message) {
+          props.addGlobal({ ...props.global, isLogout: true });
+          clearInterval(webSocketInterval);
+          AlertDialog(ERRORS.duplicateLogin.message, () => handleLogout(false), "You've been logged out");
+        }
+      }
+    };
+    return () => {
+      clearInterval(webSocketInterval);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
     AppState.addEventListener("change", handleAppStateChange);
     return () => {
       AppState.removeEventListener("change", handleAppStateChange);
@@ -92,6 +153,7 @@ const PrivateRouteComponent: FunctionComponent<GlobalStoreProps> = (props: Globa
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (inactivityStatus === false) {
+        props.addGlobal({ ...props.global, isLogout: true });
         handleLogout();
       }
     }, DICTIONARY_INACTIVITY_COUNTDOWN);
@@ -134,12 +196,7 @@ const PrivateRouteComponent: FunctionComponent<GlobalStoreProps> = (props: Globa
     <Fragment>
       <UserInactivity isActive={inactivityStatus} onAction={handleInactivity} timeForInactivity={DICTIONARY_INACTIVITY_TIMER}>
         <Navigator initialRouteName="Dashboard" screenOptions={{ headerShown: false }}>
-          <Screen
-            name="Dashboard"
-            component={DashboardPage}
-            initialParams={{ isLogout: isLogout, setIsLogout: setIsLogout }}
-            options={defaultOptions}
-          />
+          <Screen name="Dashboard" component={DashboardPage} options={defaultOptions} />
           <Screen name="Logout" component={LogoutPage} options={defaultOptions} />
           <Screen name="Onboarding" component={OnboardingPage} options={defaultOptions} />
           <Screen name="ForceUpdate" component={ForceUpdatePage} options={defaultOptions} />

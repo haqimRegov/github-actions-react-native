11854f925aa00602ab0412e9f017b5e8a560f12e
Aldrin Castro <aldrin.castro@regovtech.com>
2022-11-14 10:49:42 +0800

    [ios] native exception handling alert (not working when modal is present)

--- ios/KenangaMobile/AppDelegate.m
+++ ios/KenangaMobile/AppDelegate.m
@@ -7,6 +7,7 @@
 #import <Firebase.h>
 #import <UserNotifications/UserNotifications.h>
 #import <RNCPushNotificationIOS.h>
+#import "ReactNativeExceptionHandler.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -34,7 +35,7 @@ static void InitializeFlipper(UIApplication *application) {
 // Required for the register event.
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
- [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+  [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
 }
 // Required for the notification event. You must call the completion handler after handling the remote notification.
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
@@ -45,49 +46,87 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 // Required for the registrationError event.
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
- [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
+  [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
 }
 // Required for localNotification event
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
 didReceiveNotificationResponse:(UNNotificationResponse *)response
          withCompletionHandler:(void (^)(void))completionHandler
 {
+  
   [RNCPushNotificationIOS didReceiveNotificationResponse:response];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    if ([FIRApp defaultApp] == nil) {
+  if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
   }
-
+  
   // Define UNUserNotificationCenter
-   UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-   center.delegate = self;
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
   
   
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"KenangaMobile"
                                             initialProperties:nil];
-
+  
   if (@available(iOS 13.0, *)) {
-      rootView.backgroundColor = [UIColor systemBackgroundColor];
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
-      rootView.backgroundColor = [UIColor whiteColor];
+    rootView.backgroundColor = [UIColor whiteColor];
   }
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [RNSplashScreen show];
+  [RNSplashScreen show];  
 
+  // Native Exception Handler (alert not working when a modal is already present)
+  [ReactNativeExceptionHandler replaceNativeExceptionHandlerBlock:^(NSException *exception, NSString *readeableException){
+    // THE CODE YOU WRITE HERE WILL REPLACE THE EXISTING NATIVE POPUP THAT COMES WITH THIS MODULE.
+    //We create an alert box
+    UIAlertController* alert = [UIAlertController
+                                alertControllerWithTitle:@"Something went wrong"
+                                message:@"Sorry, we encountered an unexpected error. Please contact support for more details.\n\nError Code: R004"
+                                preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+       handler:^(UIAlertAction * action) {
+      [ReactNativeExceptionHandler releaseExceptionHold];
+    }];
+     
+    [alert addAction:defaultAction];  
+  
+    // We show the alert box using the rootViewController
+    [rootViewController presentViewController:alert animated:YES completion:nil];
+    // THIS IS THE IMPORTANT PART
+    // By default when an exception is raised we will show an alert box as per our code.
+    // But since our buttons wont work because our click handlers wont work.
+    // to close the app or to remove the UI lockup on exception.
+    // we need to call this method
+    // [ReactNativeExceptionHandler releaseExceptionHold]; // to release the lock and let the app crash.
+
+    // Hence we set a timer of 4 secs and then call the method releaseExceptionHold to quit the app after
+    // 4 secs of showing the popup
+//    [NSTimer scheduledTimerWithTimeInterval:4.0
+//                                     target:[ReactNativeExceptionHandler class]
+//                                   selector:@selector(releaseExceptionHold)
+//                                   userInfo:nil
+//                                    repeats:NO];
+
+    // or  you can call
+    // [ReactNativeExceptionHandler releaseExceptionHold]; when you are done to release the UI lock.
+  }];
+  
   return YES;
 }
 

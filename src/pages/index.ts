bbcc4330fd7b600334cdc36592a11d26f54fe082
Aldrin Castro <aldrin.castro@regovtech.com>
2022-11-14 10:50:33 +0800

    added GlobalError handler without using components as much as possible

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 export * from "./Auth";
 export * from "./Dashboard";
 export * from "./ForceUpdate";
+export * from "./GlobalError";
 export * from "./Logout";
 export * from "./NewSales";
 export * from "./Onboarding";

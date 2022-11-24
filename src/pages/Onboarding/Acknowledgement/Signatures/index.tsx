8c93463a80fe71636a4fc276916bd450976e9e24
Nithin <johnnithin08@gmail.com>
2022-11-24 12:28:53 +0800

    [update]: removed redundant usage

--- src/pages/Onboarding/Acknowledgement/Signatures/index.tsx
+++ src/pages/Onboarding/Acknowledgement/Signatures/index.tsx
@@ -4,12 +4,20 @@ import { connect } from "react-redux";
 
 import { AcknowledgementMapDispatchToProps, AcknowledgementMapStateToProps, AcknowledgementStoreProps } from "../../../../store";
 import { flexChild } from "../../../../styles";
-import { EditPdf } from "./EditPDF";
+import { EditPdf } from "../../../../templates/Signatures/EditPDF";
 import { PDFList } from "./PDFList";
 
 interface SignaturesProps extends AcknowledgementStoreProps, OnboardingContentProps {}
 
-export const SignaturesComponent: FunctionComponent<SignaturesProps> = ({ handleNextStep, handleResetOnboarding }: SignaturesProps) => {
+export const SignaturesComponent: FunctionComponent<SignaturesProps> = ({
+  accountType,
+  handleNextStep,
+  handleResetOnboarding,
+  newSales,
+  personalInfo,
+  receipts,
+  updateReceipts,
+}: SignaturesProps) => {
   const [editReceipt, setEditReceipt] = useState<IOnboardingReceiptState | undefined>(undefined);
   const [pageWidth, setPageWidth] = useState<number>(0);
 
@@ -22,7 +30,17 @@ export const SignaturesComponent: FunctionComponent<SignaturesProps> = ({ handle
       {editReceipt === undefined ? (
         <PDFList handleNextStep={handleNextStep} handleResetOnboarding={handleResetOnboarding} setEditReceipt={setEditReceipt} />
       ) : (
-        <EditPdf pageWidth={pageWidth} setEditReceipt={setEditReceipt} editReceipt={editReceipt} />
+        <EditPdf
+          accountType={accountType}
+          currentTransactionType={"Sales-AO"}
+          editReceipt={editReceipt}
+          newSales={newSales}
+          pageWidth={pageWidth}
+          personalInfo={personalInfo}
+          receipts={receipts}
+          setEditReceipt={setEditReceipt}
+          updateReceipts={updateReceipts}
+        />
       )}
     </View>
   );

32840e6c3a2cfaf75085f732709ed4c560d8161f
rgzahir <zahir.ayob@regovtech.com>
2023-01-17 13:23:11 +0800

    [update] employment details ui & logic

--- src/pages/Onboarding/EmploymentDetails/Principal.tsx
+++ src/pages/Onboarding/EmploymentDetails/Principal.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback, useEffect, useMemo, useState } from "react";
 import { View } from "react-native";
 
-import { AccountHeader, defaultContentProps, LabeledTitle } from "../../../components";
+import { defaultContentProps, LabeledTitle } from "../../../components";
 import { Language } from "../../../constants";
+import { DICTIONARY_COUNTRIES, EMPLOYMENT_EXEMPTIONS } from "../../../data/dictionary";
 import { px, sw24 } from "../../../styles";
 import { EmploymentInfo } from "./Details";
 
@@ -13,24 +14,91 @@ interface PrincipalEmploymentDetails {
   employmentDetails: IEmploymentDetailsState;
   personalDetails: IPersonalDetailsState;
   setEmploymentDetails: (value: IEmploymentDetailsState) => void;
+  setPersonalInfoDetails: (value: IPersonalDetailsState) => void;
   setValidations: (value: IEmploymentDetailsValidations) => void;
   validations: IEmploymentDetailsValidations;
 }
+
+const initialPrincipalEmploymentDetails: IEmploymentDetailsState = {
+  businessNature: "",
+  employerName: "",
+  occupation: "",
+  isOptional: false,
+  othersOccupation: "",
+  address: {
+    line1: "",
+    line2: undefined,
+    line3: undefined,
+  },
+  city: "",
+  country: DICTIONARY_COUNTRIES[0].value,
+  postCode: "",
+  state: "",
+};
 
 export const PrincipalEmploymentDetails: FunctionComponent<PrincipalEmploymentDetails> = ({
   accountType,
   employmentDetails,
   personalDetails,
   setEmploymentDetails,
+  setPersonalInfoDetails,
   setValidations,
   validations,
 }: PrincipalEmploymentDetails) => {
   const padding = accountType === "Joint" ? px(sw24) : {};
 
+  const handleOccupation = (occupation: string) => {
+    if (employmentDetails.isOptional === false && EMPLOYMENT_EXEMPTIONS.indexOf(occupation) !== -1) {
+      setEmploymentDetails({
+        ...initialPrincipalEmploymentDetails,
+        occupation: occupation,
+        country: "",
+        isOptional: employmentDetails.isOptional,
+      });
+    } else if (employmentDetails.isOptional === true && EMPLOYMENT_EXEMPTIONS.indexOf(occupation) !== -1) {
+      setEmploymentDetails({
+        ...initialPrincipalEmploymentDetails,
+        occupation: occupation,
+        isOptional: employmentDetails.isOptional,
+      });
+    } else if (occupation === "Others") {
+      setEmploymentDetails({
+        ...initialPrincipalEmploymentDetails,
+        occupation: occupation,
+        othersOccupation: "",
+        isOptional: employmentDetails.isOptional,
+      });
+    } else {
+      setEmploymentDetails({
+        ...initialPrincipalEmploymentDetails,
+        occupation: occupation,
+        isOptional: employmentDetails.isOptional,
+      });
+    }
+  };
+
+  const handleToggle = (value: boolean) => {
+    if (value === true) {
+      setEmploymentDetails({
+        ...initialPrincipalEmploymentDetails,
+        occupation: employmentDetails.occupation,
+        isOptional: value,
+      });
+    }
+
+    if (value === false) {
+      setEmploymentDetails({
+        ...initialPrincipalEmploymentDetails,
+        occupation: employmentDetails.occupation,
+        country: "",
+        isOptional: value,
+      });
+    }
+  };
+
   return (
     <View>
       <View style={px(sw24)}>
-        {accountType === "Joint" ? <AccountHeader subtitle={EMPLOYMENT_DETAILS.LABEL_PRINCIPAL} title={personalDetails.name!} /> : null}
         <LabeledTitle
           label={EMPLOYMENT_DETAILS.HEADING}
           labelStyle={defaultContentProps.subheadingStyle}
@@ -44,7 +112,11 @@ export const PrincipalEmploymentDetails: FunctionComponent<PrincipalEmploymentDe
         <EmploymentInfo
           accountType={accountType}
           employmentDetails={employmentDetails}
+          handleOccupation={handleOccupation}
+          handleToggle={handleToggle}
+          personalDetails={personalDetails}
           setEmploymentDetails={setEmploymentDetails}
+          setPersonalInfoDetails={setPersonalInfoDetails}
           setValidations={setValidations}
           validations={validations}
         />

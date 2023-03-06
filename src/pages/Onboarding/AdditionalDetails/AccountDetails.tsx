add9e07af9ba7e8094d50fa4736b89863909e52d
Aldrin Castro <aldrin.castro@regovtech.com>
2023-03-06 18:21:36 +0800

    used new IncomeDistribution from templates

--- src/pages/Onboarding/AdditionalDetails/AccountDetails.tsx
+++ src/pages/Onboarding/AdditionalDetails/AccountDetails.tsx
@@ -4,6 +4,7 @@ import React, { Fragment, FunctionComponent } from "react";
 import { AdvanceToggleButton, ColorCard, CustomSpacer } from "../../../components";
 import { Language } from "../../../constants";
 import { alignSelfCenter, centerHV, fs16BoldBlack2, sh24, sw12, sw14, sw18, sw24, sw240 } from "../../../styles";
+import { IncomeDistribution } from "../../../templates";
 
 const { ADDITIONAL_DETAILS } = Language.PAGE;
 
@@ -30,31 +31,22 @@ export const AccountDetails: FunctionComponent<AccountDetailsProps> = ({
     );
   });
 
-  const distributionOptions: ICheckBoxWithSubLabel[] = [
-    { label: ADDITIONAL_DETAILS.OPTION_DISTRIBUTION_REINVEST, value: ADDITIONAL_DETAILS.OPTION_DISTRIBUTION_REINVEST },
-  ];
-
-  if (fundsWithPayout.length !== 0) {
-    distributionOptions.push({
-      label: ADDITIONAL_DETAILS.OPTION_DISTRIBUTION_PAYOUT,
-      value: ADDITIONAL_DETAILS.OPTION_DISTRIBUTION_PAYOUT,
-    });
-  }
-
   const jointAge = accountType === "Joint" ? moment().diff(personalInfo.joint!.personalDetails!.dateOfBirth, "years") : undefined;
   const operatingControl: ICheckBoxWithSubLabel[] = [
     { label: ADDITIONAL_DETAILS.OPTION_CONTROL_PRINCIPAL, value: ADDITIONAL_DETAILS.OPTION_CONTROL_PRINCIPAL },
   ];
+
   if (accountType === "Joint" && jointAge !== undefined && jointAge >= 18) {
     operatingControl.push(
       { label: ADDITIONAL_DETAILS.OPTION_CONTROL_BOTH, value: ADDITIONAL_DETAILS.OPTION_CONTROL_BOTH },
       { label: ADDITIONAL_DETAILS.OPTION_CONTROL_EITHER, value: ADDITIONAL_DETAILS.OPTION_CONTROL_EITHER },
     );
   }
-  const handleInputDistribution = (index: number) => {
-    const value = index !== -1 ? distributionOptions[index].value : "";
+
+  const setInputDistribution = (value: string) => {
     setPersonalInfo({ incomeDistribution: value });
   };
+
   const handleInputSignatory = (index: number) => {
     const value = index !== -1 ? operatingControl[index].value : "";
     setPersonalInfo({ signatory: value });
@@ -62,23 +54,10 @@ export const AccountDetails: FunctionComponent<AccountDetailsProps> = ({
 
   return (
     <Fragment>
-      <ColorCard
-        header={{ label: ADDITIONAL_DETAILS.LABEL_DISTRIBUTION, title: ADDITIONAL_DETAILS.HINT_DISTRIBUTION }}
-        content={
-          <AdvanceToggleButton
-            buttonContainerStyle={alignSelfCenter}
-            buttonStyle={{ ...centerHV, height: sw18, width: sw18 }}
-            contentToRadioSpace={sw12}
-            direction="row"
-            iconSize={sw14}
-            labels={distributionOptions}
-            labelStyle={fs16BoldBlack2}
-            onSelect={handleInputDistribution}
-            space={sw24}
-            style={{ width: sw240 }}
-            value={distributionOptions.findIndex((eachIndex) => eachIndex.value === inputDistribution)}
-          />
-        }
+      <IncomeDistribution
+        inputDistribution={inputDistribution}
+        setInputDistribution={setInputDistribution}
+        withPayout={fundsWithPayout.length > 0}
       />
       {accountType === "Individual" ? null : (
         <Fragment>
